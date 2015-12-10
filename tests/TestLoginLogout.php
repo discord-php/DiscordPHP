bb7669c30efcb9f275b0c913181d6d94799b00bd
Uniquoooo <cheese5505@gmail.com>
2015-12-10 20:39:27 +1300

    Trying out PHPUnit and Travis CI

---
+++ tests/TestLoginLogout.php
@@ -0,0 +1,28 @@
+<?php
+
+use Discord\Discord;
+
+class TestLoginLogout extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * Tests logging into Discord.
+	 *
+	 * @return void 
+	 */
+	public function testLoggingIn()
+	{
+		$discord = new Discord(getenv('DISCORD_EMAIL'), getenv('DISCORD_PASSWORD'));
+	}
+
+	/**
+	 * Tests logging out of Discord.
+	 *
+	 * @return void 
+	 */
+	public function testLoggingOut()
+	{
+		$discord = new Discord(getenv('DISCORD_EMAIL'), getenv('DISCORD_PASSWORD'));
+
+		$discord->logout();
+	}
+}
