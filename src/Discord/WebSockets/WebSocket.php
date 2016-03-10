0d9330476a76d97dccddd6070ced995e87a8158c
David <david@team-reflex.com>
2016-03-10 21:40:02 +1300

    cherry-picking from voice

--- src/Discord/WebSockets/WebSocket.php
+++ src/Discord/WebSockets/WebSocket.php
@@ -80,6 +80,27 @@ class WebSocket extends EventEmitter
     protected $reconnectCount = 0;
 
     /**
+     * If the WebSocket is reconnecting.
+     *
+     * @var bool Whether the WebSocket is reconnecting.
+     */
+    protected $reconnecting = false;
+
+    /**
+     * The Voice Client instance.
+     *
+     * @var VoiceClient The Voice Client.
+     */
+    protected $voice;
+
+    /**
+     * The WebSocket heartbeat.
+     *
+     * @var TimerInterface The WebSocket heartbeat.
+     */
+    protected $heartbeat;
+
+    /**
      * Constructs the WebSocket instance.
      *
      * @param Discord            $discord The Discord REST client instance.
@@ -220,7 +241,11 @@ class WebSocket extends EventEmitter
                 $this->discord->setCache('guilds', $guilds);
 
                 // after we do everything, emit ready
-                $this->emit('ready', [$this->discord]);
+                if (! $this->reconnecting) {
+                    $this->emit('ready', [$this->discord]);
+                }
+
+                $this->reconnecting = false;
             }
         });
 
@@ -234,11 +259,14 @@ class WebSocket extends EventEmitter
                 return;
             }
 
-            $this->emit('reconnecting', [$this->discord]);
+            if (! $this->reconnecting) {
+                $this->emit('reconnecting', [$this->discord]);
 
-            $this->getGateway();
-            $this->wsfactory->createConnection($this->gateway)->then([$this, 'handleWebSocketConnection'], [$this, 'handleWebSocketError']);
-            ++$this->reconnectCount;
+                $this->reconnecting = true;
+                $this->getGateway();
+                $this->wsfactory->createConnection($this->gateway)->then([$this, 'handleWebSocketConnection'], [$this, 'handleWebSocketError']);
+                ++$this->reconnectCount;
+            }
         });
 
         $ws->on('error', function ($error, $ws) {
@@ -260,17 +288,6 @@ class WebSocket extends EventEmitter
     public function handleWebSocketError($e)
     {
         $this->emit('ws-connect-error', [$e]);
-
-        if ($this->reconnectCount >= 4) {
-            $this->emit('ws-reconnect-max', [$this->discord]);
-            $this->loop->stop();
-
-            return;
-        }
-
-        $this->getGateway();
-        $this->wsfactory->createConnection($this->gateway)->then([$this, 'handleWebSocketConnection'], [$this, 'handleWebSocketError']);
-        ++$this->reconnectCount;
     }
 
     /**
