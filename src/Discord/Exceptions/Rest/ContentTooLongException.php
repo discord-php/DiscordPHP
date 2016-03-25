96e82be4aa402c48381a9898e540eb8e824002fa
David Cole <david@team-reflex.com>
2016-03-25 18:43:10 +1300

    Imported DiscordRequestFailedException

--- src/Discord/Exceptions/Rest/ContentTooLongException.php
+++ src/Discord/Exceptions/Rest/ContentTooLongException.php
@@ -11,6 +11,8 @@
 
 namespace Discord\Exceptions\Rest;
 
+use Discord\Exceptions\DiscordRequestFailedException;
+
 /**
  * Thrown when the Discord servers return `content longer than 2000 characters` after
  * a REST request. The user must use WebSockets to obtain this data if they need it.
