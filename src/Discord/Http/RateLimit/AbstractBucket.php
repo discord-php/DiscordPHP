b2ef0afc76337928e29599c53837560ca847badb
David <david@team-reflex.com>
2016-04-13 04:10:18 -0400

    Applied fixes from StyleCI

--- src/Discord/Http/RateLimit/AbstractBucket.php
+++ src/Discord/Http/RateLimit/AbstractBucket.php
@@ -1,90 +1,98 @@
 <?php
 
+/*
+ * This file is apart of the DiscordPHP project.
+ *
+ * Copyright (c) 2016 David Cole <david@team-reflex.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
 namespace Discord\Http\RateLimit;
 
-use Discord\Http\HttpDriver;
 use React\EventLoop\LoopInterface;
 use React\Promise\Deferred;
 
 abstract class AbstractBucket
 {
-	/**
-	 * The ReactPHP event loop.
-	 *
-	 * @var LoopInterface Event loop.
-	 */
-	protected $loop;
+    /**
+     * The ReactPHP event loop.
+     *
+     * @var LoopInterface Event loop.
+     */
+    protected $loop;
 
-	/**
-	 * How many requests the bucket can handle
-	 * within the time specified in $time.
-	 *
-	 * @var int Total requests.
-	 */
-	protected $uses;
+    /**
+     * How many requests the bucket can handle
+     * within the time specified in $time.
+     *
+     * @var int Total requests.
+     */
+    protected $uses;
 
-	/**
-	 * How often the bucket resets.
-	 *
-	 * @var int Bucket reset time.
-	 */
-	protected $time;
+    /**
+     * How often the bucket resets.
+     *
+     * @var int Bucket reset time.
+     */
+    protected $time;
 
-	/**
-	 * The current use count.
-	 *
-	 * @var int How many requests have been run so far.
-	 */
-	protected $currentCount = 0;
+    /**
+     * The current use count.
+     *
+     * @var int How many requests have been run so far.
+     */
+    protected $currentCount = 0;
 
-	/**
-	 * Array of current promises.
-	 *
-	 * @var array Promise array.
-	 */
-	protected $promises = [];
+    /**
+     * Array of current promises.
+     *
+     * @var array Promise array.
+     */
+    protected $promises = [];
 
-	/**
-	 * Constructs a Rate Limit bucket.
-	 *
-	 * @param LoopInterface $loop   The ReactPHP event loop.
-	 *
-	 * @return void 
-	 */
-	public function __construct(LoopInterface $loop)
-	{
-		$this->loop = $loop;
+    /**
+     * Constructs a Rate Limit bucket.
+     *
+     * @param LoopInterface $loop The ReactPHP event loop.
+     *
+     * @return void
+     */
+    public function __construct(LoopInterface $loop)
+    {
+        $this->loop = $loop;
 
-		$this->loop->addPeriodicTimer($this->time, function () {
-			$this->currentCount = 0;
+        $this->loop->addPeriodicTimer($this->time, function () {
+            $this->currentCount = 0;
 
-			$promises = $this->promises;
-			$this->promises = [];
+            $promises = $this->promises;
+            $this->promises = [];
 
-			foreach ($promises as $deferred) {
-				$this->handle()->then(function () use ($deferred) {
-					$deferred->resolve();
-				});
-			}
-		});
-	}
+            foreach ($promises as $deferred) {
+                $this->handle()->then(function () use ($deferred) {
+                    $deferred->resolve();
+                });
+            }
+        });
+    }
 
-	/**
-	 * Handles a request on the bucket.
-	 *
-	 * @return \React\Promise\Promise 
-	 */
-	public function handle()
-	{
-		$deferred = new Deferred();
+    /**
+     * Handles a request on the bucket.
+     *
+     * @return \React\Promise\Promise
+     */
+    public function handle()
+    {
+        $deferred = new Deferred();
 
-		if ($this->currentCount >= $this->uses) {
-			$this->promises[] = $deferred;
-		} else {
-			++$this->currentCount;
-			$deferred->resolve();
-		}
+        if ($this->currentCount >= $this->uses) {
+            $this->promises[] = $deferred;
+        } else {
+            ++$this->currentCount;
+            $deferred->resolve();
+        }
 
-		return $deferred->promise();
-	}
+        return $deferred->promise();
+    }
 }
