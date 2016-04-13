219878ccf637119b942e348e644295ea484c0578
David <david@team-reflex.com>
2016-04-13 07:54:09 -0400

    Applied fixes from StyleCI

--- src/Discord/Repository/PrivateChannelRepository.php
+++ src/Discord/Repository/PrivateChannelRepository.php
@@ -1,9 +1,17 @@
 <?php
 
+/*
+ * This file is apart of the DiscordPHP project.
+ *
+ * Copyright (c) 2016 David Cole <david@team-reflex.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
 namespace Discord\Repository;
 
 use Discord\Parts\Channel\Channel;
-use Discord\Repository\AbstractRepository;
 
 class PrivateChannelRepository extends AbstractRepository
 {
