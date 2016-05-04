64cf257edac4c1816c171dd9cbe7db49db40b478
Aaron Scherer <aequasi@gmail.com>
2016-05-03 22:21:55 -0700

    syntax error

--- src/Discord/Parts/Guild/Guild.php
+++ src/Discord/Parts/Guild/Guild.php
@@ -356,7 +356,7 @@ class Guild extends Part
             return;
         }
 
-        return "https://cdn.discordapp.com/icons/{$this->attributes['id']}/{$this->attributes['icon']}.jpg"
+        return "https://cdn.discordapp.com/icons/{$this->attributes['id']}/{$this->attributes['icon']}.jpg";
     }
 
     /**
@@ -380,7 +380,7 @@ class Guild extends Part
             return;
         }
 
-        return "https://cdn.discordapp.com/splash/{$this->attributes['id']}/{$this->attributes['icon']}.jpg"
+        return "https://cdn.discordapp.com/splash/{$this->attributes['id']}/{$this->attributes['icon']}.jpg";
     }
 
     /**
