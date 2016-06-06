a7c20b6e7d469c668023edc65fc1cdc1fe2c6611
David Cole <david@team-reflex.com>
2016-06-06 20:15:15 +1200

    Fixing `GUILD_UPDATE` events

--- src/Discord/WebSockets/Events/GuildUpdate.php
+++ src/Discord/WebSockets/Events/GuildUpdate.php
@@ -50,61 +50,7 @@ class GuildUpdate extends Event
             $roles->push($rolePart);
         }
 
-        $channels = new ChannelRepository(
-            $this->http,
-            $this->cache,
-            $this->factory
-        );
-
-        foreach ($data->channels as $channel) {
-            $channel = (array) $channel;
-            $channel['guild_id'] = $data->id;
-            $channelPart = $this->factory->create(Channel::class, $channel, true);
-
-            $this->cache->set("channel.{$channelPart->id}", $channelPart);
-            $channels->push($channelPart);
-        }
-
-        $members = new MemberRepository(
-            $this->http,
-            $this->cache,
-            $this->factory
-        );
-
-        foreach ($data->members as $member) {
-            $memberPart = $this->factory->create(Member::class, [
-                'user' => $member->user,
-                'roles' => $member->roles,
-                'mute' => $member->mute,
-                'deaf' => $member->deaf,
-                'joined_at' => $member->joined_at,
-                'guild_id' => $data->id,
-                'status' => 'offline',
-                'game' => null,
-            ], true);
-
-            foreach ($data->presences as $presence) {
-                if ($presence->user->id == $member->user->id) {
-                    $memberPart->status = $presence->status;
-                    $memberPart->game = $presence->game;
-                }
-            }
-
-            $this->cache->set("guild.{$guildPart->id}.members.{$memberPart->id}", $memberPart);
-            $this->cache->set("user.{$memberPart->id}", $memberPart->user);
-            $this->discord->users->push($memberPart->user);
-            $members->push($memberPart);
-        }
-
-        foreach ($data->voice_states as $state) {
-            if ($channel = $guildPart->channels->get('id', $state->channel_id)) {
-                $channel->members->push($this->factory->create(VoiceStateUpdatePart::class, (array) $state, true));
-            }
-        }
-
         $guildPart->roles = $roles;
-        $guildPart->channels = $channels;
-        $guildPart->members = $members;
 
         if ($guildPart->large) {
             $this->discord->addLargeGuild($guildPart);
