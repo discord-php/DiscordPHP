ccf35b3db3d49aaf6d0c95dc8227c65bf666e80b
David <david@team-reflex.com>
2016-07-04 01:55:50 -0400

    Applied fixes from StyleCI

--- src/Discord/CommandClient/Command.php
+++ src/Discord/CommandClient/Command.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is apart of the DiscordPHP project.
+ *
+ * Copyright (c) 2016 David Cole <david@team-reflex.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
 namespace Discord\CommandClient;
 
 use Discord\DiscordCommandClient;
@@ -7,109 +16,109 @@ use Discord\Parts\Channel\Message;
 
 class Command
 {
-	protected $command;
-	protected $description;
-	protected $usage;
-	protected $subCommands = [];
-	protected $subCommandAliases = [];
+    protected $command;
+    protected $description;
+    protected $usage;
+    protected $subCommands       = [];
+    protected $subCommandAliases = [];
 
-	/**
-	 * Creates a command instance.
-	 *
-	 * @param DiscordCommandClient $client      The Discord Command Client.
-	 * @param string               $command     The command trigger.
-	 * @param \Callable            $callable    The callable function.
-	 * @param string               $description The description of the command.
-	 * @param string               $usage       The usage of the command.
-	 */
-	public function __construct(
-		DiscordCommandClient $client,
-		$command,
-		callable $callable,
-		$description,
-		$usage
-	) {
-		$this->client = $client;
-		$this->command = $command;
-		$this->callable = $callable;
-		$this->description = $description;
-		$this->usage = $usage;
-	}
+    /**
+     * Creates a command instance.
+     *
+     * @param DiscordCommandClient $client      The Discord Command Client.
+     * @param string               $command     The command trigger.
+     * @param \Callable            $callable    The callable function.
+     * @param string               $description The description of the command.
+     * @param string               $usage       The usage of the command.
+     */
+    public function __construct(
+        DiscordCommandClient $client,
+        $command,
+        callable $callable,
+        $description,
+        $usage
+    ) {
+        $this->client      = $client;
+        $this->command     = $command;
+        $this->callable    = $callable;
+        $this->description = $description;
+        $this->usage       = $usage;
+    }
 
-	/**
-	 * Registers a new command.
-	 *
-	 * @param string           $command  The command name.
-	 * @param \Callable|string $callable The function called when the command is executed.
-	 * @param array            $options  An array of options.
-	 *
-	 * @return Command The command instance.
-	 */
-	public function registerSubCommand($command, $callable, array $options = [])
-	{
-		if (array_key_exists($command, $this->subCommands)) {
-			throw new \Exception("A sub-command with the name {$command} already exists.");
-		}
+    /**
+     * Registers a new command.
+     *
+     * @param string           $command  The command name.
+     * @param \Callable|string $callable The function called when the command is executed.
+     * @param array            $options  An array of options.
+     *
+     * @return Command The command instance.
+     */
+    public function registerSubCommand($command, $callable, array $options = [])
+    {
+        if (array_key_exists($command, $this->subCommands)) {
+            throw new \Exception("A sub-command with the name {$command} already exists.");
+        }
 
-		list($commandInstance, $options) = $this->client->buildCommand($command, $callable, $options);
-		$this->subCommands[$command] = $commandInstance;
+        list($commandInstance, $options) = $this->client->buildCommand($command, $callable, $options);
+        $this->subCommands[$command]     = $commandInstance;
 
-		foreach ($options['aliases'] as $alias) {
-			$this->addSubCommandAlias($alias, $command);
-		}
+        foreach ($options['aliases'] as $alias) {
+            $this->addSubCommandAlias($alias, $command);
+        }
 
-		return $commandInstance;
-	}
+        return $commandInstance;
+    }
 
-	/**
-	 * Adds a sub-command alias.
-	 *
-	 * @param string $alias   The alias to add.
-	 * @param string $command The command.
-	 */
-	public function addSubCommandAlias($alias, $command)
-	{
-		$this->subCommandAliases[$alias] = $command;
-	}
+    /**
+     * Adds a sub-command alias.
+     *
+     * @param string $alias   The alias to add.
+     * @param string $command The command.
+     */
+    public function addSubCommandAlias($alias, $command)
+    {
+        $this->subCommandAliases[$alias] = $command;
+    }
 
-	/**
-	 * Executes the command.
-	 *
-	 * @param Message $message The message.
-	 * @param array   $args    An array of arguments.
-	 *
-	 * @return mixed The response.
-	 */
-	public function handle(Message $message, array $args)
-	{
-		$subCommand = array_shift($args);
+    /**
+     * Executes the command.
+     *
+     * @param Message $message The message.
+     * @param array   $args    An array of arguments.
+     *
+     * @return mixed The response.
+     */
+    public function handle(Message $message, array $args)
+    {
+        $subCommand = array_shift($args);
 
-		if (array_key_exists($subCommand, $this->subCommands)) {
-			return $this->subCommands[$subCommand]->handle($message, $args);
-		} elseif (array_key_exists($subCommand, $this->subCommandAliases)) {
-			return $this->subCommands[$this->subCommandAliases[$subCommand]]->handle($message, $args);
-		}
+        if (array_key_exists($subCommand, $this->subCommands)) {
+            return $this->subCommands[$subCommand]->handle($message, $args);
+        } elseif (array_key_exists($subCommand, $this->subCommandAliases)) {
+            return $this->subCommands[$this->subCommandAliases[$subCommand]]->handle($message, $args);
+        }
 
-		if (! is_null($subCommand)) {
-			array_unshift($args, $subCommand);
-		}
+        if (! is_null($subCommand)) {
+            array_unshift($args, $subCommand);
+        }
 
-		return call_user_func_array($this->callable, [$message, $args]);
-	}
+        return call_user_func_array($this->callable, [$message, $args]);
+    }
 
-	/**
-	 * Handles dynamic get calls to the class.
-	 *
-	 * @param string $variable The variable to get.
-	 *
-	 * @return mixed The value.
-	 */
-	public function __get($variable)
-	{
-		$allowed = ['command', 'description', 'usage'];
+    /**
+     * Handles dynamic get calls to the class.
+     *
+     * @param string $variable The variable to get.
+     *
+     * @return mixed The value.
+     */
+    public function __get($variable)
+    {
+        $allowed = ['command', 'description', 'usage'];
 
-		if (array_search($variable, $allowed) !== false) {
-			return $this->{$variable};
-		}
-	}
+        if (array_search($variable, $allowed) !== false) {
+            return $this->{$variable};
+        }
+    }
 }
--- src/Discord/DiscordCommandClient.php
+++ src/Discord/DiscordCommandClient.php
@@ -1,184 +1,192 @@
 <?php
 
+/*
+ * This file is apart of the DiscordPHP project.
+ *
+ * Copyright (c) 2016 David Cole <david@team-reflex.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
 namespace Discord;
 
 use Discord\CommandClient\Command;
-use Discord\Discord;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class DiscordCommandClient extends Discord
 {
-	protected $commandClientOptions;
-	protected $commands = [];
-	protected $aliases = [];
+    protected $commandClientOptions;
+    protected $commands = [];
+    protected $aliases  = [];
 
-	/**
-	 * Constructs a new command client.
-	 *
-	 * @param array $options An array of options.
-	 */
-	public function __construct(array $options = [])
-	{
-		$this->commandClientOptions = $this->resolveCommandClientOptions($options);
+    /**
+     * Constructs a new command client.
+     *
+     * @param array $options An array of options.
+     */
+    public function __construct(array $options = [])
+    {
+        $this->commandClientOptions = $this->resolveCommandClientOptions($options);
 
-		$discordOptions = array_merge($this->commandClientOptions['discordOptions'], [
-			'token' => $this->commandClientOptions['token'],
-		]);
+        $discordOptions = array_merge($this->commandClientOptions['discordOptions'], [
+            'token' => $this->commandClientOptions['token'],
+        ]);
 
-		parent::__construct($discordOptions);
+        parent::__construct($discordOptions);
 
-		$this->on('ready', function () {
-			$this->commandClientOptions['prefix'] = str_replace('@mention', (string) $this->user, $this->commandClientOptions['prefix']);
+        $this->on('ready', function () {
+            $this->commandClientOptions['prefix'] = str_replace('@mention', (string) $this->user, $this->commandClientOptions['prefix']);
 
-			$this->on('message', function ($message) {
-				if ($message->author->id == $this->id) {
-					return;
-				}
+            $this->on('message', function ($message) {
+                if ($message->author->id == $this->id) {
+                    return;
+                }
 
-				if (substr($message->content, 0, strlen($this->commandClientOptions['prefix'])) == $this->commandClientOptions['prefix']) {
-					$withoutPrefix = substr($message->content, strlen($this->commandClientOptions['prefix']));
-					$args = explode(' ', $withoutPrefix);
-					$command = array_shift($args);
+                if (substr($message->content, 0, strlen($this->commandClientOptions['prefix'])) == $this->commandClientOptions['prefix']) {
+                    $withoutPrefix = substr($message->content, strlen($this->commandClientOptions['prefix']));
+                    $args = explode(' ', $withoutPrefix);
+                    $command = array_shift($args);
 
-					if (array_key_exists($command, $this->commands)) {
-						$command = $this->commands[$command];
-					} elseif (array_key_exists($command, $this->aliases)) {
-						$command = $this->commands[$this->aliases[$command]];
-					} else {
-						// Command doesn't exist.
-						return;
-					}
+                    if (array_key_exists($command, $this->commands)) {
+                        $command = $this->commands[$command];
+                    } elseif (array_key_exists($command, $this->aliases)) {
+                        $command = $this->commands[$this->aliases[$command]];
+                    } else {
+                        // Command doesn't exist.
+                        return;
+                    }
 
-					$result = $command->handle($message, $args);
+                    $result = $command->handle($message, $args);
 
-					if (is_string($result)) {
-						$message->reply($result);
-					}
-				}
-			});
-		});
-	}
+                    if (is_string($result)) {
+                        $message->reply($result);
+                    }
+                }
+            });
+        });
+    }
 
-	/**
-	 * Registers a new command.
-	 *
-	 * @param string           $command  The command name.
-	 * @param \Callable|string $callable The function called when the command is executed.
-	 * @param array            $options  An array of options.
-	 *
-	 * @return Command The command instance.
-	 */
-	public function registerCommand($command, $callable, array $options = [])
-	{
-		if (array_key_exists($command, $this->commands)) {
-			throw new \Exception("A command with the name {$command} already exists.");
-		}
+    /**
+     * Registers a new command.
+     *
+     * @param string           $command  The command name.
+     * @param \Callable|string $callable The function called when the command is executed.
+     * @param array            $options  An array of options.
+     *
+     * @return Command The command instance.
+     */
+    public function registerCommand($command, $callable, array $options = [])
+    {
+        if (array_key_exists($command, $this->commands)) {
+            throw new \Exception("A command with the name {$command} already exists.");
+        }
 
-		list($commandInstance, $options) = $this->buildCommand($command, $callable, $options);
-		$this->commands[$command] = $commandInstance;
+        list($commandInstance, $options) = $this->buildCommand($command, $callable, $options);
+        $this->commands[$command]        = $commandInstance;
 
-		foreach ($options['aliases'] as $alias) {
-			$this->addCommandAlias($alias, $command);
-		}
+        foreach ($options['aliases'] as $alias) {
+            $this->addCommandAlias($alias, $command);
+        }
 
-		return $commandInstance;
-	}
+        return $commandInstance;
+    }
 
-	/**
-	 * Adds a command alias.
-	 *
-	 * @param string $alias   The alias to add.
-	 * @param string $command The command.
-	 */
-	public function addCommandAlias($alias, $command)
-	{
-		$this->aliases[$alias] = $command;
-	}
+    /**
+     * Adds a command alias.
+     *
+     * @param string $alias   The alias to add.
+     * @param string $command The command.
+     */
+    public function addCommandAlias($alias, $command)
+    {
+        $this->aliases[$alias] = $command;
+    }
 
-	/**
-	 * Builds a command and returns it.
-	 *
-	 * @param string           $command  The command name.
-	 * @param \Callable|string $callable The function called when the command is executed.
-	 * @param array            $options  An array of options.
-	 *
-	 * @return array[Command, array] The command instance and options.
-	 */
-	public function buildCommand($command, $callable, array $options = [])
-	{
-		if (is_string($callable)) {
-			$callable = function ($message) use ($callable) {
-				return $callable;
-			};
-		} elseif (is_array($callable)) {
-			$callable = function ($message) use ($callable) {
-				return $callable[array_rand($callable)];
-			};
-		}
+    /**
+     * Builds a command and returns it.
+     *
+     * @param string           $command  The command name.
+     * @param \Callable|string $callable The function called when the command is executed.
+     * @param array            $options  An array of options.
+     *
+     * @return array[Command, array] The command instance and options.
+     */
+    public function buildCommand($command, $callable, array $options = [])
+    {
+        if (is_string($callable)) {
+            $callable = function ($message) use ($callable) {
+                return $callable;
+            };
+        } elseif (is_array($callable)) {
+            $callable = function ($message) use ($callable) {
+                return $callable[array_rand($callable)];
+            };
+        }
 
-		if (! is_callable($callable)) {
-			throw new \Exception('The callable parameter must be a string, array or callable.');
-		}
+        if (! is_callable($callable)) {
+            throw new \Exception('The callable parameter must be a string, array or callable.');
+        }
 
-		$options = $this->resolveCommandOptions($options);
+        $options = $this->resolveCommandOptions($options);
 
-		$commandInstance = new Command(
-			$this, $command, $callable,
-			$options['description'], $options['usage'], $options['aliases']);
+        $commandInstance = new Command(
+            $this, $command, $callable,
+            $options['description'], $options['usage'], $options['aliases']);
 
-		return [$commandInstance, $options];
-	}
+        return [$commandInstance, $options];
+    }
 
-	/**
-	 * Resolves command options.
-	 *
-	 * @param array $options Array of options.
-	 *
-	 * @return array Options.
-	 */
-	protected function resolveCommandOptions(array $options)
-	{
-		$resolver = new OptionsResolver();
+    /**
+     * Resolves command options.
+     *
+     * @param array $options Array of options.
+     *
+     * @return array Options.
+     */
+    protected function resolveCommandOptions(array $options)
+    {
+        $resolver = new OptionsResolver();
 
-		$resolver
-			->setDefined([
-				'description',
-				'usage',
-				'aliases',
-			])
-			->setDefaults([
-				'description' => '',
-				'usage' => '',
-				'aliases' => [],
-			]);
+        $resolver
+            ->setDefined([
+                'description',
+                'usage',
+                'aliases',
+            ])
+            ->setDefaults([
+                'description' => '',
+                'usage'       => '',
+                'aliases'     => [],
+            ]);
 
-		return $resolver->resolve($options);
-	}
+        return $resolver->resolve($options);
+    }
 
-	/**
+    /**
      * Resolves the options.
      *
      * @param array $options Array of options.
      *
      * @return array Options.
      */
-	protected function resolveCommandClientOptions(array $options)
-	{
-		$resolver = new OptionsResolver();
+    protected function resolveCommandClientOptions(array $options)
+    {
+        $resolver = new OptionsResolver();
 
-		$resolver
-			->setRequired('token')
-			->setAllowedTypes('token', 'string')
-			->setDefined([
-				'token',
-				'prefix',
-				'discordOptions',
-			])
-			->setDefaults([
-				'prefix' => '@mention ',
-				'discordOptions' => [],
-			]);
+        $resolver
+            ->setRequired('token')
+            ->setAllowedTypes('token', 'string')
+            ->setDefined([
+                'token',
+                'prefix',
+                'discordOptions',
+            ])
+            ->setDefaults([
+                'prefix'         => '@mention ',
+                'discordOptions' => [],
+            ]);
 
-		return $resolver->resolve($options);
-	}
+        return $resolver->resolve($options);
+    }
 }
