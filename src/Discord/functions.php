83c9c3d018af5a6e2021c26813362ba2ce8aa39d
David <david@team-reflex.com>
2016-07-20 22:59:50 -0400

    Applied fixes from StyleCI

--- src/Discord/functions.php
+++ src/Discord/functions.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is apart of the DiscordPHP project.
+ *
+ * Copyright (c) 2016 David Cole <david@team-reflex.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
 namespace Discord;
 
 use Discord\Parts\Channel\Channel;
@@ -11,21 +20,21 @@ use Discord\Parts\User\User;
 
 /**
  * Checks to see if a part has been mentioned.
- * 
- * @param  Part|string $part    The part or mention to look for.
- * @param  Message     $message The message to check.
+ *
+ * @param Part|string $part    The part or mention to look for.
+ * @param Message     $message The message to check.
  *
  * @return bool Whether the part was mentioned.
  */
 function mentioned($part, Message $message)
 {
-	if ($part instanceof User || $part instanceof Member) {
-		return $message->mentions->has($part->id);
-	} elseif ($part instanceof Role) {
-		return $message->mention_roles->has($part->id);
-	} elseif ($part instanceof Channel) {
-		return (strpos($message->content, "<#{$part->id}>") !== false);
-	} else {
-		return (strpos($message->content, $part) !== false);
-	}
+    if ($part instanceof User || $part instanceof Member) {
+        return $message->mentions->has($part->id);
+    } elseif ($part instanceof Role) {
+        return $message->mention_roles->has($part->id);
+    } elseif ($part instanceof Channel) {
+        return (strpos($message->content, "<#{$part->id}>") !== false);
+    } else {
+        return (strpos($message->content, $part) !== false);
+    }
 }
