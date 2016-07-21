4345fd1971436f00d97a5e2ac801e2099b14edf7
David Cole <david@team-reflex.com>
2016-07-22 11:45:12 +1200

    Added the new parameters to Application

--- src/Discord/Parts/OAuth/Application.php
+++ src/Discord/Parts/OAuth/Application.php
@@ -12,6 +12,7 @@
 namespace Discord\Parts\OAuth;
 
 use Discord\Parts\Part;
+use Discord\Parts\User\User;
 
 /**
  * The OAuth2 application of the bot.
@@ -21,13 +22,26 @@ use Discord\Parts\Part;
  * @property string $description The description of the OAuth application.
  * @property string $icon The icon hash of the OAuth application.
  * @property string $invite_url The invite URL to invite the bot to a guild.
+ * @property array[string] $rpc_origins An array of RPC origin URLs.
+ * @property int    $flags ?
+ * @property Discord\Parts\User\User $owner The owner of the OAuth application.
  */
 class Application extends Part
 {
     /**
      * {@inheritdoc}
      */
-    protected $fillable = ['id', 'name', 'description', 'icon'];
+    protected $fillable = ['id', 'name', 'description', 'icon', 'rpc_origins', 'flags', 'owner'];
+
+    /**
+     * Returns the owner of the application.
+     *
+     * @return User Owner of the application.
+     */
+    public function getOwnerAttribute()
+    {
+        return $this->factory->create(User::class, $this->attributes['owner'], true);
+    }
 
     /**
      * Returns the invite URL for the application.
