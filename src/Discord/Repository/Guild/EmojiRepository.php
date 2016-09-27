4c148c271448fba5097d06b194e2deb3b5c95113
David <david@team-reflex.com>
2016-09-27 04:25:43 +0000

    Applied fixes from StyleCI

--- src/Discord/Repository/Guild/EmojiRepository.php
+++ src/Discord/Repository/Guild/EmojiRepository.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is apart of the DiscordPHP project.
+ *
+ * Copyright (c) 2016 David Cole <david@team-reflex.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
 namespace Discord\Repository\Guild;
 
 use Discord\Parts\Guild\Emoji;
@@ -9,22 +18,22 @@ use Discord\Repository\AbstractRepository;
  * Contains emojis that belong to guilds.
  *
  * @see Discord\Parts\Guild\Emoji
- * @see Discord\Parts\Guild\Guild 
+ * @see Discord\Parts\Guild\Guild
  */
 class EmojiRepository extends AbstractRepository
 {
-	/**
-	 * {@inheritdoc}
-	 */
-	protected $endpoints = [
-		'all'    => 'guilds/:guild_id/emojis',
-		'create' => 'guilds/:guild_id/emojis',
-		'delete' => 'guilds/:guild_id/emojis/:id',
-		'update' => 'guilds/:guild_id/emojis/:id',
-	];
+    /**
+     * {@inheritdoc}
+     */
+    protected $endpoints = [
+        'all'    => 'guilds/:guild_id/emojis',
+        'create' => 'guilds/:guild_id/emojis',
+        'delete' => 'guilds/:guild_id/emojis/:id',
+        'update' => 'guilds/:guild_id/emojis/:id',
+    ];
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected $part = Emoji::class;
+    /**
+     * {@inheritdoc}
+     */
+    protected $part = Emoji::class;
 }
