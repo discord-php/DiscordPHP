3dc59e8ecf61e15f461e81c5209aca87598fa60f
Dan <eyeballkill@hotmail.com>
2017-02-19 09:02:02 +0000

    Apply fixes from StyleCI

--- src/Discord/WebSockets/Events/VoiceStateUpdate.php
+++ src/Discord/WebSockets/Events/VoiceStateUpdate.php
@@ -28,8 +28,8 @@ class VoiceStateUpdate extends Event
             if ($guild->id == $state->guild_id) {
                 foreach ($guild->channels as $channel) {
                     if ($channel->members->has($state->user_id)) {
-						$channel->members->pull($state->user_id);
-					}
+                        $channel->members->pull($state->user_id);
+                    }
 
                     if ($channel->id == $state->channel_id) {
                         $channel->members->offsetSet($state->user_id, $state);
