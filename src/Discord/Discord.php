fd320c204ccd8e791ca4c98a371ef267d1c87273
Alexander Maassen <outsider@scarynet.org>
2020-10-24 04:08:17 +0200

    applied suggestion

--- src/Discord/Discord.php
+++ src/Discord/Discord.php
@@ -296,43 +296,43 @@ class Discord
      * @array HTML Color Table.
      */
     public $colorTable = [
-        'indianred' => 'cd5c5c', 'lightcoral' => 'f08080', 'salmon' => 'fa8072', 'darksalmon' => 'e9967a',
-        'lightsalmon' => 'ffa07a', 'crimson' => 'dc143c', 'red' => 'ff0000', 'firebrick' => 'b22222',
-        'darkred' => '8b0000', 'pink' => 'ffc0cb', 'lightpink' => 'ffb6c1', 'hotpink' => 'ff69b4',
-        'deeppink' => 'ff1493', 'mediumvioletred' => 'c71585', 'palevioletred' => 'db7093',
-        'lightsalmon' => 'ffa07a', 'coral' => 'ff7f50', 'tomato' => 'ff6347', 'orangered' => 'ff4500',
-        'darkorange' => 'ff8c00', 'orange' => 'ffa500', 'gold' => 'ffd700', 'yellow' => 'ffff00',
-        'lightyellow' => 'ffffe0', 'lemonchiffon' => 'fffacd', 'lightgoldenrodyellow' => 'fafad2',
-        'papayawhip' => 'ffefd5', 'moccasin' => 'ffe4b5', 'peachpuff' => 'ffdab9', 'palegoldenrod' => 'eee8aa',
-        'khaki' => 'f0e68c', 'darkkhaki' => 'bdb76b', 'lavender' => 'e6e6fa', 'thistle' => 'd8bfd8',
-        'plum' => 'dda0dd', 'violet' => 'ee82ee', 'orchid' => 'da70d6', 'fuchsia' => 'ff00ff',
-        'magenta' => 'ff00ff', 'mediumorchid' => 'ba55d3', 'mediumpurple' => '9370db', 'rebeccapurple' => '663399',
-        'blueviolet' => '8a2be2', 'darkviolet' => '9400d3', 'darkorchid' => '9932cc', 'darkmagenta' => '8b008b',
-        'purple' => '800080', 'indigo' => '4b0082', 'slateblue' => '6a5acd', 'darkslateblue' => '483d8b',
-        'mediumslateblue' => '7b68ee', 'greenyellow' => 'adff2f', 'chartreuse' => '7fff00', 'lawngreen' => '7cfc00',
-        'lime' => '00ff00', 'limegreen' => '32cd32', 'palegreen' => '98fb98', 'lightgreen' => '90ee90',
-        'mediumspringgreen' => '00fa9a', 'springgreen' => '00ff7f', 'mediumseagreen' => '3cb371',
-        'seagreen' => '2e8b57', 'forestgreen' => '228b22', 'green' => '008000', 'darkgreen' => '006400',
-        'yellowgreen' => '9acd32', 'olivedrab' => '6b8e23', 'olive' => '808000', 'darkolivegreen' => '556b2f',
-        'mediumaquamarine' => '66cdaa', 'darkseagreen' => '8fbc8b', 'lightseagreen' => '20b2aa',
-        'darkcyan' => '008b8b', 'teal' => '008080', 'aqua' => '00ffff', 'cyan' => '00ffff', 'lightcyan' => 'e0ffff',
-        'paleturquoise' => 'afeeee', 'aquamarine' => '7fffd4', 'turquoise' => '40e0d0', 'mediumturquoise' => '48d1cc',
-        'darkturquoise' => '00ced1', 'cadetblue' => '5f9ea0', 'steelblue' => '4682b4', 'lightsteelblue' => 'b0c4de',
-        'powderblue' => 'b0e0e6', 'lightblue' => 'add8e6', 'skyblue' => '87ceeb', 'lightskyblue' => '87cefa',
-        'deepskyblue' => '00bfff', 'dodgerblue' => '1e90ff', 'cornflowerblue' => '6495ed',
-        'mediumslateblue' => '7b68ee', 'royalblue' => '4169e1', 'blue' => '0000ff', 'mediumblue' => '0000cd',
-        'darkblue' => '00008b', 'navy' => '000080', 'midnightblue' => '191970', 'cornsilk' => 'fff8dc',
-        'blanchedalmond' => 'ffebcd', 'bisque' => 'ffe4c4', 'navajowhite' => 'ffdead', 'wheat' => 'f5deb3',
-        'burlywood' => 'deb887', 'tan' => 'd2b48c', 'rosybrown' => 'bc8f8f', 'sandybrown' => 'f4a460',
-        'goldenrod' => 'daa520', 'darkgoldenrod' => 'b8860b', 'peru' => 'cd853f', 'chocolate' => 'd2691e',
-        'saddlebrown' => '8b4513', 'sienna' => 'a0522d', 'brown' => 'a52a2a', 'maroon' => '800000',
-        'white' => 'ffffff', 'snow' => 'fffafa', 'honeydew' => 'f0fff0', 'mintcream' => 'f5fffa', 'azure' => 'f0ffff',
-        'aliceblue' => 'f0f8ff', 'ghostwhite' => 'f8f8ff', 'whitesmoke' => 'f5f5f5', 'seashell' => 'fff5ee',
-        'beige' => 'f5f5dc', 'oldlace' => 'fdf5e6', 'floralwhite' => 'fffaf0', 'ivory' => 'fffff0',
-        'antiquewhite' => 'faebd7', 'linen' => 'faf0e6', 'lavenderblush' => 'fff0f5', 'mistyrose' => 'ffe4e1',
-        'gainsboro' => 'dcdcdc', 'lightgray' => 'd3d3d3', 'silver' => 'c0c0c0', 'darkgray' => 'a9a9a9',
-        'gray' => '808080', 'dimgray' => '696969', 'lightslategray' => '778899', 'slategray' => '708090',
-        'darkslategray' => '2f4f4f', 'black' => '000000'
+        'indianred' => 0xcd5c5c, 'lightcoral' => 0xf08080, 'salmon' => 0xfa8072, 'darksalmon' => 0xe9967a,
+        'lightsalmon' => 0xffa07a, 'crimson' => 0xdc143c, 'red' => 0xff0000, 'firebrick' => 0xb22222,
+        'darkred' => 0x8b0000, 'pink' => 0xffc0cb, 'lightpink' => 0xffb6c1, 'hotpink' => 0xff69b4,
+        'deeppink' => 0xff1493, 'mediumvioletred' => 0xc71585, 'palevioletred' => 0xdb7093,
+        'lightsalmon' => 0xffa07a, 'coral' => 0xff7f50, 'tomato' => 0xff6347, 'orangered' => 0xff4500,
+        'darkorange' => 0xff8c00, 'orange' => 0xffa500, 'gold' => 0xffd700, 'yellow' => 0xffff00,
+        'lightyellow' => 0xffffe0, 'lemonchiffon' => 0xfffacd, 'lightgoldenrodyellow' => 0xfafad2,
+        'papayawhip' => 0xffefd5, 'moccasin' => 0xffe4b5, 'peachpuff' => 0xffdab9, 'palegoldenrod' => 0xeee8aa,
+        'khaki' => 0xf0e68c, 'darkkhaki' => 0xbdb76b, 'lavender' => 0xe6e6fa, 'thistle' => 0xd8bfd8,
+        'plum' => 0xdda0dd, 'violet' => 0xee82ee, 'orchid' => 0xda70d6, 'fuchsia' => 0xff00ff,
+        'magenta' => 0xff00ff, 'mediumorchid' => 0xba55d3, 'mediumpurple' => 0x9370db, 'rebeccapurple' => 0x663399,
+        'blueviolet' => 0x8a2be2, 'darkviolet' => 0x9400d3, 'darkorchid' => 0x9932cc, 'darkmagenta' => 0x8b008b,
+        'purple' => 0x800080, 'indigo' => 0x4b0082, 'slateblue' => 0x6a5acd, 'darkslateblue' => 0x483d8b,
+        'mediumslateblue' => 0x7b68ee, 'greenyellow' => 0xadff2f, 'chartreuse' => 0x7fff00, 'lawngreen' => 0x7cfc00,
+        'lime' => 0x00ff00, 'limegreen' => 0x32cd32, 'palegreen' => 0x98fb98, 'lightgreen' => 0x90ee90,
+        'mediumspringgreen' => 0x00fa9a, 'springgreen' => 0x00ff7f, 'mediumseagreen' => 0x3cb371,
+        'seagreen' => 0x2e8b57, 'forestgreen' => 0x228b22, 'green' => 0x008000, 'darkgreen' => 0x006400,
+        'yellowgreen' => 0x9acd32, 'olivedrab' => 0x6b8e23, 'olive' => 0x808000, 'darkolivegreen' => 0x556b2f,
+        'mediumaquamarine' => 0x66cdaa, 'darkseagreen' => 0x8fbc8b, 'lightseagreen' => 0x20b2aa,
+        'darkcyan' => 0x008b8b, 'teal' => 0x008080, 'aqua' => 0x00ffff, 'cyan' => 0x00ffff, 'lightcyan' => 0xe0ffff,
+        'paleturquoise' => 0xafeeee, 'aquamarine' => 0x7fffd4, 'turquoise' => 0x40e0d0, 'mediumturquoise' => 0x48d1cc,
+        'darkturquoise' => 0x00ced1, 'cadetblue' => 0x5f9ea0, 'steelblue' => 0x4682b4, 'lightsteelblue' => 0xb0c4de,
+        'powderblue' => 0xb0e0e6, 'lightblue' => 0xadd8e6, 'skyblue' => 0x87ceeb, 'lightskyblue' => 0x87cefa,
+        'deepskyblue' => 0x00bfff, 'dodgerblue' => 0x1e90ff, 'cornflowerblue' => 0x6495ed,
+        'mediumslateblue' => 0x7b68ee, 'royalblue' => 0x4169e1, 'blue' => 0x0000ff, 'mediumblue' => 0x0000cd,
+        'darkblue' => 0x00008b, 'navy' => 0x000080, 'midnightblue' => 0x191970, 'cornsilk' => 0xfff8dc,
+        'blanchedalmond' => 0xffebcd, 'bisque' => 0xffe4c4, 'navajowhite' => 0xffdead, 'wheat' => 0xf5deb3,
+        'burlywood' => 0xdeb887, 'tan' => 0xd2b48c, 'rosybrown' => 0xbc8f8f, 'sandybrown' => 0xf4a460,
+        'goldenrod' => 0xdaa520, 'darkgoldenrod' => 0xb8860b, 'peru' => 0xcd853f, 'chocolate' => 0xd2691e,
+        'saddlebrown' => 0x8b4513, 'sienna' => 0xa0522d, 'brown' => 0xa52a2a, 'maroon' => 0x800000,
+        'white' => 0xffffff, 'snow' => 0xfffafa, 'honeydew' => 0xf0fff0, 'mintcream' => 0xf5fffa, 'azure' => 0xf0ffff,
+        'aliceblue' => 0xf0f8ff, 'ghostwhite' => 0xf8f8ff, 'whitesmoke' => 0xf5f5f5, 'seashell' => 0xfff5ee,
+        'beige' => 0xf5f5dc, 'oldlace' => 0xfdf5e6, 'floralwhite' => 0xfffaf0, 'ivory' => 0xfffff0,
+        'antiquewhite' => 0xfaebd7, 'linen' => 0xfaf0e6, 'lavenderblush' => 0xfff0f5, 'mistyrose' => 0xffe4e1,
+        'gainsboro' => 0xdcdcdc, 'lightgray' => 0xd3d3d3, 'silver' => 0xc0c0c0, 'darkgray' => 0xa9a9a9,
+        'gray' => 0x808080, 'dimgray' => 0x696969, 'lightslategray' => 0x778899, 'slategray' => 0x708090,
+        'darkslategray' => 0x2f4f4f, 'black' => 0x000000
     ];
 
     /**
@@ -1453,7 +1453,7 @@ class Discord
         if (preg_match('/^([a-z]+)$/ui', $color, $match)) {
             $colorName = str_tolower($match[1]);
             if(isset($this->colorTable[$colorname])) {
-                return hexdec($this->colorTable[$colorName]);
+                return $this->colorTable[$colorName];
             }
         }
 
