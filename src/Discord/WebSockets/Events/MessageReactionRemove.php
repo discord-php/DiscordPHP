9044a76a8b15f3ca21f992b3effb5fdcd1604ef2
David Cole <david.cole1340@gmail.com>
2020-11-20 09:37:34 +1300

    MessageReactionRemove: update message reactions #429

--- src/Discord/WebSockets/Events/MessageReactionRemove.php
+++ src/Discord/WebSockets/Events/MessageReactionRemove.php
@@ -22,7 +22,37 @@ class MessageReactionRemove extends Event
      */
     public function handle(Deferred &$deferred, $data): void
     {
-        $reaction = $this->factory->create(MessageReaction::class, $data, true);
+        $reaction = new MessageReaction($this->discord, (array) $data, true);
         $deferred->resolve($reaction);
+
+        if ($guild = $reaction->guild) {
+            if ($channel = $reaction->channel) {
+                if ($message = $channel->messages->offsetGet($reaction->message_id)) {
+                    $reactions = [];
+                    $rawReactions = $message->getRawAttributes()['reactions'] ?? [];
+
+                    foreach ($rawReactions as $react) {
+                        if ($react['emoji']['name'] == $reaction->emoji->name) {
+                            --$react['count'];
+
+                            if ($reaction->user_id == $this->discord->id) {
+                                $react['me'] = false;
+                            }
+                        }
+
+                        if ($react['count'] > 0) {
+                            $reactions[] = $react;
+                        }
+                    }
+
+                    $message->reactions = $reactions;
+                    $channel->messages->push($message);
+                }
+
+                $guild->channels->push($channel);
+            }
+
+            $this->discord->guilds->push($guild);
+        }
     }
 }
