210528840f64e2c574e05709c11ee5ab51acf7de
Valithor Obsidion <valzargaming@gmail.com>
2020-12-30 12:25:32 -0500

    x86/x64 support

--- src/Discord/Parts/Guild/Guild.php
+++ src/Discord/Parts/Guild/Guild.php
@@ -517,6 +517,25 @@ class Guild extends Part
      */
     public function createdTimestamp()
     {
-	    return ( ( (int) $this->id / 4194304 ) + 1420070400000 );
+	    if (\PHP_INT_SIZE === 4) { //x86
+            $binary = \str_pad(\base_convert($this->id, 10, 2), 64, 0, \STR_PAD_LEFT);
+            $time = \base_convert(\substr($binary, 0, 42), 2, 10);
+            $timestamp = (float) ((((int) \substr($time, 0, -3)) + 1420070400).'.'.\substr($time, -3));
+            $workerID = (int) \base_convert(\substr($binary, 42, 5), 2, 10);
+            $processID = (int) \base_convert(\substr($binary, 47, 5), 2, 10);
+            $increment = (int) \base_convert(\substr($binary, 52, 12), 2, 10);
+        } else { //x64
+            $snowflake = (int) $this->id;
+            $time = (string) ($snowflake >> 22);
+            $timestamp = (float) ((((int) \substr($time, 0, -3)) + 1420070400).'.'.\substr($time, -3));
+            $workerID = ($snowflake & 0x3E0000) >> 17;
+            $processID = ($snowflake & 0x1F000) >> 12;
+            $increment = ($snowflake & 0xFFF);
+        }
+        if ($timestamp < 1420070400 || $workerID < 0 || $workerID >= 32 || $processID < 0 || $processID >= 32 || $increment < 0 || $increment >= 4096) {
+            return null;
+        }
+
+        return $timestamp;
     }
 }
