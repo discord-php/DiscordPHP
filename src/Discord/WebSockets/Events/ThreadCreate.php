d309e5039427a99e0a00f64e5ff688abb011e6e4
David Cole <david.cole1340@gmail.com>
2021-06-14 21:18:50 +1200

    Added most thread events

---
+++ src/Discord/WebSockets/Events/ThreadCreate.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\WebSockets\Events;
+
+use Discord\Helpers\Deferred;
+use Discord\Parts\Thread\Member;
+use Discord\Parts\Thread\Thread;
+use Discord\WebSockets\Event;
+
+class ThreadCreate extends Event
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function handle(Deferred &$deferred, $data)
+    {
+        /** @var Thread */
+        $thread = $this->factory->create(Thread::class, $data, true);
+
+        // Ignore threads that have already been added
+        if ($guild = $thread->guild) {
+            if ($guild->threads->get('id', $thread->id)) {
+                return;
+            }
+        }
+
+        foreach ($data->members as $member) {
+            $member = $this->factory->create(Member::class, $member, true);
+            $thread->members->push($member);
+        }
+
+        if ($guild = $thread->guild) {
+            $guild->threads->push($thread);
+        }
+        
+        $deferred->resolve($thread);
+    }
+}
---
+++ src/Discord/WebSockets/Events/ThreadDelete.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\WebSockets\Events;
+
+use Discord\Helpers\Deferred;
+use Discord\WebSockets\Event;
+
+class ThreadDelete extends Event
+{
+    public function handle(Deferred &$deferred, $data)
+    {
+        $thread = null;
+
+        if ($guild = $this->discord->guilds->get('id', $data->guild_id)) {
+            $thread = $guild->threads->pull($data->id);
+        }
+
+        $deferred->resolve($thread);
+    }
+}
