76ae9a7f55bb955d3bfa2e5adcc577fe5963bf6f
David Cole <david.cole1340@gmail.com>
2021-11-18 12:06:57 +1300

    Update src/Discord/Parts/User/Member.php

--- src/Discord/Parts/User/Member.php
+++ src/Discord/Parts/User/Member.php
@@ -402,7 +402,7 @@ class Member extends Part
     public function getAvatarAttribute(string $format = 'jpg', int $size = 1024): ?string
     {
         if (is_null($this->attributes['avatar'])) {
-            return '';
+            return null;
         }
 
         if (false === array_search($format, ['png', 'jpg', 'webp', 'gif'])) {
