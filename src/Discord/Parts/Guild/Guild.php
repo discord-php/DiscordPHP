c4de9d79dd26b1bd6fe1f33ec97afc531809678b
Valithor Obsidion <valzargaming@gmail.com>
2021-12-23 11:32:38 -0500

    Default to webp instead of jpg

--- src/Discord/Parts/Guild/Guild.php
+++ src/Discord/Parts/Guild/Guild.php
@@ -284,7 +284,7 @@ class Guild extends Part
      *
      * @return string|null The URL to the guild icon or null.
      */
-    public function getIconAttribute(string $format = 'jpg', int $size = 1024)
+    public function getIconAttribute(string $format = 'webp', int $size = 1024)
     {
         if (is_null($this->attributes['icon'])) {
             return null;
@@ -293,7 +293,7 @@ class Guild extends Part
         $allowed = ['png', 'jpg', 'webp'];
 
         if (! in_array(strtolower($format), $allowed)) {
-            $format = 'jpg';
+            $format = 'webp';
         }
 
         return "https://cdn.discordapp.com/icons/{$this->id}/{$this->attributes['icon']}.{$format}?size={$size}";
@@ -317,7 +317,7 @@ class Guild extends Part
      *
      * @return string|null The URL to the guild splash or null.
      */
-    public function getSplashAttribute(string $format = 'jpg', int $size = 2048)
+    public function getSplashAttribute(string $format = 'webp', int $size = 2048)
     {
         if (is_null($this->attributes['splash'])) {
             return null;
@@ -326,7 +326,7 @@ class Guild extends Part
         $allowed = ['png', 'jpg', 'webp'];
 
 		if (! in_array(strtolower($format), $allowed)) {
-            $format = 'jpg';
+            $format = 'webp';
         }
 
         return "https://cdn.discordapp.com/splashes/{$this->id}/{$this->attributes['splash']}.{$format}?size={$size}";
