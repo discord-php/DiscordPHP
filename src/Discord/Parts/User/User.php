ee40f31f71378689df14dc0b92719b5423e2291f
Valithor Obsidion <valzargaming@gmail.com>
2021-12-23 10:37:47 -0500

    Fixes typo

--- src/Discord/Parts/User/User.php
+++ src/Discord/Parts/User/User.php
@@ -173,7 +173,7 @@ class User extends Part
 
         $allowed = ['png', 'jpg', 'webp', 'gif'];
 	
-		if (in_array(strtolower($format), $allowed)) {
+		if (! in_array(strtolower($format), $allowed)) {
             $format = 'webp';
         }
 
