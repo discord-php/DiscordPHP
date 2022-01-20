ce4e9d77265214a51a504d574536aa01e13eb982
SQKo <87897282+SQKo@users.noreply.github.com>
2022-01-20 19:52:51 +0700

    update header and license year

--- src/Discord/Helpers/RegisteredCommand.php
+++ src/Discord/Helpers/RegisteredCommand.php
@@ -1,12 +1,12 @@
 <?php
 
 /*
- * This file was a part of the DiscordPHP-Slash project.
+ * This file is a part of the DiscordPHP project.
  *
- * Copyright (c) 2021 David Cole <david.cole1340@gmail.com>
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
  *
- * This source file is subject to the MIT license which is
- * bundled with this source code in the LICENSE.md file.
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
  */
 
 namespace Discord\Helpers;
--- src/Discord/Parts/Interactions/Command/Option.php
+++ src/Discord/Parts/Interactions/Command/Option.php
@@ -3,10 +3,10 @@
 /*
  * This file is a part of the DiscordPHP project.
  *
- * Copyright (c) 2021 David Cole <david.cole1340@gmail.com>
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
  *
- * This source file is subject to the MIT license which is
- * bundled with this source code in the LICENSE.md file.
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
  */
 
 namespace Discord\Parts\Interactions\Command;
