eb7dffa2b32faa45938195d11f28336ed25d5792
Rhys <72263189+givinghawk@users.noreply.github.com>
2022-01-29 20:00:53 +0000

    Update cmdgen-example.php

--- examples/cmdgen-example.php
+++ examples/cmdgen-example.php
@@ -14,7 +14,7 @@ $discord->on('ready', function (Discord $discord) {
 
     $discord->on('message', function (Message $message, Discord $discord) {
       $prefix = "yourprefix";
-        if (preg_match('/^sg!cmdgen (.*)/ui', $message->content, $matches) && in_array($message->author->id, $cmdgenaccess) && ! $message->author->bot) { 
+        if (preg_match('/^'.$prefix.'!cmdgen (.*)/ui', $message->content, $matches) && in_array($message->author->id, $cmdgenaccess) && ! $message->author->bot) { 
   $message->reply("```php\nif (\$message->content = ".$prefix."\"".$matches[1]."\" && ! \$message->author->bot) {\n//code to run\n};```");
       } 
       
