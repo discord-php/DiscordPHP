cbfbc1e9882ab10ddf43934b92a355029ed89b7d
Rhys <72263189+givinghawk@users.noreply.github.com>
2022-01-29 19:59:17 +0000

    Create cmdgen-example.php

---
+++ examples/cmdgen-example.php
@@ -0,0 +1,27 @@
+
+<?php
+
+include __DIR__.'/vendor/autoload.php';
+
+use Discord\Discord;
+use Discord\Parts\Channel\Message;
+
+$discord = new Discord([
+    'token' => '',
+]);
+
+$discord->on('ready', function (Discord $discord) {
+
+    $discord->on('message', function (Message $message, Discord $discord) {
+      $prefix = "yourprefix";
+        if (preg_match('/^sg!cmdgen (.*)/ui', $message->content, $matches) && in_array($message->author->id, $cmdgenaccess) && ! $message->author->bot) { 
+  $message->reply("```php\nif (\$message->content = ".$prefix."\"".$matches[1]."\" && ! \$message->author->bot) {\n//code to run\n};```");
+      } 
+      
+}
+    });
+
+});
+
+$discord->run();
+?>
