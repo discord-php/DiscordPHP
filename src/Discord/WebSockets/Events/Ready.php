2b254007a343a6589e064d2ae6d29bdfa59dc70f
SQKo <87897282+SQKo@users.noreply.github.com>
2022-03-30 13:06:30 +0700

    Add Event::READY handler

---
+++ src/Discord/WebSockets/Events/Ready.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\WebSockets\Events;
+
+use Discord\WebSockets\Event;
+use Discord\Helpers\Deferred;
+
+/**
+ * @see https://discord.com/developers/docs/topics/gateway#ready
+ */
+class Ready extends Event
+{
+    /**
+     * @inheritdoc
+     */
+    public function handle(Deferred &$deferred, $data): void
+    {
+        $deferred->resolve($data);
+    }
+}
