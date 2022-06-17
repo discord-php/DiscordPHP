212e91178761cbc597b1f3bb44a0c20538075b90
SQKo <87897282+SQKo@users.noreply.github.com>
2022-06-18 02:18:33 +0700

    Push to cache if channel is a DM

--- src/Discord/WebSockets/Events/MessageCreate.php
+++ src/Discord/WebSockets/Events/MessageCreate.php
@@ -30,7 +30,7 @@ class MessageCreate extends Event
         $messagePart = $this->factory->create(Message::class, $data, true);
 
         // assume it is a private channel
-        if (! isset($data->channel, $messagePart->guild)) {
+        if (! $messagePart->guild && $messagePart->channel->type == Channel::TYPE_DM) {
             /** @var Channel */
             $channel = $this->factory->create(Channel::class, [
                 'id' => $messagePart->channel_id,
