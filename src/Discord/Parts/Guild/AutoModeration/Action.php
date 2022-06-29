a98d5499c07e1afe8aafed83c380035b97355d94
SQKo <87897282+SQKo@users.noreply.github.com>
2022-06-29 13:42:43 +0700

    Add Parts, update dependency

---
+++ src/Discord/Parts/Guild/AutoModeration/Action.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Parts\Guild\AutoModeration;
+
+use Discord\Parts\Part;
+
+/**
+ * An action which will execute whenever a rule is triggered.
+ *
+ * @see https://discord.com/developers/docs/resources/auto-moderation#auto-moderation-action-object
+ *
+ * @property int     $type     The type of action.
+ * @property ?object $metadata Additional metadata needed during execution for this specific action type (contains `channel_id` and `duration_seconds`).
+ */
+class Action extends Part
+{
+    /**
+     * @inheritdoc
+     */
+    protected $fillable = [
+        'type',
+        'metadata',
+    ];
+
+    public const TYPE_BLOCK_MESSAGE = 1;
+    public const TYPE_SEND_ALERT_MESSAGE = 2;
+    public const TYPE_TIMEOUT = 3;
+}
