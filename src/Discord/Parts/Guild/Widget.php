b15e21c0a7b82652bf813153d2c9956722e8cd8c
SQKo <87897282+SQKo@users.noreply.github.com>
2022-07-13 15:05:03 +0700

    add style phpdoc

--- src/Discord/Parts/Guild/Widget.php
+++ src/Discord/Parts/Guild/Widget.php
@@ -44,10 +44,19 @@ class Widget extends Part
         'presence_count',
     ];
 
+    /** shield style widget with Discord icon and guild members online count */
     public const STYLE_SHIELD = 'shield';
+
+    /** large image with guild icon, name and online count. "POWERED BY DISCORD" as the footer of the widget */
     public const STYLE_BANNER1 = 'banner1';
+
+    /** smaller widget style with guild icon, name and online count. Split on the right with Discord logo */
     public const STYLE_BANNER2 = 'banner2';
+
+    /** large image with guild icon, name and online count. In the footer, Discord logo on the left and "Chat Now" on the right */
     public const STYLE_BANNER3 = 'banner3';
+
+    /** large Discord logo at the top of the widget. Guild icon, name and online count in the middle portion of the widget and a "JOIN MY SERVER" button at the bottom */
     public const STYLE_BANNER4 = 'banner4';
 
     public const STYLE = [
