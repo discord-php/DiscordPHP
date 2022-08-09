bcf9c06606c730bc7bd535de272034d51e513af1
SQKo <87897282+SQKo@users.noreply.github.com>
2022-08-09 22:45:20 +0700

    little fix WelcomeChannel

--- src/Discord/Parts/Guild/WelcomeChannel.php
+++ src/Discord/Parts/Guild/WelcomeChannel.php
@@ -22,15 +22,20 @@ use Discord\Parts\Part;
  * @property string       $channel_id  The channel's id.
  * @property Channel|null $channel     The channel object.
  * @property string       $description The description shown for the channel.
- * @property string|null  $emoji_id    The emoji id, if the emoji is custom.
- * @property string|null  $emoji_name  The emoji name if custom, the unicode character if standard, or null if no emoji is set.
+ * @property ?string      $emoji_id    The emoji id, if the emoji is custom.
+ * @property ?string      $emoji_name  The emoji name if custom, the unicode character if standard, or null if no emoji is set.
  */
 class WelcomeChannel extends Part
 {
     /**
      * @inheritdoc
      */
-    protected $fillable = ['channel_id', 'description', 'emoji_id', 'emoji_name'];
+    protected $fillable = [
+        'channel_id',
+        'description',
+        'emoji_id',
+        'emoji_name'
+    ];
 
     /**
      * Returns the Channel object for the Welcome Screen.
