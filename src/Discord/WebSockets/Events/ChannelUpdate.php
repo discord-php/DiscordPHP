c60552a244e2935cdfa3781f8c6e3015e9733d19
SQKo <87897282+SQKo@users.noreply.github.com>
2022-08-13 22:36:13 +0700

    Event ChannelUpdate cache code

--- src/Discord/WebSockets/Events/ChannelUpdate.php
+++ src/Discord/WebSockets/Events/ChannelUpdate.php
@@ -15,6 +15,8 @@ use Discord\Parts\Channel\Channel;
 use Discord\WebSockets\Event;
 use Discord\Helpers\Deferred;
 
+use function React\Async\coroutine;
+
 /**
  * @see https://discord.com/developers/docs/topics/gateway#channel-update
  */
@@ -25,29 +27,35 @@ class ChannelUpdate extends Event
      */
     public function handle(Deferred &$deferred, $data): void
     {
-        $oldChannel = null;
+        coroutine(function ($data) {
+            $oldChannel = $promise = null;
 
-        /** @var Channel */
-        $channelPart = $this->factory->create(Channel::class, $data, true);
+            /** @var Channel */
+            $channelPart = $this->factory->create(Channel::class, $data, true);
 
-        if ($channelPart->is_private) {
-            if (! $oldChannel = $this->discord->private_channels->get('id', $data->id)) {
-                $this->discord->private_channels->pushItem($channelPart);
+            if ($channelPart->is_private) {
+                if (! $oldChannel = $this->discord->private_channels[$data->id]) {
+                    $promise = $this->discord->private_channels->cache->set($data->id, $channelPart);
+                }
+            } elseif ($guild = $channelPart->guild) {
+                if (! $oldChannel = $guild->channels[$data->id]) {
+                    $promise = $guild->channels->cache->set($data->id, $channelPart);
+                }
             }
-        } elseif ($guild = $channelPart->guild) {
-            if (! $oldChannel = $guild->channels->get('id', $data->id)) {
-                $guild->channels->pushItem($channelPart);
+
+            if ($oldChannel) {
+                // Swap
+                $channelPart = $oldChannel;
+                $oldChannel = clone $oldChannel;
+
+                $channelPart->fill((array) $data);
             }
-        }
 
-        if ($oldChannel) {
-            // Swap
-            $channelPart = $oldChannel;
-            $oldChannel = clone $oldChannel;
+            if ($promise) {
+                yield $promise;
+            }
 
-            $channelPart->fill((array) $data);
-        }
-
-        $deferred->resolve([$channelPart, $oldChannel]);
+            return [$channelPart, $oldChannel];
+        }, $data)->then([$deferred, 'resolve']);
     }
 }
