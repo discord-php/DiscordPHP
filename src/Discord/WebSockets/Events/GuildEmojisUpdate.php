2f37a683c1c0d885df11584a776ee620a0861790
SQKo <87897282+SQKo@users.noreply.github.com>
2022-08-14 16:17:49 +0700

    Update Event GuildEmojisUpdate cache code

--- src/Discord/WebSockets/Events/GuildEmojisUpdate.php
+++ src/Discord/WebSockets/Events/GuildEmojisUpdate.php
@@ -15,6 +15,9 @@ use Discord\Helpers\Collection;
 use Discord\WebSockets\Event;
 use Discord\Helpers\Deferred;
 use Discord\Parts\Guild\Emoji;
+use Discord\Parts\Guild\Guild;
+
+use function React\Async\coroutine;
 
 /**
  * @see https://discord.com/developers/docs/topics/gateway#guild-emojis-update
@@ -26,30 +29,33 @@ class GuildEmojisUpdate extends Event
      */
     public function handle(Deferred &$deferred, $data): void
     {
-        $oldEmojis = Collection::for(Emoji::class);
-        $emojiParts = Collection::for(Emoji::class);
+        coroutine(function ($data) {
+            $oldEmojis = Collection::for(Emoji::class);
+            $emojiParts = Collection::for(Emoji::class);
 
-        if ($guild = $this->discord->guilds->get('id', $data->guild_id)) {
-            $oldEmojis->merge($guild->emojis);
-            $guild->emojis->clear();
-        }
-
-        foreach ($data->emojis as $emoji) {
-            if (isset($emoji->user)) {
-                // User caching from emoji uploader
-                $this->cacheUser($emoji->user);
-            } elseif ($oldEmoji = $oldEmojis->offsetGet($emoji->id)) {
-                $emoji->user = $oldEmoji->user;
+            /** @var ?Guild */
+            if ($guild = yield $this->discord->guilds->cacheGet($data->guild_id)) {
+                // @todo load all emojis from cache?
+                $oldEmojis->merge($guild->emojis);
             }
-            /** @var Emoji */
-            $emojiPart = $this->factory->create(Emoji::class, $emoji, true);
-            $emojiParts->pushItem($emojiPart);
-        }
 
-        if ($guild) {
-            $guild->emojis->merge($emojiParts);
-        }
+            foreach ($data->emojis as &$emoji) {
+                if (isset($emoji->user)) {
+                    // User caching from emoji uploader
+                    $this->cacheUser($emoji->user);
+                } elseif ($oldEmoji = $oldEmojis->offsetGet($emoji->id)) {
+                    if ($uploader = $oldEmoji->user) {
+                        $emoji->user = (object) $uploader->getRawAttributes();
+                    }
+                }
+                $emojiParts->pushItem($this->factory->create(Emoji::class, $emoji, true));
+            }
 
-        $deferred->resolve([$emojiParts, $oldEmojis]);
+            if ($guild) {
+                yield $guild->emojis->cache->setMultiple($emojiParts->toArray());
+            }
+
+            return [$emojiParts, $oldEmojis];
+        }, $data)->then([$deferred, 'resolve']);
     }
 }
