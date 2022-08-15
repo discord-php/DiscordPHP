94fca01d95fa71cb4711d1fee0fc6375f6fc90b3
SQKo <87897282+SQKo@users.noreply.github.com>
2022-08-15 20:58:36 +0700

    test: reactphp cache filesystem

--- tests/DiscordSingleton.php
+++ tests/DiscordSingleton.php
@@ -38,7 +38,9 @@ class DiscordSingleton
         //$redis = (new Clue\React\Redis\Factory($loop))->createLazyClient('localhost:6379');
         //$cache = new WyriHaximus\React\Cache\Redis($redis);
 
-        $cache = new seregazhuk\React\Cache\Memcached\Memcached($loop);
+        //$cache = new seregazhuk\React\Cache\Memcached\Memcached($loop);
+
+        $cache = new WyriHaximus\React\Cache\Filesystem(React\Filesystem\Filesystem::create($loop), getenv('RUNNER_TEMP'));
 
         $logger = new Logger('DiscordPHP-UnitTests');
         $handler = new StreamHandler(fopen(__DIR__.'/../phpunit.log', 'w'));
