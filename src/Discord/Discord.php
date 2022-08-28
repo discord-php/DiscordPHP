123555f400ce1fec0072dfcd6556666a6eee2167
Alexander Maassen <outsider@scarynet.org>
2022-08-28 13:49:59 +0200

    rephrased the notification a little

--- src/Discord/Discord.php
+++ src/Discord/Discord.php
@@ -1302,6 +1302,9 @@ class Discord
 
         if (is_null($gateway)) {
             $this->http->get(Endpoint::GATEWAY_BOT)->done(function ($response) use ($buildParams) {
+                if ($response->shards > 1) {
+                    $this->logger->warning('Please contact the DiscordPHP devs at https://discord.gg/F8nqn7VqR5 or https://github.com/discord-php/DiscordPHP/issues if you are interrested in assisting us with sharding support development.');
+                }
                 $buildParams($this->resume_gateway_url ?? $response->url, $response->session_start_limit);
             }, function ($e) use ($buildParams) {
                 // Can't access the API server so we will use the default gateway.
