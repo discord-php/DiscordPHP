c03f213df94ad496feeaf7ab8c0d9fbc88def11c
Valithor Obsidion <valzargaming@gmail.com>
2022-08-31 13:26:09 -0400

    Update Discord.php

--- src/Discord/Discord.php
+++ src/Discord/Discord.php
@@ -1374,6 +1374,7 @@ class Discord
                 'dnsConfig',
                 'cacheInterface',
                 'cacheSweep',
+                'cacheInterface',
             ])
             ->setDefaults([
                 'logger' => null,
