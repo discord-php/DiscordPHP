a13fed82aad2cb495510f2d2f2a80d8015b27e43
Alexander Maassen <outsider@scarynet.org>
2022-09-10 05:55:59 +0200

    $data->author not set during MESSAGE_UPDATE
    PHP Warning:  Undefined property: stdClass::$author in /home/.../vendor/team-reflex/discord-php/src/Discord/WebSockets/Events/MessageUpdate.php on line 53
    PHP Warning:  Attempt to read property "id" on null in /home/.../vendor/team-reflex/discord-php/src/Discord/WebSockets/Events/MessageUpdate.php on line 53

--- src/Discord/WebSockets/Events/MessageUpdate.php
+++ src/Discord/WebSockets/Events/MessageUpdate.php
@@ -50,7 +50,7 @@ class MessageUpdate extends Event
                 $messagePart->fill((array) $data);
 
                 // Deal with empty message content intent
-                if (! ($this->discord->options['intents'] & Intents::MESSAGE_CONTENT) && $data->author->id != $this->discord->id) {
+                if (! ($this->discord->options['intents'] & Intents::MESSAGE_CONTENT) && $messagePart->author->id != $this->discord->id) {
                     $cacheMessagePart = clone $oldMessagePart;
                     // Ignore intent required fields
                     $cacheMessagePart->fill(array_filter((array) $data, fn ($value, $key) => ! in_array($key, ['content', 'embeds', 'attachments', 'components']), ARRAY_FILTER_USE_BOTH));
