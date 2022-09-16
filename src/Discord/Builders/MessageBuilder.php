2b4517ee2be552b5a4b8b3cdc1e954e9e25c5001
SQKo <87897282+SQKo@users.noreply.github.com>
2022-09-16 19:04:10 +0700

    allow components only message

--- src/Discord/Builders/MessageBuilder.php
+++ src/Discord/Builders/MessageBuilder.php
@@ -657,6 +657,7 @@ class MessageBuilder implements JsonSerializable
 
         if (isset($this->components)) {
             $body['components'] = $this->components;
+            $empty = false;
         }
 
         if ($this->sticker_ids) {
