8503796f8cf507e6f8da61cc7ed8d9624e497fa9
Alexander Maassen <outsider@scarynet.org>
2022-11-13 04:20:09 +0100

    Update VoiceClient.php

--- src/Discord/Voice/VoiceClient.php
+++ src/Discord/Voice/VoiceClient.php
@@ -1514,7 +1514,7 @@ class VoiceClient extends EventEmitter
     private function insertSilence(): void
     {
         // https://discord.com/developers/docs/topics/voice-connections#voice-data-interpolation
-        while ($this->silenceRemaining-- > 0) {
+        while (--$this->silenceRemaining > 0) {
             $this->sendBuffer(self::SILENCE_FRAME);
         }
     }
