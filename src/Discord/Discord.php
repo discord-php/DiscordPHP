74b75b43ad2e3ee43aa4b9df51fb0c8528f7fc68
SQKo <87897282+SQKo@users.noreply.github.com>
2022-12-14 22:25:11 +0700

    Replace zlib-stream implementation (#1023)
    * replace clue zlib stream
    
    * trailing comma fix
    
    * move compress params after check

--- src/Discord/Discord.php
+++ src/Discord/Discord.php
@@ -289,7 +289,7 @@ class Discord
     /**
      * zlib decompressor.
      *
-     * @var \Clue\React\Zlib\Decompressor
+     * @var \InflateContext|false
      */
     protected $zlibDecompressor;
 
@@ -631,12 +631,8 @@ class Discord
                     return;
                 }
 
-                $this->zlibDecompressor->once('data', function ($data) {
-                    $this->processWsMessage($data);
-                    $this->payloadBuffer = '';
-                });
-
-                $this->zlibDecompressor->write($this->payloadBuffer);
+                $this->processWsMessage(inflate_add($this->zlibDecompressor, $this->payloadBuffer));
+                $this->payloadBuffer = '';
             } else {
                 $this->processWsMessage(zlib_decode($payload));
             }
@@ -1322,9 +1318,7 @@ class Discord
                 'encoding' => $this->encoding,
             ];
 
-            if (class_exists('\Clue\React\Zlib\Decompressor')) {
-                $this->logger->warning('The `clue/zlib-react` is installed, Enabling experimental zlib-stream compressed gateway message.');
-                $this->zlibDecompressor = new \Clue\React\Zlib\Decompressor(ZLIB_ENCODING_DEFLATE);
+            if ($this->zlibDecompressor = inflate_init(ZLIB_ENCODING_DEFLATE)) {
                 $params['compress'] = 'zlib-stream';
             }
 
