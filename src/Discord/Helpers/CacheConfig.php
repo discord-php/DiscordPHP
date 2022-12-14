1494c9fd996c535c6455bb55544ff507df38d606
SQKo <87897282+SQKo@users.noreply.github.com>
2022-12-14 21:35:25 +0700

    react cache supports float ttl

--- src/Discord/Helpers/CacheConfig.php
+++ src/Discord/Helpers/CacheConfig.php
@@ -49,9 +49,9 @@ class CacheConfig
     protected string $separator = '.';
 
     /**
-     * The Time To Live for `$interface::set()` and `$interface::setMultiple()`.
+     * The default Time To Live for `$interface::set()` and `$interface::setMultiple()`.
      *
-     * @var null|int|\DateInterval
+     * @var null|int|\DateInterval|float
      */
     public $ttl;
 
@@ -60,7 +60,7 @@ class CacheConfig
      * @param bool                                                        $compress  Whether to compress cache data before serialization, ignored in ArrayCache.
      * @param bool                                                        $sweep     Whether to automatically sweep cache.
      * @param string|null                                                 $separator The cache key prefix separator, null for default.
-     * @param null|int|\DateInterval                                      $ttl       The cache time to live value to pass to the interface.
+     * @param null|int|\DateInterval|float                                $ttl       The cache time to live default value to pass to the interface.
      */
     public function __construct($interface, bool $compress = false, bool $sweep = false, ?string $separator = null, $ttl = null)
     {
