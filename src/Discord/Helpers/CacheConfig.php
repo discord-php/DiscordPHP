2f276c0b59129f1c784663a10acf7e58bd4ee503
SQKo <87897282+SQKo@users.noreply.github.com>
2022-12-15 19:05:33 +0700

    fix condition

--- src/Discord/Helpers/CacheConfig.php
+++ src/Discord/Helpers/CacheConfig.php
@@ -67,7 +67,7 @@ class CacheConfig
         $this->interface = $interface;
         $this->sweep = $sweep;
         $interfaceName = get_class($interface);
-        if ($this->compress = $compress && stripos($interfaceName, 'Symfony') !== false) {
+        if (($this->compress = $compress) && stripos($interfaceName, 'Symfony') !== false) {
             trigger_error('Symfony cache is not compatible with option `$compress = true`, Use the DeflateMarshaller to compress data!');
         }
         if (null === $separator) {
