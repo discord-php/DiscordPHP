51f940a94532dd8d1497ec7ed0fa88fb2a375eb2
Alexander Maassen <outsider@scarynet.org>
2023-01-20 20:31:52 +0100

    codacy is_null

--- src/Discord/DiscordCommandClient.php
+++ src/Discord/DiscordCommandClient.php
@@ -113,7 +113,7 @@ class DiscordCommandClient extends Discord
                         $commandString = array_shift($args);
                         $newCommand = $command->getCommand($commandString);
 
-                        if (is_null($newCommand)) {
+                        if ($newCommand === null) {
                             return "The command {$commandString} does not exist.";
                         }
 
