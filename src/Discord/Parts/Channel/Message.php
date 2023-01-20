ee5408eee7574c3f425a4db039e15c70748d942e
Alexander Maassen <outsider@scarynet.org>
2023-01-20 20:24:44 +0100

    codacy is_null

--- src/Discord/Parts/Channel/Message.php
+++ src/Discord/Parts/Channel/Message.php
@@ -1050,7 +1050,7 @@ class Message extends Part
                     $this->discord->removeListener(Event::MESSAGE_REACTION_ADD, $eventHandler);
                     $deferred->resolve($reactions);
 
-                    if (! is_null($timer)) {
+                    if ($timer !== null) {
                         $this->discord->getLoop()->cancelTimer($timer);
                     }
                 }
