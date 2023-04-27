019db22a4216ce3ca123b82c8b8900519cfee2ac
SQKo <87897282+SQKo@users.noreply.github.com>
2023-04-27 13:50:10 +0700

    Forum Tag::emoji_id nullable (#1098)

--- src/Discord/Parts/Channel/Forum/Tag.php
+++ src/Discord/Parts/Channel/Forum/Tag.php
@@ -16,6 +16,7 @@ use Discord\Parts\Part;
 /**
  * An object that represents a tag that is able to be applied to a thread in a
  * `GUILD_FORUM` channel.
+ * At most one of `emoji_id` and `emoji_name` may be set to a non-null value.
  *
  * @link https://discord.com/developers/docs/resources/channel#forum-tag-object
  *
@@ -24,7 +25,7 @@ use Discord\Parts\Part;
  * @property string      $id         The id of the tag.
  * @property string      $name       The name of the tag (0-20 characters).
  * @property bool        $moderated  Whether this tag can only be added to or removed from threads by a member with the `MANAGE_THREADS` permission.
- * @property string      $emoji_id   The id of a guild's custom emoji.
+ * @property string|null $emoji_id   The id of a guild's custom emoji.
  * @property string|null $emoji_name The unicode character of the emoji.
  */
 class Tag extends Part
