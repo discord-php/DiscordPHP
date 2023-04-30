1740ae4a36bc8d8386e55dd003a423a6464702d2
Exanlv <exan@landviz.nl>
2023-04-30 16:40:22 +0200

    use switch instead of several elseif

--- src/Discord/Parts/Channel/Channel.php
+++ src/Discord/Parts/Channel/Channel.php
@@ -1379,52 +1379,62 @@ class Channel extends Part
             'position' => $this->position,
         ];
 
-        if ($attr['type'] == self::TYPE_GUILD_TEXT) {
-            $attr['topic'] = $this->topic;
-            $attr['rate_limit_per_user'] = $this->rate_limit_per_user;
-            $attr['parent_id'] = $this->parent_id;
-            $attr['nsfw'] = $this->nsfw;
-            $attr['default_auto_archive_duration'] = $this->default_auto_archive_duration;
-            $attr['default_thread_rate_limit_per_user'] = $this->default_thread_rate_limit_per_user ?? null;
-        } elseif ($attr['type'] == self::TYPE_GUILD_VOICE) {
-            $attr['user_limit'] = $this->user_limit;
-            $attr['parent_id'] = $this->parent_id;
-            $attr['nsfw'] = $this->nsfw;
-            $attr['rtc_region'] = $this->rtc_region;
-            $attr['video_quality_mode'] = $this->video_quality_mode;
-        } elseif ($attr['type'] == self::TYPE_GUILD_ANNOUNCEMENT) {
-            $attr['topic'] = $this->topic;
-            $attr['parent_id'] = $this->parent_id;
-            $attr['nsfw'] = $this->nsfw;
-            $attr['default_auto_archive_duration'] = $this->default_auto_archive_duration;
-        } elseif ($attr['type'] == self::TYPE_GUILD_STAGE_VOICE) {
-            $attr['user_limit'] = $this->user_limit;
-            $attr['parent_id'] = $this->parent_id;
-            $attr['nsfw'] = $this->nsfw;
-            $attr['rtc_region'] = $this->rtc_region;
-            $attr['video_quality_mode'] = $this->video_quality_mode;
-        } elseif ($attr['type'] == self::TYPE_GUILD_FORUM) {
-            $attr['topic'] = $this->topic;
-            $attr['rate_limit_per_user'] = $this->rate_limit_per_user;
-            $attr['parent_id'] = $this->parent_id;
-            $attr['nsfw'] = $this->nsfw;
-            $attr['default_auto_archive_duration'] = $this->default_auto_archive_duration;
-
-            if (array_key_exists('default_reaction_emoji', $this->attributes)) {
-                $attr['default_reaction_emoji'] = $this->attributes['default_reaction_emoji'];
+        switch ($attr['type']) {
+            case self::TYPE_GUILD_TEXT: {
+                $attr['topic'] = $this->topic;
+                $attr['rate_limit_per_user'] = $this->rate_limit_per_user;
+                $attr['parent_id'] = $this->parent_id;
+                $attr['nsfw'] = $this->nsfw;
+                $attr['default_auto_archive_duration'] = $this->default_auto_archive_duration;
+                $attr['default_thread_rate_limit_per_user'] = $this->default_thread_rate_limit_per_user ?? null;
             }
 
-            $attr['available_tags'] = $this->attributes['available_tags'] ?? null;
-
-            if (array_key_exists('default_sort_order', $this->attributes)) {
-                $attr['default_sort_order'] = $this->default_sort_order;
+            case self::TYPE_GUILD_VOICE: {
+                $attr['user_limit'] = $this->user_limit;
+                $attr['parent_id'] = $this->parent_id;
+                $attr['nsfw'] = $this->nsfw;
+                $attr['rtc_region'] = $this->rtc_region;
+                $attr['video_quality_mode'] = $this->video_quality_mode;
             }
 
-            if (array_key_exists('default_forum_layout', $this->attributes)) {
-                $attr['default_forum_layout'] = $this->default_forum_layout;
+            case self::TYPE_GUILD_ANNOUNCEMENT: {
+                $attr['topic'] = $this->topic;
+                $attr['parent_id'] = $this->parent_id;
+                $attr['nsfw'] = $this->nsfw;
+                $attr['default_auto_archive_duration'] = $this->default_auto_archive_duration;
             }
 
-            $attr['default_thread_rate_limit_per_user'] = $this->default_thread_rate_limit_per_user ?? null;
+            case self::TYPE_GUILD_STAGE_VOICE: {
+                $attr['user_limit'] = $this->user_limit;
+                $attr['parent_id'] = $this->parent_id;
+                $attr['nsfw'] = $this->nsfw;
+                $attr['rtc_region'] = $this->rtc_region;
+                $attr['video_quality_mode'] = $this->video_quality_mode;
+            }
+
+            case self::TYPE_GUILD_FORUM: {
+                $attr['topic'] = $this->topic;
+                $attr['rate_limit_per_user'] = $this->rate_limit_per_user;
+                $attr['parent_id'] = $this->parent_id;
+                $attr['nsfw'] = $this->nsfw;
+                $attr['default_auto_archive_duration'] = $this->default_auto_archive_duration;
+
+                if (array_key_exists('default_reaction_emoji', $this->attributes)) {
+                    $attr['default_reaction_emoji'] = $this->attributes['default_reaction_emoji'];
+                }
+
+                $attr['available_tags'] = $this->attributes['available_tags'] ?? null;
+
+                if (array_key_exists('default_sort_order', $this->attributes)) {
+                    $attr['default_sort_order'] = $this->default_sort_order;
+                }
+
+                if (array_key_exists('default_forum_layout', $this->attributes)) {
+                    $attr['default_forum_layout'] = $this->default_forum_layout;
+                }
+
+                $attr['default_thread_rate_limit_per_user'] = $this->default_thread_rate_limit_per_user ?? null;
+            }
         }
 
         return $attr;
