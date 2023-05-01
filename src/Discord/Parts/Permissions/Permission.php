1869dba77a61d1757d8075d977eb1427d12fb01b
Alexander Maassen <outsider@scarynet.org>
2023-05-01 18:55:57 +0200

    forgot use_soundboard

--- src/Discord/Parts/Permissions/Permission.php
+++ src/Discord/Parts/Permissions/Permission.php
@@ -64,6 +64,7 @@ abstract class Permission extends Part
         'use_vad' => 25,
         'manage_events' => 33,
         'use_embedded_activities' => 39,
+        'use_soundboard' => 42,
     ];
 
     /**
