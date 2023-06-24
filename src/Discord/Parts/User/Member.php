87fec50fd3fdeb6d0f404eb880bb277875b11e72
SQKo <87897282+SQKo@users.noreply.github.com>
2023-06-24 13:53:13 +0700

    fix Member::getDisplayname attribute

--- src/Discord/Parts/User/Member.php
+++ src/Discord/Parts/User/Member.php
@@ -43,7 +43,7 @@ use function React\Promise\reject;
  * @property-read string|null         $username                     The username of the member.
  * @property-read string|null         $discriminator                The discriminator of the member.
  * @property      ?string|null        $nick                         The nickname of the member.
- * @property-read string              $displayname                  The nickname or username with discriminator of the member.
+ * @property-read string              $displayname                  The nickname or display name with discriminator of the member.
  * @property      ?string|null        $avatar                       The avatar URL of the member or null if member has no guild avatar.
  * @property      ?string|null        $avatar_hash                  The avatar hash of the member or null if member has no guild avatar.
  * @property      Collection|Role[]   $roles                        A collection of Roles that the member has.
@@ -510,13 +510,15 @@ class Member extends Part
     }
 
     /**
-     * Returns the member nickname or username with the discriminator.
+     * Returns the member nickname or display name with optional #discriminator.
      *
-     * @return string Nickname#Discriminator
+     * @return string Either nick or global_name or username with optional #discriminator.
      */
     protected function getDisplaynameAttribute(): string
     {
-        return ($this->nick ?? $this->username).'#'.$this->discriminator;
+        $user = $this->user;
+
+        return ($this->nick ?? $user->global_name ?? $user->username) . ($user->discriminator ? '#' . $this->discriminator : '');
     }
 
     /**
