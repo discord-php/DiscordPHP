545f3737efc2b42aeb0fad2445e75eac200d6cb4
SQKo <87897282+SQKo@users.noreply.github.com>
2023-08-21 20:59:38 +0700

    Update WebhooksUpdate.php
    Fix yield

--- src/Discord/WebSockets/Events/WebhooksUpdate.php
+++ src/Discord/WebSockets/Events/WebhooksUpdate.php
@@ -27,24 +27,22 @@ class WebhooksUpdate extends Event
      */
     public function handle($data)
     {
-        return yield $this->discord->guilds->cacheGet($data->guild_id)->then(function ($guild) use ($data) {
-            if ($guild) {
-                /** @var ?Channel */
-                if (! $channel = yield $guild->channels->cacheGet($data->channel_id)) {
-                    /** @var Channel */
-                    foreach ($guild->channels as $parent) {
-                        /** @var ?Thread */
-                        if ($thread = yield $parent->threads->cacheGet($data->channel_id)) {
-                            $channel = $thread;
-                            break;
-                        }
+        if ($guild = yield $this->discord->guilds->cacheGet($data->guild_id)) {
+            /** @var ?Channel */
+            if (! $channel = yield $guild->channels->cacheGet($data->channel_id)) {
+                /** @var Channel */
+                foreach ($guild->channels as $parent) {
+                    /** @var ?Thread */
+                    if ($thread = yield $parent->threads->cacheGet($data->channel_id)) {
+                        $channel = $thread;
+                        break;
                     }
                 }
-
-                return [$guild, $channel ?? (object) ['id' => $data->channel_id]];
             }
 
-            return [(object) ['id' => $data->guild_id], (object) ['id' => $data->channel_id]];
-        });
+            return [$guild, $channel ?? (object) ['id' => $data->channel_id]];
+        }
+
+        return [(object) ['id' => $data->guild_id], (object) ['id' => $data->channel_id]];
     }
 }
