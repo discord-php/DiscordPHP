6248c87e20b40a7b161fd210e49c69b9a6fd4626
Valithor Obsidion <valzargaming@gmail.com>
2025-01-21 08:55:54 -0500

    Attempt to use the value's discrim property as the key if offset is null

--- src/Discord/Helpers/CollectionTrait.php
+++ src/Discord/Helpers/CollectionTrait.php
@@ -587,6 +587,15 @@ trait CollectionTrait
      */
     public function offsetSet($offset, $value): void
     {
+        // Attempt to use the value's discrim property as the key if offset is null
+        if (is_null($offset)) {
+            if (is_object($value) && isset($value->{$this->discrim})) {
+                $offset = $value->{$this->discrim};
+            } elseif (is_array($value) && isset($value[$this->discrim])) {
+                $offset = $value[$this->discrim];
+            }
+        }
+
         $this->items[$offset] = $value;
     }
 
