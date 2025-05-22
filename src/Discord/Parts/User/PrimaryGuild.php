d27122fe3eb648a77b2f6f45015a8ad9fc70f3eb
Valithor Obsidion <valzargaming@gmail.com>
2025-05-22 09:32:02 -0400

    Fix casing

--- src/Discord/Parts/User/PrimaryGuild.php
+++ src/Discord/Parts/User/PrimaryGuild.php
@@ -22,8 +22,8 @@ use Discord\Parts\Part;
  *
  * @since 10.10.0
  *
- * @property string  $identityGuildId   The id of the user's primary clan.
- * @property bool    $identityEnabled   Whether the user is displaying their clan tag.
+ * @property string  $identity_guild_id The id of the user's primary clan.
+ * @property bool    $identity_enabled  Whether the user is displaying their clan tag.
  * @property string  $tag               The text of the user's clan tag (max 4 characters).
  * @property string  $badge             The clan badge hash.
  *
@@ -36,8 +36,8 @@ class PrimaryGuild extends Part
      */
     protected $fillable = [
         'id', // @internal
-        'identityGuildId',
-        'identityEnabled',
+        'identity_guild_id',
+        'identity_enabled',
         'tag',
         'badge',
     ];
