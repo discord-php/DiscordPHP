fbc9b898804295c76c39074ade3d45585e9eea81
Valithor Obsidion <valzargaming@gmail.com>
2025-05-31 16:51:16 -0400

    Factory::create

--- src/Discord/Parts/Channel/Message/MediaGallery.php
+++ src/Discord/Parts/Channel/Message/MediaGallery.php
@@ -46,11 +46,7 @@ class MediaGallery extends Content
         $collection = new Collection([], 'media', MediaGalleryItem::class);
 
         foreach ($this->attributes['items'] as $item) {
-            $collection->pushItem(MediaGalleryItem::new(
-                $item['media'],
-                $item['description'] ?? null,
-                $item['spoiler'] ?? false
-            ));
+            $collection->pushItem($this->factory->create(MediaGalleryItem::class, $item, true));
         }
 
         return $collection;
