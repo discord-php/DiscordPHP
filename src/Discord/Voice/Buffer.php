e367550ae19df85436c89468efd99b3271d4bbbb
alexandre433 <alexandreluisbarreto@gmail.com>
2025-06-01 16:11:13 +0100

    Updates usage of trafficcophp/bytebuffer to a locally ovewritten class Adds enum for format pack

--- src/Discord/Voice/Buffer.php
+++ src/Discord/Voice/Buffer.php
@@ -12,8 +12,9 @@
 namespace Discord\Voice;
 
 use ArrayAccess;
+use SplFixedArray;
+use Discord\Helpers\FormatPackEnum;
 use TrafficCophp\ByteBuffer\Buffer as BaseBuffer;
-use TrafficCophp\ByteBuffer\FormatPackEnum;
 
 /**
  * A Byte Buffer similar to Buffer in NodeJS.
@@ -22,6 +23,285 @@ use TrafficCophp\ByteBuffer\FormatPackEnum;
  */
 class Buffer extends BaseBuffer implements ArrayAccess
 {
+    protected SplFixedArray $buffer;
+
+    public function __construct($argument)
+    {
+        match (true) {
+            is_string($argument) => $this->initializeStructs(strlen($argument), $argument),
+            is_int($argument) => $this->initializeStructs($argument, pack(FormatPackEnum::x->value . "$argument")),
+            default => throw new \InvalidArgumentException('Constructor argument must be an binary string or integer')
+        };
+    }
+
+    public function __toString(): string
+    {
+        $buf = '';
+        foreach ($this->buffer as $bytes) {
+            $buf .= $bytes;
+        }
+        return $buf;
+    }
+
+    public static function make($argument): static
+    {
+        return new static($argument);
+    }
+
+    protected function initializeStructs($length, $content): void
+    {
+        $this->buffer = new SplFixedArray($length);
+        for ($i = 0; $i < $length; $i++) {
+            $this->buffer[$i] = $content[$i];
+        }
+    }
+
+    /**
+     * Inserts a value into the buffer at the specified offset.
+     *
+     * @param FormatPackEnum|string $format
+     * @param mixed $value
+     * @param int $offset
+     * @param mixed $length
+     * @return Buffer
+     */
+    protected function insert($format, $value, $offset, $length): self
+    {
+        $bytes = pack($format?->value ?? $format, $value);
+
+        if (null === $length) {
+            $length = strlen($bytes);
+        }
+
+        for ($i = 0; $i < strlen($bytes); $i++) {
+            $this->buffer[$offset++] = $bytes[$i];
+        }
+
+        return $this;
+    }
+
+    /**
+     * Extracts a value from the buffer at the specified offset.
+     *
+     * @param FormatPackEnum|string $format
+     * @param int $offset
+     * @param int $length
+     * @return mixed
+     */
+    protected function extract($format, $offset, $length)
+    {
+        $encoded = '';
+        for ($i = 0; $i < $length; $i++) {
+            $encoded .= $this->buffer->offsetGet($offset + $i);
+        }
+
+        if ($format == FormatPackEnum::N && PHP_INT_SIZE <= 4) {
+            [, $h, $l] = unpack('n*', $encoded);
+            $result = $l + $h * 0x010000;
+        } elseif ($format == FormatPackEnum::V && PHP_INT_SIZE <= 4) {
+            [, $h, $l] = unpack('v*', $encoded);
+            $result = $h + $l * 0x010000;
+        } else {
+            [, $result] = unpack($format?->value ?? $format, $encoded);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Checks if the actual value exceeds the expected maximum size.
+     *
+     * @param mixed $excpectedMax
+     * @param mixed $actual
+     * @throws \InvalidArgumentException
+     * @return static
+     */
+    protected function checkForOverSize($excpectedMax, $actual)
+    {
+        if ($actual > $excpectedMax) {
+            throw new \InvalidArgumentException(sprintf('%d exceeded limit of %d', $actual, $excpectedMax));
+        }
+
+        return $this;
+    }
+
+    public function length(): int
+    {
+        return $this->buffer->getSize();
+    }
+
+    public function getLastEmptyPosition(): int
+    {
+        foreach($this->buffer as $key => $value) {
+            if (empty(trim($value))) {
+                return $key;
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * Writes a string to the buffer at the specified offset.
+     *
+     * @param string $value  The value that will be written.
+     * @param int|null $offset The offset that the value will be written at.
+     * @return static
+     */
+    public function write($value, $offset = null)
+    {
+        if (null === $offset) {
+            $offset = $this->getLastEmptyPosition();
+        }
+
+        $length = strlen($value);
+        $this->insert('a' . $length, $value, $offset, $length);
+
+        return $this;
+    }
+
+    /**
+     * Writes an 8-bit signed integer to the buffer at the specified offset.
+     *
+     * @param int $value  The value that will be written.
+     * @param int|null $offset The offset that the value will be written at.
+     * @return static
+     */
+    public function writeInt8($value, $offset = null)
+    {
+        if (null === $offset) {
+            $offset = $this->getLastEmptyPosition();
+        }
+
+        $format = FormatPackEnum::C;
+        $this->checkForOverSize(0xff, $value);
+        $this->insert($format, $value, $offset, $format->getLength());
+
+        return $this;
+    }
+
+    /**
+     * Writes a 16-bit signed integer to the buffer at the specified offset.
+     *
+     * @param int $value  The value that will be written.
+     * @param int|null $offset The offset that the value will be written at.
+     * @return static
+     */
+    public function writeInt16BE($value, $offset = null)
+    {
+        if (null === $offset) {
+            $offset = $this->getLastEmptyPosition();
+        }
+
+        $format = FormatPackEnum::n;
+        $this->checkForOverSize(0xffff, $value);
+        $this->insert($format, $value, $offset, $format->getLength());
+
+        return $this;
+    }
+
+    /**
+     * Writes a 16-bit signed integer to the buffer at the specified offset.
+     *
+     * @param int $value  The value that will be written.
+     * @param int|null $offset The offset that the value will be written at.
+     * @return static
+     */
+    public function writeInt16LE($value, $offset = null)
+    {
+        if (null === $offset) {
+            $offset = $this->getLastEmptyPosition();
+        }
+
+        $format = FormatPackEnum::v;
+        $this->checkForOverSize(0xffff, $value);
+        $this->insert($format, $value, $offset, $format->getLength());
+
+        return $this;
+    }
+
+    /**
+     * Writes a 32-bit signed integer to the buffer at the specified offset.
+     *
+     * @param int $value  The value that will be written.
+     * @param int|null $offset The offset that the value will be written at.
+     * @return static
+     */
+    public function writeInt32BE($value, $offset = null)
+    {
+        if (null === $offset) {
+            $offset = $this->getLastEmptyPosition();
+        }
+
+        $format = FormatPackEnum::N;
+        $this->checkForOverSize(0xffffffff, $value);
+        $this->insert($format, $value, $offset, $format->getLength());
+
+        return $this;
+    }
+
+    /**
+     * Writes a 32-bit signed integer to the buffer at the specified offset.
+     *
+     * @param int $value  The value that will be written.
+     * @param int|null $offset The offset that the value will be written at.
+     * @return static
+     */
+    public function writeInt32LE($value, $offset = null)
+    {
+        if (null === $offset) {
+            $offset = $this->getLastEmptyPosition();
+        }
+
+        $format = FormatPackEnum::V;
+        $this->checkForOverSize(0xffffffff, $value);
+        $this->insert($format, $value, $offset, $format->getLength());
+
+        return $this;
+    }
+
+    /**
+     * Reads a string from the buffer at the specified offset.
+     *
+     * @param int $offset The offset to read from.
+     * @param int $length The length of the string to read.
+     * @return string The data read.
+     */
+    public function read($offset, $length)
+    {
+        return $this->extract('a' . $length, $offset, $length);
+    }
+
+    public function readInt8($offset)
+    {
+        $format = FormatPackEnum::C;
+        return $this->extract($format, $offset, $format->getLength());
+    }
+
+    public function readInt16BE($offset)
+    {
+        $format = FormatPackEnum::n;
+        return $this->extract($format, $offset, $format->getLength());
+    }
+
+    public function readInt16LE($offset)
+    {
+        $format = FormatPackEnum::v;
+        return $this->extract($format, $offset, $format->getLength());
+    }
+
+    public function readInt32BE($offset)
+    {
+        $format = FormatPackEnum::N;
+        return $this->extract($format, $offset, $format->getLength());
+    }
+
+    public function readInt32LE($offset)
+    {
+        $format = FormatPackEnum::V;
+        return $this->extract($format, $offset, $format->getLength());
+    }
+
     /**
      * Writes a 32-bit unsigned integer with big endian.
      *
