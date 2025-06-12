f4bd945089c27ee89118f7f4767bdf3f764b23d2
Valithor Obsidion <valzargaming@gmail.com>
2025-06-12 08:39:18 -0400

    Message::delete bug fix (#1357)

--- src/Discord/Parts/Channel/Message.php
+++ src/Discord/Parts/Channel/Message.php
@@ -1248,14 +1248,16 @@ class Message extends Part
     /**
      * Deletes the message from the channel.
      *
-     * @link https://discord.com/developers/docs/resources/channel#delete-message
+     * @link https://discord.com/developers/docs/resources/message#delete-message
+     *
+     * @param string|null $reason Reason for Audit Log (if supported).
      *
      * @return PromiseInterface
      *
      * @throws \RuntimeException      This type of message cannot be deleted.
      * @throws NoPermissionsException Missing manage_messages permission when deleting others message.
      */
-    public function delete(): PromiseInterface
+    public function delete(?string $reason = null): PromiseInterface
     {
         if (! $this->isDeletable()) {
             return reject(new \RuntimeException("Cannot delete this type of message: {$this->type}", 50021));
@@ -1268,7 +1270,17 @@ class Message extends Part
             }
         }
 
-        return $this->http->delete(Endpoint::bind(Endpoint::CHANNEL_MESSAGE, $this->channel_id, $this->id));
+        if ($channel = $this->channel) {
+            return $channel->messages->delete($this, $reason);
+        }
+
+        // We can still delete the message if we don't have the channel object cached.
+        $headers = [];
+        if (isset($reason)) {
+            $headers['X-Audit-Log-Reason'] = $reason;
+        }
+
+        return $this->http->delete(Endpoint::bind(Endpoint::CHANNEL_MESSAGE, $this->channel_id, $this->id), null, $headers);
     }
 
     /**
