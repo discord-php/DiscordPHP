d61ee77efd49df3e596f91438135ef053ffdfd3f
Valithor Obsidion <valzargaming@gmail.com>
2025-06-13 05:39:23 -0400

    SKURepository

---
+++ src/Discord/Repository/SKURepository.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Repository;
+
+use Discord\Discord;
+use Discord\Http\Endpoint;
+use Discord\Parts\SKU\SKU;
+
+/**
+ * Contains all SKUs for a given application.
+ *
+ * Because of how our SKU and subscription systems work, you will see two SKUs for your subscription offering.
+ *
+ * For integration and testing entitlements for Subscriptions, you should use the SKU with type: 5.
+ *
+ * @see \Discord\Parts\SKU\SKU
+ * @see \Discord\Parts\User\Client
+ *
+ * @since 10.15.0
+ */
+class SKURepository extends AbstractRepository
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $endpoints = [
+        'all' => Endpoint::APPLICATION_SKUS
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $class = SKU::class;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct(Discord $discord, array $vars = [])
+    {
+        $vars['application_id'] = $discord->application->id;
+
+        parent::__construct($discord, $vars);
+    }
+}
