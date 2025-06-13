baacf8dd4f7e34ca00a8daf01cae8a801e8f5088
Valithor Obsidion <valzargaming@gmail.com>
2025-06-13 06:17:00 -0400

    Add missing copyright header
    Whoops

--- src/Discord/Repository/Monetization/SubscriptionRepository.php
+++ src/Discord/Repository/Monetization/SubscriptionRepository.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
 namespace Discord\Repository\Monetization;
 
 use Discord\Helpers\Collection;
