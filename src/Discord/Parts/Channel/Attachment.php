0a0623484e71854ed89d2e798fd4bf3d04d4ab29
Valithor Obsidion <valzargaming@gmail.com>
2025-06-14 04:21:08 -0400

    Update Attachment.php
    Closes #1223

--- src/Discord/Parts/Channel/Attachment.php
+++ src/Discord/Parts/Channel/Attachment.php
@@ -22,16 +22,20 @@ use Discord\Parts\Part;
  *
  * @since 7.0.0
  *
- * @property string      $id           Attachment ID.
- * @property string      $filename     Name of file attached.
- * @property string|null $description  Description for the file.
- * @property string|null $content_type The attachment's media type.
- * @property int         $size         Size of file in bytes.
- * @property string      $url          Source url of file.
- * @property string      $proxy_url    A proxied url of file.
- * @property ?int|null   $height       Height of file (if image).
- * @property ?int|null   $width        Width of file (if image).
- * @property bool|null   $ephemeral    Whether this attachment is ephemeral.
+ * @property string      $id            Attachment ID.
+ * @property string      $filename      Name of file attached.
+ * @property string|null $title         The title of the file
+ * @property string|null $description   Description for the file.
+ * @property string|null $content_type  The attachment's media type.
+ * @property int         $size          Size of file in bytes.
+ * @property string      $url           Source url of file.
+ * @property string      $proxy_url     A proxied url of file.
+ * @property ?int|null   $height        Height of file (if image).
+ * @property ?int|null   $width         Width of file (if image).
+ * @property bool|null   $ephemeral     Whether this attachment is ephemeral.
+ * @property float|null  $duration_secs The duration of the audio file (currently for voice messages).
+ * @property string|null $waveform      Base64 encoded bytearray representing a sampled waveform (currently for voice messages).
+ * @property int|null    $flags         Attachment flags combined as a bitfield.
  */
 class Attachment extends Part
 {
@@ -41,6 +45,7 @@ class Attachment extends Part
     protected $fillable = [
         'id',
         'filename',
+        'title',
         'description',
         'content_type',
         'size',
@@ -49,5 +54,8 @@ class Attachment extends Part
         'height',
         'width',
         'ephemeral',
+        'duration_secs',
+        'waveform',
+        'flags',
     ];
 }
