c8693e73cd6dee16d62addc53eb435a8663993c6
Valithor Obsidion <valzargaming@gmail.com>
2025-07-02 14:45:38 -0400

    Implement Colors class and property for Roles

--- src/Discord/Parts/Guild/Role.php
+++ src/Discord/Parts/Guild/Role.php
@@ -28,7 +28,7 @@ use Stringable;
  * @property      string         $id            The unique identifier of the role.
  * @property      string         $name          The name of the role.
  * @property      int            $color         Integer representation of hexadecimal color code.
- * @property      array          $colors        The role's colors.
+ * @property      Colors         $colors        The role's colors.
  * @property      bool           $hoist         If this role is pinned in the user listing
  * @property      ?string|null   $icon          The URL to the role icon.
  * @property-read string|null    $icon_hash     The icon hash for the role.
@@ -69,6 +69,18 @@ class Role extends Part implements Stringable
         // @internal
         'guild_id',
     ];
+
+    /**
+     * Gets the colors attribute.
+     *
+     * @return Colors The role's colors.
+     *
+     * @since 10.18.1
+     */
+    protected function getColorsAttribute(): Colors
+    {
+        return $this->createOf(Colors::class, $this->attributes['colors']);
+    }
 
     /**
      * Gets the permissions attribute.
