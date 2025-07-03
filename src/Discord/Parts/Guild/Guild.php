f65c1dd4eaee57926a58ccee0ea4efd333cdc156
Valithor Obsidion <valzargaming@gmail.com>
2025-07-03 05:22:44 -0400

    Adds support for previewing guilds (#1365)

--- src/Discord/Parts/Guild/Guild.php
+++ src/Discord/Parts/Guild/Guild.php
@@ -159,6 +159,8 @@ use function React\Promise\resolve;
  */
 class Guild extends Part
 {
+    use GuildTrait;
+
     public const REGION_DEFAULT = 'us_west';
 
     public const NOTIFICATION_ALL_MESSAGES = 0;
@@ -517,78 +519,6 @@ class Guild extends Part
     }
 
     /**
-     * Returns the guilds icon.
-     *
-     * @param string|null $format The image format.
-     * @param int         $size   The size of the image.
-     *
-     * @return string|null The URL to the guild icon or null.
-     */
-    public function getIconAttribute(?string $format = null, int $size = 1024): ?string
-    {
-        if (! isset($this->attributes['icon'])) {
-            return null;
-        }
-
-        if (isset($format)) {
-            static $allowed = ['png', 'jpg', 'webp', 'gif'];
-
-            if (! in_array(strtolower($format), $allowed)) {
-                $format = 'webp';
-            }
-        } elseif (strpos($this->attributes['icon'], 'a_') === 0) {
-            $format = 'gif';
-        } else {
-            $format = 'webp';
-        }
-
-        return "https://cdn.discordapp.com/icons/{$this->id}/{$this->attributes['icon']}.{$format}?size={$size}";
-    }
-
-    /**
-     * Returns the guild icon hash.
-     *
-     * @return string|null The guild icon hash or null.
-     */
-    protected function getIconHashAttribute(): ?string
-    {
-        return $this->attributes['icon_hash'] ?? $this->attributes['icon'];
-    }
-
-    /**
-     * Returns the guild splash.
-     *
-     * @param string $format The image format.
-     * @param int    $size   The size of the image.
-     *
-     * @return string|null The URL to the guild splash or null.
-     */
-    public function getSplashAttribute(string $format = 'webp', int $size = 2048): ?string
-    {
-        if (! isset($this->attributes['splash'])) {
-            return null;
-        }
-
-        static $allowed = ['png', 'jpg', 'webp'];
-
-        if (! in_array(strtolower($format), $allowed)) {
-            $format = 'webp';
-        }
-
-        return "https://cdn.discordapp.com/splashes/{$this->id}/{$this->attributes['splash']}.{$format}?size={$size}";
-    }
-
-    /**
-     * Returns the guild splash hash.
-     *
-     * @return string|null The guild splash hash or null.
-     */
-    protected function getSplashHashAttribute(): ?string
-    {
-        return $this->attributes['splash'] ?? null;
-    }
-
-    /**
      * Returns the channels stage instances.
      *
      * @deprecated 10.0.0 Use `$channel->stage_instances`
@@ -607,186 +537,6 @@ class Guild extends Part
         }
 
         return $stage_instances;
-    }
-
-    protected function getFeatureAnimatedBannerAttribute(): bool
-    {
-        return in_array('ANIMATED_BANNER', $this->features);
-    }
-
-    protected function getFeatureAnimatedIconAttribute(): bool
-    {
-        return in_array('ANIMATED_ICON', $this->features);
-    }
-
-    protected function getFeatureApplicationCommandPermissionsV2(): bool
-    {
-        return in_array('APPLICATION_COMMAND_PERMISSIONS_V2', $this->features);
-    }
-
-    protected function getFeatureAutoModerationAttribute(): bool
-    {
-        return in_array('AUTO_MODERATION', $this->features);
-    }
-
-    protected function getFeatureBannerAttribute(): bool
-    {
-        return in_array('BANNER', $this->features);
-    }
-
-    protected function getFeatureCommunityAttribute(): bool
-    {
-        return in_array('COMMUNITY', $this->features);
-    }
-
-    protected function getFeatureCreatorMonetizableProvisionalAttribute(): bool
-    {
-        return in_array('CREATOR_MONETIZABLE_PROVISIONAL', $this->features);
-    }
-
-    protected function getFeatureCreatorStorePageAttribute(): bool
-    {
-        return in_array('CREATOR_STORE_PAGE', $this->features);
-    }
-
-    protected function getFeatureDeveloperSupportServerAttribute(): bool
-    {
-        return in_array('DEVELOPER_SUPPORT_SERVER', $this->features);
-    }
-
-    protected function getFeatureDiscoverableAttribute(): bool
-    {
-        return in_array('DISCOVERABLE', $this->features);
-    }
-
-    protected function getFeatureFeaturableAttribute(): bool
-    {
-        return in_array('FEATURABLE', $this->features);
-    }
-
-    protected function getFeatureHasDirectoryEntryAttribute(): bool
-    {
-        return in_array('HAS_DIRECTORY_ENTRY', $this->features);
-    }
-
-    protected function getFeatureInvitesDisabledAttribute(): bool
-    {
-        return in_array('INVITES_DISABLED', $this->features);
-    }
-
-    protected function getFeatureInviteSplashAttribute(): bool
-    {
-        return in_array('INVITE_SPLASH', $this->features);
-    }
-
-    protected function getFeatureLinkedToHubAttribute(): bool
-    {
-        return in_array('LINKED_TO_HUB', $this->features);
-    }
-
-    protected function getFeatureMemberVerificationGateEnabledAttribute(): bool
-    {
-        return in_array('MEMBER_VERIFICATION_GATE_ENABLED', $this->features);
-    }
-
-    protected function getFeatureMoreSoundboardAttribute(): bool
-    {
-        return in_array('MORE_SOUNDBOARD', $this->features);
-    }
-
-    protected function getFeatureMonetizationEnabledAttribute(): bool
-    {
-        return in_array('MONETIZATION_ENABLED', $this->features);
-    }
-
-    protected function getFeatureMoreStickersAttribute(): bool
-    {
-        return in_array('MORE_STICKERS', $this->features);
-    }
-
-    protected function getFeatureNewsAttribute(): bool
-    {
-        return in_array('NEWS', $this->features);
-    }
-
-    protected function getFeaturePartneredAttribute(): bool
-    {
-        return in_array('PARTNERED', $this->features);
-    }
-
-    protected function getFeaturePreviewEnabledAttribute(): bool
-    {
-        return in_array('PREVIEW_ENABLED', $this->features);
-    }
-
-    protected function getFeaturePrivateThreadsAttribute(): bool
-    {
-        return in_array('PRIVATE_THREADS', $this->features);
-    }
-
-    protected function getFeatureRaidAlertsDisabledAttribute(): bool
-    {
-        return in_array('RAID_ALERTS_DISABLED', $this->features);
-    }
-
-    protected function getFeatureRaidAlertsEnabledAttribute(): bool
-    {
-        return in_array('RAID_ALERTS_ENABLED', $this->features);
-    }
-
-    protected function getFeatureRoleIconsAttribute(): bool
-    {
-        return in_array('ROLE_ICONS', $this->features);
-    }
-
-    protected function getFeatureRoleSubscriptionsAvailableForPurchaseAttribute(): bool
-    {
-        return in_array('ROLE_SUBSCRIPTIONS_AVAILABLE_FOR_PURCHASE', $this->features);
-    }
-
-    protected function getFeatureRoleSubscriptionsEnabledAttribute(): bool
-    {
-        return in_array('ROLE_SUBSCRIPTIONS_ENABLED', $this->features);
-    }
-
-    protected function getFeatureSoundboardAttribute(): bool
-    {
-        return in_array('SOUNDBOARD', $this->features);
-    }
-
-    protected function getFeatureTicketedEventsEnabledAttribute(): bool
-    {
-        return in_array('TICKETED_EVENTS_ENABLED', $this->features);
-    }
-
-    protected function getFeatureVanityUrlAttribute(): bool
-    {
-        return in_array('VANITY_URL', $this->features);
-    }
-
-    protected function getFeatureVerifiedAttribute(): bool
-    {
-        return in_array('VERIFIED', $this->features);
-    }
-
-    protected function getFeatureVipRegionsAttribute(): bool
-    {
-        return in_array('VIP_REGIONS', $this->features);
-    }
-
-    protected function getFeatureWelcomeScreenEnabledAttribute(): bool
-    {
-        return in_array('WELCOME_SCREEN_ENABLED', $this->features);
-    }
-
-    protected function getFeatureGuestEnabledAttribute(): bool
-    {
-        return in_array('GUEST_ENABLED', $this->features);
-    }
-
-    protected function getFeatureEnhancedRoleColorsAttribute(): bool
-    {
-        return in_array('ENHANCED_ROLE_COLORS', $this->features);
     }
 
     /**
@@ -1698,16 +1448,6 @@ class Guild extends Part
             'premium_progress_bar_enabled' => $this->premium_progress_bar_enabled,
             'safety_alerts_channel_id' => $this->safety_alerts_channel_id,
         ]);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getRepositoryAttributes(): array
-    {
-        return [
-            'guild_id' => $this->id,
-        ];
     }
 
     /**
---
+++ src/Discord/Parts/Guild/GuildTrait.php
@@ -0,0 +1,279 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Parts\Guild;
+
+trait GuildTrait
+{
+    /**
+     * Returns the guilds icon.
+     *
+     * @param string|null $format The image format.
+     * @param int         $size   The size of the image.
+     *
+     * @return string|null The URL to the guild icon or null.
+     */
+    public function getIconAttribute(?string $format = null, int $size = 1024): ?string
+    {
+        if (! isset($this->attributes['icon'])) {
+            return null;
+        }
+
+        if (isset($format)) {
+            static $allowed = ['png', 'jpg', 'webp', 'gif'];
+
+            if (! in_array(strtolower($format), $allowed)) {
+                $format = 'webp';
+            }
+        } elseif (strpos($this->attributes['icon'], 'a_') === 0) {
+            $format = 'gif';
+        } else {
+            $format = 'webp';
+        }
+
+        return "https://cdn.discordapp.com/icons/{$this->id}/{$this->attributes['icon']}.{$format}?size={$size}";
+    }
+
+    /**
+     * Returns the guild icon hash.
+     *
+     * @return string|null The guild icon hash or null.
+     */
+    protected function getIconHashAttribute(): ?string
+    {
+        return $this->attributes['icon_hash'] ?? $this->attributes['icon'];
+    }
+
+    /**
+     * Returns the guild splash.
+     *
+     * @param string $format The image format.
+     * @param int    $size   The size of the image.
+     *
+     * @return string|null The URL to the guild splash or null.
+     */
+    public function getSplashAttribute(string $format = 'webp', int $size = 2048): ?string
+    {
+        if (! isset($this->attributes['splash'])) {
+            return null;
+        }
+
+        static $allowed = ['png', 'jpg', 'webp'];
+
+        if (! in_array(strtolower($format), $allowed)) {
+            $format = 'webp';
+        }
+
+        return "https://cdn.discordapp.com/splashes/{$this->id}/{$this->attributes['splash']}.{$format}?size={$size}";
+    }
+
+    /**
+     * Returns the guild splash hash.
+     *
+     * @return string|null The guild splash hash or null.
+     */
+    protected function getSplashHashAttribute(): ?string
+    {
+        return $this->attributes['splash'] ?? null;
+    }
+
+    protected function getFeatureAnimatedBannerAttribute(): bool
+    {
+        return in_array('ANIMATED_BANNER', $this->features);
+    }
+
+    protected function getFeatureAnimatedIconAttribute(): bool
+    {
+        return in_array('ANIMATED_ICON', $this->features);
+    }
+
+    protected function getFeatureApplicationCommandPermissionsV2(): bool
+    {
+        return in_array('APPLICATION_COMMAND_PERMISSIONS_V2', $this->features);
+    }
+
+    protected function getFeatureAutoModerationAttribute(): bool
+    {
+        return in_array('AUTO_MODERATION', $this->features);
+    }
+
+    protected function getFeatureBannerAttribute(): bool
+    {
+        return in_array('BANNER', $this->features);
+    }
+
+    protected function getFeatureCommunityAttribute(): bool
+    {
+        return in_array('COMMUNITY', $this->features);
+    }
+
+    protected function getFeatureCreatorMonetizableProvisionalAttribute(): bool
+    {
+        return in_array('CREATOR_MONETIZABLE_PROVISIONAL', $this->features);
+    }
+
+    protected function getFeatureCreatorStorePageAttribute(): bool
+    {
+        return in_array('CREATOR_STORE_PAGE', $this->features);
+    }
+
+    protected function getFeatureDeveloperSupportServerAttribute(): bool
+    {
+        return in_array('DEVELOPER_SUPPORT_SERVER', $this->features);
+    }
+
+    protected function getFeatureDiscoverableAttribute(): bool
+    {
+        return in_array('DISCOVERABLE', $this->features);
+    }
+
+    protected function getFeatureFeaturableAttribute(): bool
+    {
+        return in_array('FEATURABLE', $this->features);
+    }
+
+    protected function getFeatureHasDirectoryEntryAttribute(): bool
+    {
+        return in_array('HAS_DIRECTORY_ENTRY', $this->features);
+    }
+
+    protected function getFeatureInvitesDisabledAttribute(): bool
+    {
+        return in_array('INVITES_DISABLED', $this->features);
+    }
+
+    protected function getFeatureInviteSplashAttribute(): bool
+    {
+        return in_array('INVITE_SPLASH', $this->features);
+    }
+
+    protected function getFeatureLinkedToHubAttribute(): bool
+    {
+        return in_array('LINKED_TO_HUB', $this->features);
+    }
+
+    protected function getFeatureMemberVerificationGateEnabledAttribute(): bool
+    {
+        return in_array('MEMBER_VERIFICATION_GATE_ENABLED', $this->features);
+    }
+
+    protected function getFeatureMoreSoundboardAttribute(): bool
+    {
+        return in_array('MORE_SOUNDBOARD', $this->features);
+    }
+
+    protected function getFeatureMonetizationEnabledAttribute(): bool
+    {
+        return in_array('MONETIZATION_ENABLED', $this->features);
+    }
+
+    protected function getFeatureMoreStickersAttribute(): bool
+    {
+        return in_array('MORE_STICKERS', $this->features);
+    }
+
+    protected function getFeatureNewsAttribute(): bool
+    {
+        return in_array('NEWS', $this->features);
+    }
+
+    protected function getFeaturePartneredAttribute(): bool
+    {
+        return in_array('PARTNERED', $this->features);
+    }
+
+    protected function getFeaturePreviewEnabledAttribute(): bool
+    {
+        return in_array('PREVIEW_ENABLED', $this->features);
+    }
+
+    protected function getFeaturePrivateThreadsAttribute(): bool
+    {
+        return in_array('PRIVATE_THREADS', $this->features);
+    }
+
+    protected function getFeatureRaidAlertsDisabledAttribute(): bool
+    {
+        return in_array('RAID_ALERTS_DISABLED', $this->features);
+    }
+
+    protected function getFeatureRaidAlertsEnabledAttribute(): bool
+    {
+        return in_array('RAID_ALERTS_ENABLED', $this->features);
+    }
+
+    protected function getFeatureRoleIconsAttribute(): bool
+    {
+        return in_array('ROLE_ICONS', $this->features);
+    }
+
+    protected function getFeatureRoleSubscriptionsAvailableForPurchaseAttribute(): bool
+    {
+        return in_array('ROLE_SUBSCRIPTIONS_AVAILABLE_FOR_PURCHASE', $this->features);
+    }
+
+    protected function getFeatureRoleSubscriptionsEnabledAttribute(): bool
+    {
+        return in_array('ROLE_SUBSCRIPTIONS_ENABLED', $this->features);
+    }
+
+    protected function getFeatureSoundboardAttribute(): bool
+    {
+        return in_array('SOUNDBOARD', $this->features);
+    }
+
+    protected function getFeatureTicketedEventsEnabledAttribute(): bool
+    {
+        return in_array('TICKETED_EVENTS_ENABLED', $this->features);
+    }
+
+    protected function getFeatureVanityUrlAttribute(): bool
+    {
+        return in_array('VANITY_URL', $this->features);
+    }
+
+    protected function getFeatureVerifiedAttribute(): bool
+    {
+        return in_array('VERIFIED', $this->features);
+    }
+
+    protected function getFeatureVipRegionsAttribute(): bool
+    {
+        return in_array('VIP_REGIONS', $this->features);
+    }
+
+    protected function getFeatureWelcomeScreenEnabledAttribute(): bool
+    {
+        return in_array('WELCOME_SCREEN_ENABLED', $this->features);
+    }
+
+    protected function getFeatureGuestEnabledAttribute(): bool
+    {
+        return in_array('GUEST_ENABLED', $this->features);
+    }
+
+    protected function getFeatureEnhancedRoleColorsAttribute(): bool
+    {
+        return in_array('ENHANCED_ROLE_COLORS', $this->features);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getRepositoryAttributes(): array
+    {
+        return [
+            'guild_id' => $this->id,
+        ];
+    }
+}
