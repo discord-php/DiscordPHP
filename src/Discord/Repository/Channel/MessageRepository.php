a2d30dda451ae2d70c70dd0d7474e44348ca3416
Valithor Obsidion <valzargaming@gmail.com>
2025-07-05 07:12:27 -0400

    Update Channel::getPinnedMessages

--- src/Discord/Repository/Channel/MessageRepository.php
+++ src/Discord/Repository/Channel/MessageRepository.php
@@ -60,48 +60,4 @@ class MessageRepository extends AbstractRepository
         unset($vars['thread_id']); // For thread
         parent::__construct($discord, $vars);
     }
-
-    /**
-     * Gets the pinned messages in the channel.
-     *
-     * @link https://discord.com/developers/docs/resources/message#get-channel-pins
-     *
-     * @param Channel|Thread|string $channel           The channel to get the pinned messages from.
-     * @param int                   $options['limit']  The amount of messages to retrieve.
-     * @param Message|Carbon|string $options['before'] A message or timestamp to get messages before.
-     *
-     * @return PromiseInterface<Collection<Message>>
-     *
-     * @since 10.19.0
-    */
-    public function getPinnedMessages($channel, array $options = []): PromiseInterface
-    {
-        $resolver = new OptionsResolver();
-        $resolver
-            ->setDefaults(['limit' => 50])
-            ->setDefined(['before', 'limit'])
-            ->setAllowedTypes('before', [Carbon::class, 'string'])
-            ->setAllowedTypes('limit', 'integer')
-            ->setAllowedValues('limit', fn ($value) => ($value >= 1 && $value <= 50))
-            ->setDefault('before', null);
-
-        $options = $resolver->resolve($options);
-
-        if (isset($options['before'])) {
-            if ($options['before'] instanceof Message) {
-                $options['before'] = $options['before']->timestamp;
-            }
-        }
-
-        return $this->http->get(Endpoint::bind(Endpoint::CHANNEL_MESSAGES_PINS, $this->id), $options)
-        ->then(function ($responses) {
-            $messages = Collection::for(Message::class);
-
-            foreach ($responses as $response) {
-                $messages->pushItem($this->messages->get('id', $response->id) ?: $this->messages->create($response, true));
-            }
-
-            return $messages;
-        });
-    }
 }
--- src/Discord/Repository/Guild/ChannelRepository.php
+++ src/Discord/Repository/Guild/ChannelRepository.php
@@ -13,9 +13,15 @@ declare(strict_types=1);
 
 namespace Discord\Repository\Guild;
 
+use Carbon\Carbon;
+use Discord\Helpers\Collection;
 use Discord\Http\Endpoint;
 use Discord\Parts\Channel\Channel;
+use Discord\Parts\Channel\Message;
+use Discord\Parts\Thread\Thread;
 use Discord\Repository\AbstractRepository;
+use React\Promise\PromiseInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Contains channels on a guild.
@@ -48,4 +54,52 @@ class ChannelRepository extends AbstractRepository
      * {@inheritDoc}
      */
     protected $class = Channel::class;
+
+    /**
+     * Gets the pinned messages in the channel.
+     *
+     * @link https://discord.com/developers/docs/resources/message#get-channel-pins
+     *
+     * @param Channel|Thread|string $channel           The channel to get the pinned messages from.
+     * @param int                   $options['limit']  The amount of messages to retrieve.
+     * @param Message|Carbon|string $options['before'] A message or timestamp to get messages before.
+     *
+     * @return PromiseInterface<Collection<Message>>
+     *
+     * @since 10.19.0
+    */
+    public function getPinnedMessages($channel, array $options = []): PromiseInterface
+    {
+        if ($channel instanceof Channel || $channel instanceof Thread) {
+            $channel = $channel->id;
+        }
+
+        $resolver = new OptionsResolver();
+        $resolver
+            ->setDefaults(['limit' => 50])
+            ->setDefined(['before', 'limit'])
+            ->setAllowedTypes('before', [Carbon::class, 'string'])
+            ->setAllowedTypes('limit', 'integer')
+            ->setAllowedValues('limit', fn ($value) => ($value >= 1 && $value <= 50))
+            ->setDefault('before', null);
+
+        $options = $resolver->resolve($options);
+
+        if (isset($options['before'])) {
+            if ($options['before'] instanceof Message) {
+                $options['before'] = $options['before']->timestamp;
+            }
+        }
+
+        return $this->http->get(Endpoint::bind(Endpoint::CHANNEL_MESSAGES_PINS, $channel), $options)
+        ->then(function ($responses) {
+            $messages = Collection::for(Message::class);
+
+            foreach ($responses as $response) {
+                $messages->pushItem($this->messages->get('id', $response->id) ?: $this->messages->create($response, true));
+            }
+
+            return $messages;
+        });
+    }
 }
