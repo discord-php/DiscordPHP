bbb35d00eefd7bcd37ac7f78ec42f6da3a8855f3
Valithor Obsidion <valzargaming@gmail.com>
2025-07-13 10:14:08 -0400

    Create RoleTags.php

---
+++ src/Discord/Parts/Guild/RoleTags.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Parts\Guild;
+
+use Discord\Parts\Part;
+use Stringable;
+
+/**
+ * Role tags for a Discord role.
+ *
+ * Tags with type null represent booleans. They will be present and set to null if they are "true", and will be not present if they are "false".
+ * The library currently overrides this behavior to return a boolean if the tag is present at all.
+ *
+ * @link https://discord.com/developers/docs/topics/permissions#role-object-role-tags-structure
+ *
+ * @since 10.19.0
+ *
+ * @property string $bot_id                  The id of the bot this role belongs to.
+ * @property string $integration_id          The id of the integration this role belongs to.
+ * @property bool   $premium_subscriber      Whether this is the guild's Booster role.
+ * @property string $subscription_listing_id The id of this role's subscription sku and listing.
+ * @property bool   $available_for_purchase  Whether this role is available for purchase.
+ * @property bool   $guild_connections       Whether this role is a guild's linked role.
+ */
+class RoleTags extends Part implements Stringable
+{
+    /**
+     * @inheritDoc
+     */
+    protected $fillable = [
+        'bot_id',
+        'integration_id',
+        'premium_subscriber',
+        'subscription_listing_id',
+        'available_for_purchase',
+        'guild_connections',
+    ];
+
+    protected function getPremiumSubscriberAttribute(): bool
+    {
+        return array_key_exists('premium_subscriber', $this->attributes);
+    }
+
+    protected function getAvailableForPurchaseAttribute(): bool
+    {
+        return array_key_exists('available_for_purchase', $this->attributes);
+    }
+
+    protected function getGuildConnectionsAttribute(): bool
+    {
+        return array_key_exists('guild_connections', $this->attributes);
+    }
+}
