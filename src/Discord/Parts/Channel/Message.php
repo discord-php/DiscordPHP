a035388a8682e010f9e3ba491e36ccb848b64bd7
Valithor Obsidion <valzargaming@gmail.com>
2025-07-22 08:20:36 -0400

    PHPDocs for flag constants

--- src/Discord/Parts/Channel/Message.php
+++ src/Discord/Parts/Channel/Message.php
@@ -183,22 +183,35 @@ class Message extends Part
     public const REFERENCE_DEFAULT = 0;
     public const REFERENCE_FORWARD = 1;
 
+    /** This message has been published to subscribed channels (via Channel Following). */
     public const FLAG_CROSSPOSTED = (1 << 0);
+    /** This message originated from a message in another channel (via Channel Following). */
     public const FLAG_IS_CROSSPOST = (1 << 1);
     /** @deprecated Use `Message::FLAG_SUPPRESS_EMBEDS` instead. */
     public const FLAG_SUPPRESS_EMBED = (1 << 2);
+    /** Do not include any embeds when serializing this message. */
     public const FLAG_SUPPRESS_EMBEDS = (1 << 2);
+    /** The source message for this crosspost has been deleted (via Channel Following). */
     public const FLAG_SOURCE_MESSAGE_DELETED = (1 << 3);
+    /** This message came from the urgent message system. */
     public const FLAG_URGENT = (1 << 4);
+    /** This message has an associated thread, with the same id as the message. */
     public const FLAG_HAS_THREAD = (1 << 5);
+    /** This message is only visible to the user who invoked the Interaction. */
     public const FLAG_EPHEMERAL = (1 << 6);
+    /** This message is an Interaction Response and the bot is "thinking". */
     public const FLAG_LOADING = (1 << 7);
+    /** This message failed to mention some roles and add their members to the thread. */
     public const FLAG_FAILED_TO_MENTION_SOME_ROLES_IN_THREAD = (1 << 8);
+    /** This message will not trigger push and desktop notifications. */
     public const FLAG_SUPPRESS_NOTIFICATIONS = (1 << 12);
+    /** This message is a voice message. */
     public const FLAG_IS_VOICE_MESSAGE = (1 << 13);
+    /** This message has a snapshot (via Message Forwarding). */
     public const FLAG_HAS_SNAPSHOT = (1 << 14);
     /** @deprecated Use `Message::FLAG_IS_COMPONENTS_V2` instead. */
     public const FLAG_IS_V2_COMPONENTS = (1 << 15);
+    /** Allows you to create fully component-driven messages. */
     public const FLAG_IS_COMPONENTS_V2 = (1 << 15);
 
     /**
