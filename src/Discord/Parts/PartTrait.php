dbc5f985048d4f11a3911d5e61d3b8f32ba6a043
Valithor Obsidion <valzargaming@gmail.com>
2025-07-24 10:46:16 -0400

    PartTrait::getConstArray()

--- src/Discord/Parts/PartTrait.php
+++ src/Discord/Parts/PartTrait.php
@@ -441,6 +441,23 @@ trait PartTrait
     }
 
     /**
+     * Returns an array of constant names and their values.
+     *
+     * @return array<int, string>
+     *
+     * @since 10.19.0
+     */
+    public static function getConstArray(): array
+    {
+        $reflection = new \ReflectionClass(self::class);
+        $map = [];
+        foreach ($reflection->getConstants() as $name => $value) {
+            $map[$value] = $name;
+        }
+        return $map;
+    }
+
+    /**
      * Converts the part to a string.
      *
      * @return string A JSON string of attributes.
