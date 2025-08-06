675908d8cf0cc8fb306294f5895273bacca24e8e
Valithor Obsidion <valzargaming@gmail.com>
2025-08-06 18:25:56 -0400

    Create Label Part

---
+++ src/Discord/Parts/Channel/Message/Label.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Parts\Channel\Message;
+
+/**
+ * A Label is a top-level component.
+ *
+ * @link https://discord.com/developers/docs/components/reference#label
+ *
+ * @todo Update to match Discord's documentation upon public release.
+ * @todo Update Label class to extend the relevant base class.
+ *
+ * @since 10.19.0
+ *
+ * @property int                    $type        18 for label component.
+ * @property string                 $label       The text for the label.
+ * @property string|null            $description Optional description for the label.
+ * @property StringSelect|TextInput $component   The component associated with the label.
+ */
+class Label extends Component
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $fillable = [
+        'id',
+        'type',
+        'label',
+        'description',
+        'component',
+    ];
+
+    public function getComponentAttribute(): StringSelect|TextInput|null
+    {
+        if (!isset($this->attributes['component'])) {
+            return null;
+        }
+
+        return $this->createOf(Component::TYPES[$this->attributes['component']->type ?? 0], $this->attributes['component']);
+    }
+}
