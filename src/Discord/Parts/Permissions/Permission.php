eee21c1a50b6858869e95bddc540afd93065abd6
Valithor Obsidion <valzargaming@gmail.com>
2025-08-19 16:37:14 -0400

    Add PHPDocs to Permissions

--- src/Discord/Parts/Permissions/Permission.php
+++ src/Discord/Parts/Permissions/Permission.php
@@ -48,55 +48,105 @@ use Discord\Parts\Part;
  */
 abstract class Permission extends Part
 {
+    /** Allows creation of instant invites. */
     public const CREATE_INSTANT_INVITE = 0;
+    /** Allows kicking members. */
     public const KICK_MEMBERS = 1;
+    /** Allows banning members. */
     public const BAN_MEMBERS = 2;
+    /** Allows all permissions and bypasses channel permission overwrites. */
     public const ADMINISTRATOR = 3;
+    /** Allows management and editing of channels. */
     public const MANAGE_CHANNELS = 4;
+    /** Allows management and editing of the guild. */
     public const MANAGE_GUILD = 5;
+    /** Allows for adding new reactions to messages. This permission does not apply to reacting with an existing reaction on a message. */
     public const ADD_REACTIONS = 6;
+    /** Allows for viewing of audit logs. */
     public const VIEW_AUDIT_LOG = 7;
+    /** Allows for using priority speaker in a voice channel. */
     public const PRIORITY_SPEAKER = 8;
+    /** Allows the user to go live. */
     public const STREAM = 9;
+    /** Allows guild members to view a channel, which includes reading messages in text channels and joining voice channels. */
     public const VIEW_CHANNEL = 10;
+    /** Allows for sending messages in a channel and creating threads in a forum (does not allow sending messages in threads). */
     public const SEND_MESSAGES = 11;
+    /** Allows for sending of `/tts` messages. */
     public const SEND_TTS_MESSAGES = 12;
+    /** Allows for deletion of other users messages. */
     public const MANAGE_MESSAGES = 13;
+    /** Links sent by users with this permission will be auto-embedded. */
     public const EMBED_LINKS = 14;
+    /** Allows for uploading images and files. */
     public const ATTACH_FILES = 15;
+    /** Allows for reading of message history. */
     public const READ_MESSAGE_HISTORY = 16;
+    /** Allows for using the `@everyone` tag to notify all users in a channel, and the `@here` tag to notify all online users in a channel. */
     public const MENTION_EVERYONE = 17;
+    /** Allows the usage of custom emojis from other servers. */
     public const USE_EXTERNAL_EMOJIS = 18;
+    /** Allows for viewing guild insights. */
     public const VIEW_GUILD_INSIGHTS = 19;
+    /** Allows for joining of a voice channel. */
     public const CONNECT = 20;
+    /** Allows for speaking in a voice channel. */
     public const SPEAK = 21;
+    /** Allows for muting members in a voice channel. */
     public const MUTE_MEMBERS = 22;
+    /** Allows for deafening of members in a voice channel. */
     public const DEAFEN_MEMBERS = 23;
+    /** Allows for moving of members between voice channels. */
     public const MOVE_MEMBERS = 24;
+    /** Allows for using voice-activity-detection in a voice channel. */
     public const USE_VAD = 25;
+    /** Allows for modification of own nickname. */
     public const CHANGE_NICKNAME = 26;
+    /** Allows for modification of other users nicknames. */
     public const MANAGE_NICKNAMES = 27;
+    /** Allows management and editing of roles. */
     public const MANAGE_ROLES = 28;
+    /** Allows management and editing of webhooks. */
     public const MANAGE_WEBHOOKS = 29;
+    /** Allows for editing and deleting emojis, stickers, and soundboard sounds created by all users. */
     public const MANAGE_GUILD_EXPRESSIONS = 30;
+    /** Allows members to use application commands, including slash commands and context menu commands. */
     public const USE_APPLICATION_COMMANDS = 31;
+    /** Allows for requesting to speak in stage channels. (This permission is under active development and may be changed or removed.) */
     public const REQUEST_TO_SPEAK = 32;
+    /** Allows for editing and deleting scheduled events created by all users. */
     public const MANAGE_EVENTS = 33;
+    /** Allows for deleting and archiving threads, and viewing all private threads. */
     public const MANAGE_THREADS = 34;
+    /** Allows for creating public and announcement threads. */
     public const CREATE_PUBLIC_THREADS = 35;
+    /** Allows for creating private threads. */
     public const CREATE_PRIVATE_THREADS = 36;
+    /** Allows the usage of custom stickers from other servers. */
     public const USE_EXTERNAL_STICKERS = 37;
+    /** Allows for sending messages in threads. */
     public const SEND_MESSAGES_IN_THREADS = 38;
+    /** Allows for using Activities (applications with the EMBEDDED flag). */
     public const USE_EMBEDDED_ACTIVITIES = 39;
+    /** Allows for timing out users to prevent them from sending or reacting to messages in chat and threads, and from speaking in voice and stage channels. */
     public const MODERATE_MEMBERS = 40;
+    /** Allows for viewing role subscription insights. */
     public const VIEW_CREATOR_MONETIZATION_ANALYTICS = 41;
+    /** Allows for using soundboard in a voice channel. */
     public const USE_SOUNDBOARD = 42;
+    /** Allows for creating emojis, stickers, and soundboard sounds, and editing and deleting those created by the current user. Not yet available to developers. */
     public const CREATE_GUILD_EXPRESSIONS = 43;
+    /** Allows for creating scheduled events, and editing and deleting those created by the current user. Not yet available to developers. */
     public const CREATE_EVENTS = 44;
+    /** Allows the usage of custom soundboard sounds from other servers. */
     public const USE_EXTERNAL_SOUNDS = 45;
+    /** Allows sending voice messages. */
     public const SEND_VOICE_MESSAGES = 46;
+    /** Allows sending polls. */
     public const SEND_POLLS = 49;
+    /** Allows user-installed apps to send public responses. When disabled, users will still be allowed to use their apps but the responses will be ephemeral. This only applies to apps not also installed to the server. */
     public const USE_EXTERNAL_APPS = 50;
+    /** Allows pinning and unpinning messages. */
     public const PIN_MESSAGES = 51;
 
 
