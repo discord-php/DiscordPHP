d7476de4deb3cb23fec04a112cd4b8028ae84025
Valithor Obsidion <valzargaming@gmail.com>
2025-08-23 23:20:21 -0400

    Interaction Classes

---
+++ src/Discord/Parts/Interactions/ApplicationCommandAutocomplete.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Parts\Interactions;
+
+class ApplicationCommandAutocomplete extends Interaction
+{
+    /**
+     * Type of the interaction.
+     *
+     * @var int
+     */
+    protected $type = Interaction::TYPE_APPLICATION_COMMAND_AUTOCOMPLETE;
+}
---
+++ src/Discord/Parts/Interactions/MessageComponent.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Parts\Interactions;
+
+class MessageComponent extends Interaction
+{
+    /**
+     * Type of the interaction.
+     *
+     * @var int
+     */
+    protected $type = Interaction::TYPE_MESSAGE_COMPONENT;
+}
