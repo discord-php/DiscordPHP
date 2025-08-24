587eb19a7afabf01a5e5518956f7b79b581d606e
Valithor Obsidion <valzargaming@gmail.com>
2025-08-23 23:51:39 -0400

    Update ApplicationCommandData.php

--- src/Discord/Parts/Interactions/ApplicationCommandData.php
+++ src/Discord/Parts/Interactions/ApplicationCommandData.php
@@ -20,15 +20,15 @@ use Discord\Parts\Part;
  *
  * @link https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object-interaction-data
  *
- * @property      string            $id        ID of the invoked command
- * @property      string            $name      name of the invoked command
- * @property      int               $type      type of the invoked command
- * @property      array             $resolved  Converted users + roles + channels + attachments
- * @property      array             $options   Params + values from the user
- * @property      string            $guild_id  ID of the guild the command is registered to
- * @property      string            $target_id ID of the user or message targeted by a user or message command
- * @property-read Guild|null        $guild     The guild the command is registered to
- * @property-read User|Message|null $target    The user or message targeted by the command
+ * @property      string            $id        ID of the invoked command.
+ * @property      string            $name      name of the invoked command.
+ * @property      int               $type      type of the invoked command.
+ * @property      array             $resolved  Converted users + roles + channels + attachments.
+ * @property      array             $options   Params + values from the user.
+ * @property      string            $guild_id  ID of the guild the command is registered to.
+ * @property      string            $target_id ID of the user or message targeted by a user or message command.
+ * @property-read Guild|null        $guild     The guild the command is registered to.
+ * @property-read User|Message|null $target    The user or message targeted by the command.
  */
 class ApplicationCommandData extends Part
 {
