ba7f405b578e6fd35390928aa3bfe648dc81ebd9
Valithor Obsidion <valzargaming@gmail.com>
2025-08-26 08:37:14 -0400

    Update UserSelect.php

--- src/Discord/Builders/Components/UserSelect.php
+++ src/Discord/Builders/Components/UserSelect.php
@@ -19,6 +19,14 @@ namespace Discord\Builders\Components;
  * @link https://discord.com/developers/docs/components/reference#user-select
  *
  * @since 10.0.0
+ *
+ * @property int          $type            5 for a user select.
+ * @property string       $custom_id       ID for the select menu; max 100 characters.
+ * @property ?string|null $placeholder     Placeholder text if nothing is selected; max 150 characters.
+ * @property ?array|null  $default_values  List of default values for auto-populated select menu components; number of default values must be in the range defined by min_values and max_values.
+ * @property ?int|null    $min_values      Minimum number of items that must be chosen (defaults to 1); min 0, max 25.
+ * @property ?int|null    $max_values      Maximum number of items that can be chosen (defaults to 1); max 25.
+ * @property ?bool|null   $disabled        Whether select menu is disabled (defaults to false).
  */
 class UserSelect extends SelectMenu
 {
@@ -34,11 +42,11 @@ class UserSelect extends SelectMenu
     /**
      * Set if this component is required to be filled, default false. (Modal only).
      *
-     * @param bool $required
+     * @param bool|null $required
      *
      * @return $this
      */
-    public function setRequired(bool $required): self
+    public function setRequired(?bool $required = null): self
     {
         $this->required = $required;
 
