c48b404388956b9e971c4e1b1bf8a26c7c85ebfe
Valithor Obsidion <valzargaming@gmail.com>
2025-08-29 12:27:36 -0400

    Create Provider.php

---
+++ src/Discord/Parts/Embed/Provider.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Parts\Embed;
+
+use Discord\Parts\Part;
+
+/**
+ * The provider of an embed object.
+ *
+ * @link https://discord.com/developers/docs/resources/message#embed-object-embed-provider-structure
+ *
+ * @since 10.19.0
+ *
+ * @property ?string|null $name The name of the provider.
+ * @property ?string|null $url  The URL of the provider.
+ */
+class Provider extends Part
+{
+    /**
+     * @inheritDoc
+     */
+    protected $fillable = [
+        'name',
+        'url',
+    ];
+}
