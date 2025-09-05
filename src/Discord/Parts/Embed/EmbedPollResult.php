2910cede852a96c45d1c337802854d2bcbebeeab
Valithor Obsidion <valzargaming@gmail.com>
2025-09-04 21:02:12 -0400

    fx fixer

--- src/Discord/Parts/Embed/EmbedPollResult.php
+++ src/Discord/Parts/Embed/EmbedPollResult.php
@@ -32,13 +32,13 @@ use Discord\Helpers\ExCollectionInterface;
  * @property      ExCollectionInterface|Field[] $fields                                      A collection of embed fields (max of 25).
  * @property-read array                         $poll_fields                                 A collection of poll fields.
  * @property-read string|null                   $poll_fields['poll_question_text']           Question text from the original poll
- * @property-read  int|null                      $poll_fields['victor_answer_votes']          Number of votes for the answer(s) with the most votes
- * @property-read  int|null                      $poll_fields['total_votes']                  Total number of votes in the poll
- * @property-read  ?string|null                  $poll_fields['victor_answer_id']             ID for the winning answer (optional)
- * @property-read  ?string|null                  $poll_fields['victor_answer_text']           Text for the winning answer (optional)
- * @property-read  ?string|null                  $poll_fields['victor_answer_emoji_id']       ID for an emoji associated with the winning answer (optional)
- * @property-read  ?string|null                  $poll_fields['victor_answer_emoji_name']     Name of an emoji associated with the winning answer (optional)
- * @property-read  ?bool|null                    $poll_fields['victor_answer_emoji_animated'] If an emoji associated with the winning answer is animated (optional)
+ * @property-read int|null                      $poll_fields['victor_answer_votes']          Number of votes for the answer(s) with the most votes
+ * @property-read int|null                      $poll_fields['total_votes']                  Total number of votes in the poll
+ * @property-read ?string|null                  $poll_fields['victor_answer_id']             ID for the winning answer (optional)
+ * @property-read ?string|null                  $poll_fields['victor_answer_text']           Text for the winning answer (optional)
+ * @property-read ?string|null                  $poll_fields['victor_answer_emoji_id']       ID for an emoji associated with the winning answer (optional)
+ * @property-read ?string|null                  $poll_fields['victor_answer_emoji_name']     Name of an emoji associated with the winning answer (optional)
+ * @property-read ?bool|null                    $poll_fields['victor_answer_emoji_animated'] If an emoji associated with the winning answer is animated (optional)
  */
 class EmbedPollResult extends Embed
 {
@@ -54,7 +54,7 @@ class EmbedPollResult extends Embed
 
         $fields = array_filter(
             $this->attributes['fields'] ?? [],
-            fn($key) => !in_array($key, ['name', 'value', 'inline']),
+            fn ($key) => ! in_array($key, ['name', 'value', 'inline']),
             ARRAY_FILTER_USE_KEY
         );
 
