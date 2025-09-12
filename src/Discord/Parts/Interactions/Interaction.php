607041ca0321ef09732deb2165e5f0c753173dd9
Valithor Obsidion <valzargaming@gmail.com>
2025-09-12 04:22:44 -0400

    Deprecate Interaction::createListener

--- src/Discord/Parts/Interactions/Interaction.php
+++ src/Discord/Parts/Interactions/Interaction.php
@@ -723,6 +723,8 @@ class Interaction extends Part
      * @param int|float|null $timeout   Optional timeout in seconds after which the listener will be removed. (Mandatory for modal submit interactions)
      *
      * @return callable The listener callback to be registered for interaction events.
+     *
+     * @deprecated 10.19.3 Use `ModalSubmit::createListener()`
      */
     protected function createListener(string $custom_id, callable $submit, int|float|null $timeout = null): callable
     {
