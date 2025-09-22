88c5258f5014632d7f94102df48bd1671fbdfe00
Valithor Obsidion <valzargaming@gmail.com>
2025-09-22 14:24:21 -0400

    Update Application.php

--- src/Discord/Parts/OAuth/Application.php
+++ src/Discord/Parts/OAuth/Application.php
@@ -32,36 +32,36 @@ use function React\Promise\reject;
  *
  * @since 7.0.0
  *
- * @property string        $id                                The client ID of the OAuth application.
- * @property string        $name                              The name of the OAuth application.
- * @property string|null   $icon                              The icon URL of the OAuth application.
- * @property string        $icon_hash                         The icon hash of the OAuth application.
- * @property string        $description                       The description of the OAuth application.
- * @property string[]      $rpc_origins                       An array of RPC origin URLs.
- * @property bool          $bot_public                        When false only app owner can join the app's bot to guilds.
- * @property bool          $bot_require_code_grant            When true the app's bot will only join upon completion of the full oauth2 code grant flow.
- * @property User|null     $bot                               The partial user object for the bot user associated with the application.
- * @property string|null   $terms_of_service_url              The url of the app's terms of service.
- * @property string|null   $privacy_policy_url                The url of the app's privacy policy
- * @property User|null     $owner                             The owner of the OAuth application.
- * @property string        $verify_key                        The hex encoded key for verification in interactions and the GameSDK's GetTicket.
- * @property Team|null     $team                              If the application belongs to a team, this will be a list of the members of that team.
- * @property string|null   $guild_id                          If this application is a game sold on Discord, this field will be the guild to which it has been linked.
- * @property string|null   $primary_sku_id                    If this application is a game sold on Discord, this field will be the id of the "Game SKU" that is created, if exists.
- * @property string|null   $slug                              If this application is a game sold on Discord, this field will be the URL slug that links to the store page.
- * @property string|null   $cover_image                       The application's default rich presence invite cover image URL.
- * @property string|null   $cover_image_hash                  The application's default rich presence invite cover image hash.
- * @property int           $flags                             The application's public flags.
- * @property int|null      $approximate_guild_count           The application's approximate count of the app's guild membership.
- * @property int|null      $approximate_user_install_count    The approximate count of users that have installed the app.
- * @property string[]|null $redirect_uris                     Array of redirect URIs for the application.
- * @property string|null   $interactions_endpoint_url         The interactions endpoint URL for the application.
- * @property string|null   $role_connections_verification_url The application's role connection verification entry point, which when configured will render the app as a verification method in the guild role verification configuration.
- * @property string[]|null $tags                              Up to 5 tags describing the content and functionality of the application.
- * @property object|null   $install_params                    Settings for the application's default in-app authorization link, if enabled.
- * @property int[]|null    $integration_types
- * @property object[]|null $integration_types_config          Default scopes and permissions for each supported installation context. Value for each key is an integration type configuration object.
- * @property string|null   $custom_install_url                The application's default custom authorization link, if enabled.
+ * @property string             $id                                The client ID of the OAuth application.
+ * @property string             $name                              The name of the OAuth application.
+ * @property string|null        $icon                              The icon URL of the OAuth application.
+ * @property string             $icon_hash                         The icon hash of the OAuth application.
+ * @property string             $description                       The description of the OAuth application.
+ * @property string[]           $rpc_origins                       An array of RPC origin URLs.
+ * @property bool               $bot_public                        When false only app owner can join the app's bot to guilds.
+ * @property bool               $bot_require_code_grant            When true the app's bot will only join upon completion of the full oauth2 code grant flow.
+ * @property User|null          $bot                               The partial user object for the bot user associated with the application.
+ * @property string|null        $terms_of_service_url              The url of the app's terms of service.
+ * @property string|null        $privacy_policy_url                The url of the app's privacy policy
+ * @property User|null          $owner                             The owner of the OAuth application.
+ * @property string             $verify_key                        The hex encoded key for verification in interactions and the GameSDK's GetTicket.
+ * @property Team|null          $team                              If the application belongs to a team, this will be a list of the members of that team.
+ * @property string|null        $guild_id                          If this application is a game sold on Discord, this field will be the guild to which it has been linked.
+ * @property string|null        $primary_sku_id                    If this application is a game sold on Discord, this field will be the id of the "Game SKU" that is created, if exists.
+ * @property string|null        $slug                              If this application is a game sold on Discord, this field will be the URL slug that links to the store page.
+ * @property string|null        $cover_image                       The application's default rich presence invite cover image URL.
+ * @property string|null        $cover_image_hash                  The application's default rich presence invite cover image hash.
+ * @property int                $flags                             The application's public flags.
+ * @property int|null           $approximate_guild_count           The application's approximate count of the app's guild membership.
+ * @property int|null           $approximate_user_install_count    The approximate count of users that have installed the app.
+ * @property string[]|null      $redirect_uris                     Array of redirect URIs for the application.
+ * @property string|null        $interactions_endpoint_url         The interactions endpoint URL for the application.
+ * @property string|null        $role_connections_verification_url The application's role connection verification entry point, which when configured will render the app as a verification method in the guild role verification configuration.
+ * @property string[]|null      $tags                              Up to 5 tags describing the content and functionality of the application.
+ * @property InstallParams|null $install_params                    Settings for the application's default in-app authorization link, if enabled.
+ * @property int[]|null         $integration_types
+ * @property object[]|null      $integration_types_config          Default scopes and permissions for each supported installation context. Value for each key is an integration type configuration object.
+ * @property string|null        $custom_install_url                The application's default custom authorization link, if enabled.
  *
  * @property string $invite_url The invite URL to invite the bot to a guild.
  *
@@ -283,6 +283,16 @@ class Application extends Part
     protected function getCoverImageHashAttribute(): ?string
     {
         return $this->attributes['cover_image'];
+    }
+
+    /**
+     * Returns the install params attribute.
+     *
+     * @return InstallParams|null The install params or null.
+     */
+    protected function getInstallParamsAttribute(): ?InstallParams
+    {
+        return $this->attributePartHelper('install_params', InstallParams::class);
     }
 
     /**
