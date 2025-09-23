e48c011d3d8c250f3f83933794304baeba115d06
Valithor Obsidion <valzargaming@gmail.com>
2025-09-23 06:56:11 -0400

    Lobby and Member

---
+++ src/Discord/Parts/Lobby/Lobby.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Parts\Lobby;
+
+use Discord\Parts\Channel\Channel;
+use Discord\Parts\Part;
+
+/**
+ * Represents a lobby within Discord. See Managing Lobbies for more information.
+ *
+ * @since 10.28.0
+ *
+ * @link https://discord.com/developers/docs/resources/lobby#lobby-object
+ *
+ * @property      string       $id             The unique identifier of the lobby.
+ * @property      string       $application_id The application that created the lobby.
+ * @property      array|null   $metadata       Dictionary of string key/value pairs. The max total length is 1000.
+ * @property      array        $members        Members of the lobby.
+ * @property-read Channel|null $linked_channel The guild channel linked to the lobby.
+ */
+class Lobby extends Part
+{
+    /**
+     * @inheritDoc
+     */
+    protected $fillable = [
+        'id',
+        'application_id',
+        'metadata',
+        'members',
+        'linked_channel',
+    ];
+
+    /**
+     * Gets the linked_channel attribute.
+     *
+     * @return Channel|null The guild channel linked to the lobby.
+     */
+    protected function getLinkedChannelAttribute(): ?Channel
+    {
+        return $this->attributePartHelper('linked_channel', Channel::class);
+    }
+}
---
+++ src/Discord/Parts/Lobby/Member.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Parts\Lobby;
+
+use Discord\Parts\Part;
+
+/**
+ * Represents a member of a lobby, including optional metadata and flags.
+ *
+ * @since 10.28.0
+ *
+ * @link https://discord.com/developers/docs/resources/lobby#lobby-object
+ *
+ * @property string      $id       The unique identifier of the user.
+ * @property ?array|null $metadata Dictionary of string key/value pairs. The max total length is 1000.
+ * @property ?int|null   $flags    Lobby member flags combined as a bitfield.
+ */
+class Member extends Part
+{
+    /** User can link a text channel to a lobby. */
+    public const FLAG_CAN_LINK_LOBBY = 1 << 0;
+
+    /**
+     * @inheritDoc
+     */
+    protected $fillable = [
+        'id',
+        'application_id',
+        'metadata',
+        'members',
+        'linked_channel',
+    ];
+}
