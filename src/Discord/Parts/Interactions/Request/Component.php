eb4737371c4a979161adb6fc244c8c3b27b9baac
Valithor Obsidion <valzargaming@gmail.com>
2025-09-30 16:57:29 -0400

    Update Component.php

--- src/Discord/Parts/Interactions/Request/Component.php
+++ src/Discord/Parts/Interactions/Request/Component.php
@@ -78,15 +78,15 @@ class Component extends Part
      */
     protected function getComponentsAttribute(): ExCollectionInterface
     {
-        $components = Collection::for(Component::class);
+        $components = Collection::for(MessageComponent::class);
 
         if (! isset($this->attributes['components'])) {
             return $components;
         }
 
         foreach ($this->attributes['components'] as &$component) {
-            if (! $component instanceof Component) {
-                $component = $this->createOf(Component::TYPES[$component->type ?? 0], $component);
+            if (! $component instanceof MessageComponent) {
+                $component = $this->createOf(MessageComponent::TYPES[$component->type ?? 0], $component);
             }
             $components->pushItem($component);
         }
