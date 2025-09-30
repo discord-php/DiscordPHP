3264e92e8b900fc14bec62f78dfc3719857e32bd
Valithor Obsidion <valzargaming@gmail.com>
2025-09-30 16:44:45 -0400

    GuildAuditLogEntryCreate attempts to cache entries
    If the audit log isn't already cached it won't cache anything. This is by design such that only bots that actually utilize the audit log  and have freshened it at least once will have the new entries in memory.

--- src/Discord/WebSockets/Events/GuildAuditLogEntryCreate.php
+++ src/Discord/WebSockets/Events/GuildAuditLogEntryCreate.php
@@ -13,8 +13,10 @@ declare(strict_types=1);
 
 namespace Discord\WebSockets\Events;
 
+use Discord\Parts\Guild\AuditLog\AuditLog;
 use Discord\WebSockets\Event;
 use Discord\Parts\Guild\AuditLog\Entry;
+use Discord\Parts\Guild\Guild;
 
 /**
  * @link https://discord.com/developers/docs/topics/gateway-events#guild-audit-log-entry-create
@@ -30,6 +32,21 @@ class GuildAuditLogEntryCreate extends Event
     {
         /** @var Entry */
         $entryPart = $this->factory->part(Entry::class, (array) $data, true);
+
+        /** @var ?Guild */
+        if ($guild = yield $this->discord->guilds->cacheGet($data->guild_id ?? null)) {
+            /** @var Guild $guild */
+            /** @var ?AuditLog */
+            if ($audit_log = yield $guild->audit_log->cacheGet('')) {
+                /** @var AuditLog $audit_log */
+                $entries = $audit_log->audit_log_entries;
+                $entries->pushItem($entryPart);
+                $audit_log->audit_log_entries = $entries;
+                yield $guild->audit_log->cache->set('', $audit_log);
+            }
+        }
+
+        $this->cacheUser($data->user);
 
         return $entryPart;
     }
