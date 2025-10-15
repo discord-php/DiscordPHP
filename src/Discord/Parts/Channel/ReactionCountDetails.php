c0e34c6afd984c1036606fcdde124becdcf995eb
Valithor Obsidion <valzargaming@gmail.com>
2025-10-15 13:40:20 -0400

    Create ReactionCountDetails.php

---
+++ src/Discord/Parts/Channel/ReactionCountDetails.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Parts\Channel;
+
+use Discord\Parts\Part;
+
+/**
+ * The reaction count details object contains a breakdown of normal and super reaction counts for the associated emoji.
+ *
+ * @link https://discord.com/developers/docs/resources/message#reaction-count-details-object
+ *
+ * @since 10.36.29
+ *
+ * @property int $burst  Count of super reactions.
+ * @property int $normal Count of normal reactions.
+ */
+class ReactionCountDetails extends Part
+{
+    /**
+     * @inheritDoc
+     */
+    protected $fillable = [
+        'burst',
+        'normal',
+    ];
+}
