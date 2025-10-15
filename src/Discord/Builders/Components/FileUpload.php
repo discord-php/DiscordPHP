d5a726fe38e105dd7900cd5589e901df613af477
Valithor Obsidion <valzargaming@gmail.com>
2025-10-15 16:46:43 -0400

    Update FileUpload.php

--- src/Discord/Builders/Components/FileUpload.php
+++ src/Discord/Builders/Components/FileUpload.php
@@ -21,10 +21,10 @@ namespace Discord\Builders\Components;
  * @since 10.21.0
  *
  * @property int        $type       19 for File Upload component.
- * @property string     $custom_id  Developer-defined identifier, max 100 characters.
+ * @property string     $custom_id  ID for the file upload; max 100 characters.
  * @property ?int|null  $min_values Minimum number of files that must be uploaded (defaults to 1); min 0, max 10.
  * @property ?int|null  $max_values Maximum number of files that can be uploaded (defaults to 1); max 10.
- * @property ?bool|null $required   Whether the file upload is required to be filled in a modal (defaults to true).
+ * @property ?bool|null $required   Whether the file upload is required to be filled in a modal (defaults to `true`).
  */
 class FileUpload extends Interactive
 {
