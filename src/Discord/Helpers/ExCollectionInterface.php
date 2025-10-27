48db5fb3d3cf9bd5ac4c2384333227ae6b38f4c8
Valithor Obsidion <valzargaming@gmail.com>
2025-10-27 15:58:09 -0400

    PHPDocs

--- src/Discord/Helpers/ExCollectionInterface.php
+++ src/Discord/Helpers/ExCollectionInterface.php
@@ -20,14 +20,22 @@ interface ExCollectionInterface extends CollectionInterface
     public function find_key(callable $callback);
     public function any(callable $callback): bool;
     public function all(callable $callback): bool;
+    /** @return ExCollectionInterface */
     public function splice(int $offset, ?int $length, mixed $replacement = []): self;
     public function clear(): void;
+    /** @return ExCollectionInterface */
     public function slice(int $offset, ?int $length = null, bool $preserve_keys = false);
+    /** @return ExCollectionInterface */
     public function sort(callable|int|null $callback);
+    /** @return ExCollectionInterface */
     public function diff($items, ?callable $callback = null);
+    /** @return ExCollectionInterface */
     public function intersect($items, ?callable $callback = null);
+    /** @return ExCollectionInterface */
     public function walk(callable $callback, mixed $arg);
+    /** @return ExCollectionInterface */
     public function reduce(callable $callback, $initial = null);
+    /** @return ExCollectionInterface */
     public function unique(int $flags = SORT_STRING);
     public function keys(): array;
     public function values(): array;
