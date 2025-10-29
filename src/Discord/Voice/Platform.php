a0cda0c67e1e09a4d4f1447d31a8a5d56a5b7d9a
Valithor Obsidion <valzargaming@gmail.com>
2025-10-29 12:06:56 -0400

    Create Platform.php

---
+++ src/Discord/Voice/Platform.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Voice;
+
+use Discord\Parts\Part;
+use Discord\Parts\User\User;
+
+/**
+ * Represents a user's platform.
+ * 
+ * Undocumented.
+ *
+ * @since 10.40.0
+ *
+ * @property      string      $user_id  The ID of the user.
+ * @property-read User        $user     The user.
+ * @property      int         $platform The platform for the user.
+ */
+class Platform extends Part
+{
+    const PLATFORM_DESKTOP = 0;
+    const PLATFORM_MOBILE = 1;
+
+    /**
+     * @inheritDoc
+     */
+    protected $fillable = [
+        'user_id',
+        'platform',
+    ];
+
+    /**
+     * Get the user attribute.
+     *
+     * @return User|null The user.
+     */
+    protected function getUserAttribute(): ?User
+    {
+        if (! isset($this->attributes['user_id'])) {
+            return $this->discord->user;
+        }
+
+        return $this->discord->users->get('id', $this->attributes['user_id']);
+    }
+}
