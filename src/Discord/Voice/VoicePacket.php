b9a15c84f63a131267e1f21a3aa2d64a0777f7c1
Valithor Obsidion <valzargaming@gmail.com>
2025-10-29 15:13:56 -0400

    Remove poly_strlen from VoicePacket

--- src/Discord/Voice/VoicePacket.php
+++ src/Discord/Voice/VoicePacket.php
@@ -13,8 +13,6 @@ declare(strict_types=1);
 
 namespace Discord\Voice;
 
-use function Discord\poly_strlen;
-
 /**
  * A voice packet received from Discord.
  *
@@ -99,7 +97,7 @@ class VoicePacket
         $data = (string) $data;
         $header = $this->buildHeader();
 
-        $buffer = new Buffer(poly_strlen((string) $header, '8bit') + poly_strlen($data, '8bit'));
+        $buffer = new Buffer(strlen((string) $header) + strlen($data));
         $buffer->write((string) $header, 0);
         $buffer->write($data, 12);
 
@@ -121,7 +119,7 @@ class VoicePacket
 
         $data = \sodium_crypto_secretbox($data, (string) $nonce, $key);
 
-        $this->buffer = new Buffer(poly_strlen((string) $header, '8bit') + poly_strlen($data, '8bit'));
+        $this->buffer = new Buffer(strlen((string) $header) + strlen($data));
         $this->buffer->write((string) $header, 0);
         $this->buffer->write($data, 12);
     }
@@ -190,7 +188,7 @@ class VoicePacket
      */
     public function getData(): string
     {
-        return $this->buffer->read(self::RTP_HEADER_BYTE_LENGTH, poly_strlen((string) $this->buffer, '8bit') - self::RTP_HEADER_BYTE_LENGTH);
+        return $this->buffer->read(self::RTP_HEADER_BYTE_LENGTH, strlen((string) $this->buffer) - self::RTP_HEADER_BYTE_LENGTH);
     }
 
     /**
