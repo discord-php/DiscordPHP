dec63b7018019790fdb1954c4168face6d337054
Valithor Obsidion <valzargaming@gmail.com>
2025-11-06 10:02:58 -0500

    Create Resumed.php

---
+++ src/Discord/Voice/Resumed.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Voice;
+
+use Discord\Parts\Part;
+
+/**
+ * Represents the resumed data.
+ * 
+ * No date is actually sent with this payload, but having a Part makes it easier to log and handle.
+ * 
+ * @link https://discord.com/developers/docs/topics/voice-connections#resuming-voice-connection-example-resumed-payload
+ *
+ * @since 10.41.0
+ */
+class Resumed extends Part
+{
+}
