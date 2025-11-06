2dfc8e33fb8a006970ba1110b43ce20315ed5dc8
Valithor Obsidion <valzargaming@gmail.com>
2025-11-06 09:33:44 -0500

    Create Ready.php

---
+++ src/Discord/Voice/Ready.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Voice;
+
+use Discord\Parts\Part;
+
+/**
+ * Represents the ready data.
+ *
+ * @since 10.41.0
+ *
+ * @property array    $streams     The streams information.
+ * @property int      $ssrc        The SSRC identifier.
+ * @property int      $port        The port number.
+ * @property string[] $modes       The supported encryption modes.
+ * @property string   $ip          The IP address.
+ * @property string[] $experiments The list of experiments.
+ *
+ * @property-read string $type     The type of the stream.
+ * @property-read int    $ssrc     The SSRC of the stream.
+ * @property-read int    $rtx_ssrc The RTX SSRC of the stream.
+ * @property-read string $rid      The RID of the stream.
+ * @property-read int    $quality  The quality of the stream.
+ * @property-read bool   $active   Whether the stream is active.
+ */
+class Ready extends Part
+{
+    /**
+     * @inheritDoc
+     */
+    protected $fillable = [
+        'streams',
+        'ssrc',
+        'port',
+        'modes',
+        'ip',
+        'experiments',
+    ];
+
+    /**
+     * Get the type of the stream.
+     *
+     * @return string The type of the stream.
+     */
+    public function getTypeAttribute(): string
+    {
+        return $this->attributes['streams'][0]['type'];
+    }
+
+    /**
+     * Get the SSRC of the stream.
+     *
+     * @return int The SSRC of the stream.
+     */
+    public function getSsrcAttribute(): int
+    {
+        return $this->attributes['streams'][0]['ssrc'];
+    }
+
+    /**
+     * Get the RTX SSRC of the stream.
+     *
+     * @return int The RTX SSRC of the stream.
+     */
+    public function getRtxSsrcAttribute(): int
+    {
+        return $this->attributes['streams'][0]['rtx_ssrc'];
+    }
+
+    /**
+     * Get the RID of the stream.
+     *
+     * @return string The RID of the stream.
+     */
+    public function getRidAttribute(): string
+    {
+        return $this->attributes['streams'][0]['rid'];
+    }
+
+    /**
+     * Get the quality of the stream.
+     *
+     * @return int The quality of the stream.
+     */
+    public function getQualityAttribute(): int
+    {
+        return $this->attributes['streams'][0]['quality'];
+    }
+
+    /**
+     * Get whether the stream is active.
+     *
+     * @return bool Whether the stream is active.
+     */
+    public function getActiveAttribute(): bool
+    {
+        return $this->attributes['streams'][0]['active'];
+    }
+}
