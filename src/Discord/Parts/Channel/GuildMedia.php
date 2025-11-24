b9381cbd4bbed787f39e31ae2d3c069c439a2a2c
Valithor Obsidion <valzargaming@gmail.com>
2025-11-24 14:18:59 -0500

    PHPDocs

--- src/Discord/Parts/Channel/GuildMedia.php
+++ src/Discord/Parts/Channel/GuildMedia.php
@@ -15,6 +15,9 @@ namespace Discord\Parts\Channel;
 
 /**
  * Channel that can only contain threads, similar to GUILD_FORUM channels.
+ * 
+ * The GUILD_MEDIA channel type is still in active development.
+ * Avoid implementing any features that are not documented, since they are subject to change without notice!
  */
 class GuildMedia extends Channel
 {
