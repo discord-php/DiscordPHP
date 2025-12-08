64e967c28b5dbd092b34472127bfa142b7990fbc
Valithor Obsidion <valzargaming@gmail.com>
2025-12-08 01:20:05 -0500

    Check if voice is set before accessing property

--- src/Discord/Discord.php
+++ src/Discord/Discord.php
@@ -673,7 +673,7 @@ class Discord
         $voiceStateUpdate = $this->factory->part(VoiceStateUpdate::class, (array) $data->d, true);
 
         $this->logger->debug('voice state update received', ['guild' => $voiceStateUpdate->guild_id, 'data' => $voiceStateUpdate]);
-        if (isset($this->voice->clients[$voiceStateUpdate->guild_id])) {
+        if (isset($this->voice, $this->voice->clients[$voiceStateUpdate->guild_id])) {
             /** @var VoiceClient */
             $client = $this->voice->clients[$voiceStateUpdate->guild_id];
             $client->handleVoiceStateUpdate($voiceStateUpdate);
