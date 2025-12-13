10940fe49e78b9ebf7a5466991191d30dd0cfcd7
Valithor Obsidion <valzargaming@gmail.com>
2025-12-13 01:33:32 -0500

    GuildTemplate::getRepository(): GuildTemplateRepository

--- src/Discord/Parts/Guild/GuildTemplate.php
+++ src/Discord/Parts/Guild/GuildTemplate.php
@@ -18,6 +18,7 @@ use Discord\Http\Endpoint;
 use Discord\Http\Exceptions\NoPermissionsException;
 use Discord\Parts\Part;
 use Discord\Parts\User\User;
+use Discord\Repository\Guild\GuildTemplateRepository;
 use DomainException;
 use React\Promise\PromiseInterface;
 use Stringable;
@@ -249,6 +250,21 @@ class GuildTemplate extends Part implements Stringable
         }
 
         return $guild->templates->sync($this->code);
+    }
+
+    /**
+     * Gets the originating repository of the part.
+     *
+     * @throws \Exception If the part does not have an originating repository.
+     *
+     * @return GuildTemplateRepository The repository.
+     */
+    public function getRepository(): GuildTemplateRepository
+    {
+        /** @var Guild $guild */
+        $guild = $this->guild ?? $this->factory->part(Guild::class, ['id' => $this->attributes['source_guild_id']], true);
+
+        return $guild->templates;
     }
 
     /**
