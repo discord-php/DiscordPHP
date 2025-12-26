09ec42caef5fa74538852263048f3d41f37af57a
Valithor Obsidion <valithor@discordphp.org>
2025-12-25 19:10:37 -0500

    Update Event.php

--- src/Discord/WebSockets/Event.php
+++ src/Discord/WebSockets/Event.php
@@ -181,10 +181,6 @@ abstract class Event
      */
     protected function cacheUser(object $userdata): void
     {
-        if (! isset($userdata)) {
-            return;
-        }
-
         $users = $this->discord->users;
         if ($user = $users->get('id', $userdata->id)) {
             $user->fill((array) $userdata);
