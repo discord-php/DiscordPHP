8e5403543261db966c4cab3eca1ceda97a64c1c0
Valithor Obsidion <valithor@discordphp.org>
2026-01-10 16:32:45 -0500

    Create GuildScheduledEventExceptionCreate.php

---
+++ src/Discord/WebSockets/Events/GuildScheduledEventExceptionCreate.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-present David Cole <david.cole1340@gmail.com>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\WebSockets\Events;
+
+use Discord\WebSockets\Event;
+use Discord\Parts\Guild\Guild;
+use Discord\Parts\Guild\ScheduledEventException;
+
+/**
+ * @link https://discord.com/developers/docs/topics/gateway-events#guild-scheduled-event-exception-create
+ *
+ * @since 10.45.1
+ */
+class GuildScheduledEventExceptionCreate extends Event
+{
+    /**
+     * @inheritDoc
+     */
+    public function handle($data)
+    {
+        /** @var ScheduledEventException */
+        $scheduledEventExceptionPart = $this->factory->part(ScheduledEventException::class, (array) $data, true);
+
+        /** @var ?Guild */
+        if ($guild = yield $this->discord->guilds->cacheGet($data->guild_id)) {
+            if ($event = $guild->guild_scheduled_events->get($scheduledEventExceptionPart->event_id)) {
+                $event->guild_scheduled_event_exceptions->set($scheduledEventExceptionPart->event_exception_id, $scheduledEventExceptionPart);
+            }
+        }
+
+        return $scheduledEventExceptionPart;
+    }
+}
