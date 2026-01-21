3264af6fd023d070f9e4fe62f8495d87a762382a
Valithor Obsidion <valithor@discordphp.org>
2026-01-21 06:53:36 -0500

    Abstract group logic

--- src/Discord/Builders/Components/Group.php
+++ src/Discord/Builders/Components/Group.php
@@ -24,10 +24,18 @@ namespace Discord\Builders\Components;
  * @property int       $type      21 for a radio group.
  * @property ?int|null $id        Optional identifier for component.
  * @property string    $custom_id Custom ID to send with interactive component.
+ * @property array     $options   List of options to render.
  * @property ?bool     $required  Whether selecting an option is required or not.
  */
 abstract class Group extends Interactive
 {
+    /**
+     * List of options to render.
+     *
+     * @var array
+     */
+    protected $options = [];
+
     /**
      * Whether selecting an option is required or not.
      *
@@ -36,9 +44,80 @@ abstract class Group extends Interactive
     protected $required;
 
     /**
+     * List of options to render.
+     *
+     * @param GroupOption[] $options List of GroupOption objects.
+     *
+     * @return $this
+     */
+    public function setOptions(array $options = []): self
+    {
+        foreach ($options as $option) {
+            $this->addOption($option);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Adds an option to the group. Maximum 10 options.
+     *
+     * @param GroupOption $option Option to add.
+     *
+     * @throws \OverflowException
+     * @throws \UnexpectedValueException
+     *
+     * @return $this
+     */
+    public function addOption($option): self
+    {
+        if (count($this->options) >= 10) {
+            throw new \OverflowException('You can only have 10 options per radio group.');
+        }
+
+        $value = $option->getValue();
+
+        foreach ($this->options as $other) {
+            if ($other->getValue() === $value) {
+                throw new \UnexpectedValueException('Another value already has the same value. These must not be the same.');
+            }
+        }
+
+        $this->options[] = $option;
+
+        return $this;
+    }
+
+    /**
+     * Removes an option from the group.
+     *
+     * @param GroupOption $option Option to remove.
+     *
+     * @return $this
+     */
+    public function removeOption($option): self
+    {
+        if (($idx = array_search($option, $this->options)) !== false) {
+            array_splice($this->options, $idx, 1);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Gets the options.
+     *
+     * @return array
+     */
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    /**
      * Set if this component is required to be filled, default false. (Modal only).
      *
-     * @param ?bool $required
+     * @param bool|null $required
      *
      * @return $this
      */
@@ -47,5 +126,15 @@ abstract class Group extends Interactive
         $this->required = $required;
 
         return $this;
+    }
+
+    /**
+     * Gets whether this component is required to be filled.
+     *
+     * @return bool|null
+     */
+    public function getRequired(): ?bool
+    {
+        return $this->required ?? null;
     }
 }
