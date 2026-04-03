75860d58bf8a77db953481981e6ba83c3387be2e
Valithor Obsidion <valithor@discordphp.org>
2026-04-03 16:07:57 -0400

    Create GameServerUpdate.php

---
+++ src/Discord/WebSockets/Events/GameServerUpdate.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-2022 David Cole <david.cole1340@gmail.com>
+ * Copyright (c) 2020-present Valithor Obsidion <valithor@discordphp.org>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\WebSockets\Events;
+
+use Discord\WebSockets\Event;
+use Discord\Parts\Monetization\GameServer;
+
+/**
+ * Handles GAME_SERVER_UPDATE gateway events.
+ *
+ * The payload contains a `game_server` object and a `guild_id`.
+ *
+ * @todo TBD
+ */
+class GameServerUpdate extends Event
+{
+    /**
+     * @inheritDoc
+     */
+    public function handle($data)
+    {
+        $oldGameServer = null;
+
+        /** @var GameServer */
+        $gameServerPart = $this->factory->part(GameServer::class, (array) $data + ['guild_id' => $data->guild_id ?? null], true);
+
+        /** @todo No persistent repository exists for game servers yet. */
+        return [$gameServerPart, $oldGameServer];
+    }
+}
