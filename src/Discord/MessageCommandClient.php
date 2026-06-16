4334df00a656091ea103d5f779f70e30deb3d42b
Valithor Obsidion <valithor@discordphp.org>
2026-06-16 08:20:53 -0400

    Initial prototype

---
+++ src/Discord/MessageCommandClient.php
@@ -0,0 +1,464 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-2022 David Cole <david.cole1340@gmail.com>
+ * Copyright (c) 2020-present Valithor Obsidion <valithor@discordphp.org>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord;
+
+use Discord\MessageCommandClient\CommandRegistry;
+use Discord\MessageCommandClient\Command;
+use Discord\Parts\Channel\Message;
+use Discord\Parts\Embed\Embed;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Modern, modular replacement for the legacy DiscordCommandClient.
+ *
+ * @since 10.49.0
+ */
+class MessageCommandClient extends Discord
+{
+    /**
+     * Options passed to the command client.
+     *
+     * @var array<string,mixed>
+     */
+    protected array $options;
+
+    /**
+     * Command registry storing commands and aliases.
+     *
+     * @var CommandRegistry
+     */
+    protected CommandRegistry $registry;
+
+    /**
+     * Construct the message command client.
+     *
+     * @param array $options Client options (see resolveOptions()).
+     */
+    public function __construct(array $options = [])
+    {
+        $this->options = $this->resolveOptions($options);
+
+        $discordOptions = array_merge($this->options['discordOptions'] ?? [], ['token' => $this->options['token']]);
+
+        parent::__construct($discordOptions);
+
+        $this->registry = new CommandRegistry((bool) ($this->options['caseInsensitiveCommands'] ?? false));
+
+        $this->on('init', function () {
+            $this->preparePrefixes();
+
+            $this->on('message', function ($message) {
+                $this->handleMessage($message);
+            });
+        });
+
+        if ($this->options['defaultHelpCommand']) {
+            $this->registerCommand('help', function ($message, $args) {
+                return $this->defaultHelpHandler($message, $args);
+            }, [
+                'description' => 'Provides a list of commands available.',
+                'usage' => '[command]',
+            ]);
+        }
+    }
+
+    /**
+     * Prepare and normalize prefixes, expanding @mention placeholders.
+     */
+    protected function preparePrefixes(): void
+    {
+        $this->options['prefix'] = str_replace('@mention', (string) $this->user, $this->options['prefix']);
+        $this->options['name'] = str_replace('<UsernamePlaceholder>', $this->username, $this->options['name']);
+
+        foreach ($this->options['prefixes'] as $key => $prefix) {
+            if (strpos($prefix, '@mention') !== false) {
+                $this->options['prefixes'][] = str_replace('@mention', "<@{$this->user->id}>", $prefix);
+                $this->options['prefixes'][] = str_replace('@mention', "<@!{$this->user->id}>", $prefix);
+                unset($this->options['prefixes'][$key]);
+            }
+        }
+    }
+
+    /**
+     * Handle an incoming message event and dispatch commands.
+     *
+     * @param Message $message The received message.
+     */
+    protected function handleMessage(Message $message): void
+    {
+        if ($message->author->id === $this->id) {
+            return;
+        }
+
+        $withoutPrefix = $this->checkForPrefix((string) $message->content);
+        if ($withoutPrefix === null) {
+            return;
+        }
+
+        $args = str_getcsv($withoutPrefix, ' ', '"', '\\');
+        $commandName = array_shift($args);
+
+        if ($commandName === null) {
+            return;
+        }
+
+        if ($this->options['caseInsensitiveCommands']) {
+            $commandName = mb_strtolower($commandName);
+        }
+
+        $command = $this->registry->get($commandName);
+        if ($command === null) {
+            return;
+        }
+
+        $result = $command($message, $args);
+
+        if (is_string($result)) {
+            $message->reply($result)->then(null, $this->options['internalRejectedPromiseHandler']);
+        }
+    }
+
+    /**
+     * Check whether the given content starts with a configured prefix.
+     *
+     * @param string $content Message content.
+     *
+     * @return string|null The content without the prefix, or null when no prefix matched.
+     */
+    protected function checkForPrefix(string $content): ?string
+    {
+        foreach ($this->options['prefixes'] as $prefix) {
+            $len = mb_strlen($prefix);
+            if (mb_substr($content, 0, $len) === $prefix) {
+                return mb_substr($content, $len);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Register a new command.
+     *
+     * @param string                $name     Command trigger name.
+     * @param callable|string|array $callable Callable, string or array to execute.
+     * @param array                 $options  Command options.
+     *
+     * @return Command The registered command instance.
+     */
+    public function registerCommand(string $name, $callable, array $options = []): Command
+    {
+        if ($this->options['caseInsensitiveCommands']) {
+            $name = mb_strtolower($name);
+        }
+
+        list($commandInstance, $resolved) = $this->buildCommand($name, $callable, $options);
+
+        $this->registry->add($name, $commandInstance);
+
+        foreach ($resolved['aliases'] as $alias) {
+            if ($this->options['caseInsensitiveCommands'] && $alias !== null) {
+                $alias = mb_strtolower($alias);
+            }
+            $this->registry->addAlias((string) $alias, $name);
+        }
+
+        return $commandInstance;
+    }
+
+    /**
+     * Unregister an existing command.
+     *
+     * @param string $name Command name to remove.
+     *
+     * @throws \RuntimeException If the command does not exist.
+     */
+    public function unregisterCommand(string $name): void
+    {
+        if (! $this->registry->has($name)) {
+            throw new \RuntimeException("A command with the name {$name} does not exist.");
+        }
+
+        $this->registry->remove($name);
+    }
+
+    /**
+     * Register an alias for a command.
+     *
+     * @param string $alias   Alias to register.
+     * @param string $command Target command name.
+     */
+    public function registerAlias(string $alias, string $command): void
+    {
+        $this->registry->addAlias($alias, $command);
+    }
+
+    /**
+     * Unregister a command alias.
+     *
+     * @param string $alias Alias to remove.
+     */
+    public function unregisterCommandAlias(string $alias): void
+    {
+        $this->registry->removeAlias($alias);
+    }
+
+    /**
+     * Get a command by name or alias.
+     *
+     * @param string $name Command name or alias.
+     *
+     * @return Command|null
+     */
+    public function getCommand(string $name): ?Command
+    {
+        return $this->registry->get($name);
+    }
+
+    /**
+     * Build a Command instance from provided parameters.
+     *
+     * @param string                $name     Command name.
+     * @param callable|string|array $callable Callable, string or array.
+     * @param array                 $options  Command options.
+     *
+     * @return array{0:Command,1:array} Tuple of Command instance and resolved options.
+     *
+     * @throws \InvalidArgumentException When callable is not valid.
+     */
+    public function buildCommand(string $name, $callable, array $options = []): array
+    {
+        if (is_string($callable)) {
+            $callable = function ($message) use ($callable) {
+                return $callable;
+            };
+        } elseif (is_array($callable) && ! is_callable($callable)) {
+            $callable = function ($message) use ($callable) {
+                return $callable[array_rand($callable)];
+            };
+        }
+
+        if (! is_callable($callable)) {
+            throw new \InvalidArgumentException('The callable parameter must be a string, array or callable.');
+        }
+
+        $options = $this->resolveCommandOptions($options);
+
+        $commandInstance = new Command(
+            $this,
+            $name,
+            $callable,
+            $options['description'],
+            $options['longDescription'],
+            $options['usage'],
+            $options['cooldown'],
+            $options['cooldownMessage'],
+            $options['showHelp']
+        );
+
+        return [$commandInstance, $options];
+    }
+
+    /**
+     * Resolve options for a single command.
+     *
+     * @param array $options Input options.
+     *
+     * @return array Resolved options.
+     */
+    protected function resolveCommandOptions(array $options): array
+    {
+        $resolver = new OptionsResolver();
+
+        $resolver
+            ->setDefined([
+                'description',
+                'longDescription',
+                'usage',
+                'aliases',
+                'cooldown',
+                'cooldownMessage',
+                'showHelp',
+            ])
+            ->setDefaults([
+                'description' => 'No description provided.',
+                'longDescription' => '',
+                'usage' => '',
+                'aliases' => [],
+                'cooldown' => 0,
+                'cooldownMessage' => 'please wait %d second(s) to use this command again.',
+                'showHelp' => true,
+            ]);
+
+        return $resolver->resolve($options);
+    }
+
+    /**
+     * Resolve client-level options.
+     *
+     * @param array $options Input options.
+     *
+     * @return array Resolved options used by the client.
+     */
+    protected function resolveOptions(array $options = []): array
+    {
+        $resolver = new OptionsResolver();
+
+        $resolver
+            ->setRequired('token')
+            ->setAllowedTypes('token', 'string')
+            ->setDefined([
+                'token',
+                'prefix',
+                'prefixes',
+                'name',
+                'description',
+                'defaultHelpCommand',
+                'discordOptions',
+                'caseInsensitiveCommands',
+                'internalRejectedPromiseHandler',
+            ])
+            ->setAllowedTypes('internalRejectedPromiseHandler', ['null', 'callable'])
+            ->setDefaults([
+                'prefix' => '@mention ',
+                'prefixes' => [],
+                'name' => '<UsernamePlaceholder>',
+                'description' => 'A bot made with DiscordPHP '.self::VERSION.'.',
+                'defaultHelpCommand' => true,
+                'discordOptions' => [],
+                'caseInsensitiveCommands' => false,
+                'internalRejectedPromiseHandler' => function ($reason): void {
+                    if (is_string($reason) || $reason instanceof \Stringable) {
+                        $this->getLogger()->error($reason);
+                    } else {
+                        $this->getLogger()->warning('Unhandled internal rejected promise, $reason is not a Throwable, '.gettype($reason).' given.');
+                    }
+                },
+            ]);
+
+        $resolved = $resolver->resolve($options);
+        $resolved['prefixes'][] = $resolved['prefix'];
+
+        return $resolved;
+    }
+
+    /**
+     * Default help command handler.
+     *
+     * @param Message $message Message instance.
+     * @param array   $args    Command path parts.
+     *
+     * @return mixed|null
+     */
+    protected function defaultHelpHandler($message, array $args)
+    {
+        $prefix = str_replace((string) $this->user, '@'.$this->username, $this->options['prefix']);
+
+        if (count($args) > 0) {
+            $command = $this;
+            while (count($args) > 0) {
+                $commandString = array_shift($args);
+                $newCommand = $this->getCommand($commandString);
+
+                if (null === $newCommand) {
+                    return "The command {$commandString} does not exist.";
+                }
+
+                $command = $newCommand;
+            }
+
+            $help = $command->getHelp($prefix);
+            if (empty($help)) {
+                return;
+            }
+
+            $embed = new Embed($this);
+            $embed->setAuthor($this->options['name'], $this->client->user->avatar)
+                ->setTitle($prefix.implode(' ', $args).'\'s Help')
+                ->setDescription(! empty($help['longDescription']) ? $help['longDescription'] : $help['description'])
+                ->setFooter($this->options['name']);
+
+            if (! empty($help['usage'])) {
+                $embed->addFieldValues('Usage', '``'.$help['usage'].'``', true);
+            }
+
+            $message->channel->sendEmbed($embed)->then(null, $this->options['internalRejectedPromiseHandler']);
+
+            return;
+        }
+
+        $embed = new Embed($this);
+        $embed->setAuthor($this->options['name'], $this->client->avatar)
+            ->setTitle($this->options['name'])
+            ->setType(Embed::TYPE_RICH)
+            ->setFooter($this->options['name']);
+
+        $commandsDescription = '';
+        $embedfields = [];
+        foreach ($this->registry->all() as $command) {
+            $help = $command->getHelp($prefix);
+            if (empty($help)) {
+                continue;
+            }
+
+            $embedfields[] = [
+                'name' => $help['command'],
+                'value' => $help['description'],
+                'inline' => true,
+            ];
+            $commandsDescription .= "\n\n`".$help['command'].'`\n'.$help['description'];
+
+            foreach ($help['subCommandsHelp'] as $subCommandHelp) {
+                $embedfields[] = [
+                    'name' => $subCommandHelp['command'],
+                    'value' => $subCommandHelp['description'],
+                    'inline' => true,
+                ];
+                $commandsDescription .= "\n\n`".$subCommandHelp['command'].'`\n'.$subCommandHelp['description'];
+            }
+        }
+
+        if (count($embedfields) <= 25) {
+            foreach ($embedfields as $field) {
+                $embed->addField($field);
+            }
+            $commandsDescription = '';
+        }
+
+        $embed->setDescription(mb_substr($this->options['description'].$commandsDescription, 0, 2048));
+
+        $message->channel->sendEmbed($embed)->then(null, $this->options['internalRejectedPromiseHandler']);
+    }
+
+    /**
+     * Return the resolved client options.
+     *
+     * @return array<string,mixed>
+     */
+    public function getCommandClientOptions(): array
+    {
+        return $this->options;
+    }
+
+    /**
+     * Return the command registry instance.
+     *
+     * @return CommandRegistry
+     */
+    public function getCommandRegistry(): CommandRegistry
+    {
+        return $this->registry;
+    }
+}
---
+++ src/Discord/MessageCommandClient/Command.php
@@ -0,0 +1,339 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-2022 David Cole <david.cole1340@gmail.com>
+ * Copyright (c) 2020-present Valithor Obsidion <valithor@discordphp.org>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\MessageCommandClient;
+
+use Discord\MessageCommandClient;
+use Discord\Parts\Channel\Message;
+
+/**
+ * A message-based command that the MessageCommandClient will listen for.
+ *
+ * @since 10.49.0
+ */
+class Command
+{
+    /**
+     * The trigger for the command.
+     *
+     * @var string
+     */
+    public string $command;
+
+    /**
+     * Short description shown in help listings.
+     *
+     * @var string
+     */
+    public string $description;
+
+    /**
+     * Long description for detailed help.
+     *
+     * @var string
+     */
+    public string $longDescription;
+
+    /**
+     * Usage string.
+     *
+     * @var string
+     */
+    public string $usage;
+
+    /**
+     * Cooldown duration in milliseconds.
+     *
+     * @var int
+     */
+    public int $cooldown;
+
+    /**
+     * Message displayed when the cooldown is active.
+     *
+     * @var string
+     */
+    public string $cooldownMessage;
+
+    /**
+     * Whether this command should be shown in help listings.
+     *
+     * @var bool
+     */
+    public bool $showHelp;
+
+    /**
+     * Cooldowns map: user ID => timestamp when cooldown expires.
+     *
+     * @var array<string,int>
+     */
+    protected array $cooldowns = [];
+
+    /**
+     * Sub-commands map: name => Command.
+     *
+     * @var array<string, Command>
+     */
+    protected array $subCommands = [];
+
+    /**
+     * Sub-command aliases: alias => sub-command name.
+     *
+     * @var array<string, string>
+     */
+    protected array $subCommandAliases = [];
+
+    /**
+     * Owning MessageCommandClient instance.
+     *
+     * @var MessageCommandClient
+     */
+    protected MessageCommandClient $client;
+
+    /**
+     * The callable executed when the command is invoked.
+     *
+     * @var callable
+     */
+    protected $callable;
+
+    /**
+     * Creates a command instance.
+     *
+     * @param MessageCommandClient $client          The message command client.
+     * @param string               $command         Command trigger.
+     * @param callable             $callable        Callable to execute.
+     * @param string               $description     Short description.
+     * @param string               $longDescription Long description.
+     * @param string               $usage           Usage string.
+     * @param int                  $cooldown        Cooldown in milliseconds.
+     * @param string               $cooldownMessage Cooldown message.
+     * @param bool                 $showHelp        Whether to show in help.
+     */
+    public function __construct(
+        MessageCommandClient $client,
+        string $command,
+        callable $callable,
+        string $description,
+        string $longDescription,
+        string $usage,
+        int $cooldown,
+        string $cooldownMessage,
+        bool $showHelp = true
+    ) {
+        $this->client = $client;
+        $this->command = $command;
+        $this->callable = $callable;
+        $this->description = $description;
+        $this->longDescription = $longDescription;
+        $this->usage = $usage;
+        $this->cooldown = $cooldown;
+        $this->cooldownMessage = $cooldownMessage;
+        $this->showHelp = $showHelp;
+    }
+
+    /**
+     * Attempts to get a sub command.
+     *
+     * @param string $command The sub-command to get.
+     * @param bool   $aliases Whether to search aliases as well.
+     *
+     * @return Command|null
+     */
+    public function getCommand(string $command, bool $aliases = true): ?Command
+    {
+        if (array_key_exists($command, $this->subCommands)) {
+            return $this->subCommands[$command];
+        }
+
+        if ($aliases && array_key_exists($command, $this->subCommandAliases)) {
+            return $this->subCommands[$this->subCommandAliases[$command]];
+        }
+
+        return null;
+    }
+
+    /**
+     * Registers a new sub-command.
+     *
+     * @param string          $command  The command name.
+     * @param callable|string $callable The function called when the sub-command is executed.
+     * @param array           $options  Options for the sub-command.
+     *
+     * @return Command The created sub-command instance.
+     */
+    public function registerSubCommand(string $command, $callable, array $options = []): Command
+    {
+        if (array_key_exists($command, $this->subCommands)) {
+            throw new \RuntimeException("A sub-command with the name {$command} already exists.");
+        }
+
+        if ($command !== null && $this->client->getCommandClientOptions()['caseInsensitiveCommands']) {
+            $command = mb_strtolower($command);
+        }
+
+        list($commandInstance, $options) = $this->client->buildCommand($command, $callable, $options);
+        $this->subCommands[$command] = $commandInstance;
+
+        foreach ($options['aliases'] as $alias) {
+            $this->registerSubCommandAlias($alias, $command);
+        }
+
+        return $commandInstance;
+    }
+
+    /**
+     * Unregister a sub-command.
+     *
+     * @param string $command Sub-command name.
+     */
+    public function unregisterSubCommand(string $command): void
+    {
+        if (! array_key_exists($command, $this->subCommands)) {
+            throw new \RuntimeException("A sub-command with the name {$command} does not exist.");
+        }
+
+        unset($this->subCommands[$command]);
+    }
+
+    /**
+     * Register an alias for a sub-command.
+     *
+     * @param string $alias   Alias name.
+     * @param string $command Target sub-command name.
+     */
+    public function registerSubCommandAlias(string $alias, string $command): void
+    {
+        if ($alias !== null && $this->client->getCommandClientOptions()['caseInsensitiveCommands']) {
+            $alias = mb_strtolower($alias);
+        }
+
+        $this->subCommandAliases[$alias] = $command;
+    }
+
+    /**
+     * Unregister a sub-command alias.
+     *
+     * @param string $alias Alias name.
+     */
+    public function unregisterSubCommandAlias(string $alias): void
+    {
+        if (! array_key_exists($alias, $this->subCommandAliases)) {
+            throw new \RuntimeException("A sub-command alias with the name {$alias} does not exist.");
+        }
+
+        unset($this->subCommandAliases[$alias]);
+    }
+
+    /**
+     * Executes the command.
+     *
+     * @param Message $message The message that triggered the command.
+     * @param array   $args    Parsed arguments.
+     *
+     * @return mixed The response from the callable or sub-command.
+     */
+    public function handle(Message $message, array $args)
+    {
+        $subCommand = $originalSubCommand = array_shift($args);
+
+        if ($subCommand !== null && $this->client->getCommandClientOptions()['caseInsensitiveCommands']) {
+            $subCommand = mb_strtolower($subCommand);
+        }
+
+        if (array_key_exists($subCommand, $this->subCommands)) {
+            return $this->subCommands[$subCommand]->handle($message, $args);
+        } elseif (array_key_exists($subCommand, $this->subCommandAliases)) {
+            return $this->subCommands[$this->subCommandAliases[$subCommand]]->handle($message, $args);
+        }
+
+        if (null !== $subCommand) {
+            array_unshift($args, $originalSubCommand);
+        }
+
+        $currentTime = (int) round(microtime(true) * 1000);
+        if (isset($this->cooldowns[$message->author->id])) {
+            if ($this->cooldowns[$message->author->id] < $currentTime) {
+                $this->cooldowns[$message->author->id] = $currentTime + $this->cooldown;
+            } else {
+                return sprintf($this->cooldownMessage, (($this->cooldowns[$message->author->id] - $currentTime) / 1000));
+            }
+        } else {
+            $this->cooldowns[$message->author->id] = $currentTime + $this->cooldown;
+        }
+
+        return call_user_func_array($this->callable, [$message, $args]);
+    }
+
+    /**
+     * Gets help information for this command.
+     *
+     * @param string $prefix The prefix to display before the command.
+     *
+     * @return array Help information.
+     */
+    public function getHelp(string $prefix): array
+    {
+        if (! $this->showHelp) {
+            return [];
+        }
+
+        $subCommandsHelp = [];
+
+        foreach ($this->subCommands as $command) {
+            if ($command->showHelp) {
+                $subCommandsHelp[] = $command->getHelp($prefix.$this->command.' ');
+            }
+        }
+
+        return [
+            'command' => $prefix.$this->command,
+            'description' => $this->description,
+            'longDescription' => $this->longDescription,
+            'usage' => $this->usage,
+            'subCommandsHelp' => $subCommandsHelp,
+        ];
+    }
+
+    /**
+     * Handles dynamic getters for some public properties.
+     *
+     * @param string $variable Property name.
+     *
+     * @return mixed|null
+     */
+    public function __get(string $variable)
+    {
+        static $allowed = ['command', 'description', 'longDescription', 'usage', 'cooldown', 'cooldownMessage', 'showHelp'];
+
+        if (in_array($variable, $allowed, true)) {
+            return $this->{$variable};
+        }
+
+        return null;
+    }
+
+    /**
+     * Magic invoke to allow the command to be called like a callable.
+     *
+     * @param Message $message The message that triggered the command.
+     * @param array   $args    Parsed arguments.
+     *
+     * @return mixed The result of {@see handle()}.
+     */
+    public function __invoke(Message $message, array $args)
+    {
+        return $this->handle($message, $args);
+    }
+}
