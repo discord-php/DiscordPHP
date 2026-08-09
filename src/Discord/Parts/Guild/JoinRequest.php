566bde1f518f1b2f6cd087d568599aa87a57e933
Valithor Obsidion <valithor@discordphp.org>
2026-08-09 10:25:40 -0400

    Create JoinRequest.php

---
+++ src/Discord/Parts/Guild/JoinRequest.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-2022 David Cole <david.cole1340@gmail.com>
+ * Copyright (c) 2020-present Valithor Obsidion <valithor@discordphp.org>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\Parts\Guild;
+
+use Carbon\Carbon;
+use Discord\Helpers\ExCollectionInterface;
+use Discord\Parts\Part;
+use Discord\Parts\User\User;
+
+/**
+ * Represents a guild join request.
+ * 
+ * @link TODO
+ *
+ * @since 10.55.0
+ *
+ * @property string                    $id                 ID of the join request.
+ * @property Carbon|null               $created_at         When the applicant started the join request.
+ * @property Carbon|null               $reviewed_at        When the join request was approved or rejected.
+ * @property string|null               $application_status Application status (STARTED, SUBMITTED, APPROVED, REJECTED).
+ * @property string|null               $rejection_reason   Reason the join request was rejected (only set when REJECTED).
+ * @property string                    $guild_id           ID of the guild the applicant is applying to.
+ * @property string                    $user_id            ID of the applicant.
+ * @property ?User|null                $user               The applicant user object.
+ * @property ?FormFieldResponse[]|null $form_responses     Applicant's responses to the guild's verification form.
+ * @property ?User|null                $actioned_by_user   User who approved or rejected the join request.
+ */
+class JoinRequest extends Part
+{
+    /**
+     * @inheritDoc
+     */
+    protected $fillable = [
+        'id',
+        'created_at',
+        'reviewed_at',
+        'application_status',
+        'rejection_reason',
+        'guild_id',
+        'user_id',
+        'user',
+        'form_responses',
+        'actioned_by_user',
+    ];
+
+    protected function getCreatedAtAttribute(): ?Carbon
+    {
+        return $this->attributeCarbonHelper('created_at');
+    }
+
+    protected function getReviewedAtAttribute(): ?Carbon
+    {
+        return $this->attributeCarbonHelper('reviewed_at');
+    }
+
+    protected function getGuildAttribute(): ?Guild
+    {
+        return $this->discord->guilds->get('id', $this->guild_id);
+    }
+
+    protected function getUserAttribute(): ?User
+    {
+        return $this->attributePartHelper('user', User::class);
+    }
+
+    /**
+     * Returns form responses as an array of FormFieldResponse parts.
+     *
+     * @return ExCollectionInterface<FormFieldResponse>|null
+     */
+    protected function getFormResponsesAttribute(): ?ExCollectionInterface
+    {
+        return $this->attributeTypedCollectionHelper(FormFieldResponse::class, 'form_responses');
+    }
+
+    protected function getActionedByUserAttribute(): ?User
+    {
+        return $this->attributePartHelper('actioned_by_user', User::class);
+    }
+}
