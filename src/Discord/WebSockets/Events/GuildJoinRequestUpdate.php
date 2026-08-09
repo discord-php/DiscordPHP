279092894714c79e97510e70ae72ffb7fe626e10
Valithor Obsidion <valithor@discordphp.org>
2026-08-09 10:51:46 -0400

    Create GuildJoinRequestUpdate.php

---
+++ src/Discord/WebSockets/Events/GuildJoinRequestUpdate.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the DiscordPHP project.
+ *
+ * Copyright (c) 2015-2022 David Cole <david.cole1340@gmail.com>
+ * Copyright (c) 2020-present Valithor Obsidion <valithor@discordphp.org>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Discord\WebSockets\Events;
+
+use Discord\WebSockets\Event;
+use Discord\WebSockets\Events\Data\GuildJoinRequestUpdateData;
+use Discord\Parts\Guild\Guild;
+use Discord\Parts\Guild\JoinRequest;
+
+/**
+ * @link https://docs.discord.com/developers/events/gateway-events#guild-join-request-update
+ *
+ * @since 10.55.0
+ */
+class GuildJoinRequestUpdate extends Event
+{
+    /** @inheritDoc */
+    public function handle($data)
+    {
+        /** @var GuildJoinRequestUpdateData */
+        $data = $this->factory->part(GuildJoinRequestUpdateData::class, (array) $data, true);
+
+        /** @var JoinRequest */
+        $request = $this->factory->part(JoinRequest::class, (array) $data->request, true);
+
+        /** @var Guild $guild */
+        if ($guild = yield $this->discord->guilds->cacheGet($request->guild_id)) {
+            $guild->join_requests->set($request->id, $request);
+        }
+
+        if (isset($request->user)) {
+            $this->cacheUser($request->user);
+        }
+
+        return $request;
+    }
+}
