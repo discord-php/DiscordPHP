55c67ca414b54019422bce377906b90e2390abde
Valithor Obsidion <valithor@discordphp.org>
2026-08-25 10:55:42 -0400

    Bump patch version

--- src/Discord/Discord.php
+++ src/Discord/Discord.php
@@ -122,7 +122,7 @@ class Discord
      *
      * @var string Version.
      */
-    public const VERSION = 'v10.56.6';
+    public const VERSION = 'v10.56.9';
 
     public const REFERRER = 'https://github.com/discord-php/DiscordPHP';
 
